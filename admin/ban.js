e6e5a762922c80bcc1680b400979d1a9c9dc6607
sprayx <sprayhasswag@gmail.com>
2021-04-16 12:59:41 -0300

    Emoji/Ban

--- admin/ban.js
+++ admin/ban.js
@@ -2,9 +2,9 @@ const Discord = require ("discord.js")
 
 exports.run = (bot,message,args) => {
   if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("você não tem permissão para executar o comando.");
-  if(args.lenght === 0) return message.reply("use **f!ban <@Pessoa> <Motivo>** para banir alguém.");
+  if(args.lenght === 0) return message.reply("use **s.ban <@Pessoa> <Motivo>** para banir alguém.");
   let banMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-  if(!banMember) return message.reply("use **f!ban <@Pessoa> <Motivo>** para banir alguém.");
+  if(!banMember) return message.reply("use **s.ban <@Pessoa> <Motivo>** para banir alguém.");
   let banReason = args.join(" ").slice(22) || args.slice(1).join(" ");
   if(!banReason){
     banReason = "O motivo não foi especificado."
--- commands/emoji.js
+++ commands/emoji.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
     return message.channel.send(
       `**${message.author.username}, a sintaxe correta é:** ` +
         "`" +
-        "!emoji nomedoemoji`"
+        "s.emoji <nomedoemoji>`"
     ); //Troque a exclamação ! da mensagem acima pelo seu prefixo
   let emoji = message.guild.emojis.cache.find(emoji => emoji.name === args[0]);
 
