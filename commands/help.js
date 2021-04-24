e9e0bfb8fd6817b2eb3717849b161f77ea87e7c3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-04-24 20:57:02 -0300

    Help

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
-const config = require("../commands.json");
 
-exports.run = async (bot, message, args) => {
+exports.run = async (client, message, args) => {
   let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author; 
   let avatar = user.displayAvatarURL({ dynamic: true, size: 4096 })
 
