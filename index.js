0ffdee4b9b9819d33a6d926a0e44ce0ebfab6025
sprayx <71204668+sprayx@users.noreply.github.com>
2021-04-25 11:18:18 -0300

    [+] Update mentions

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const { APIMessage, Structures } = require("discord.js");
 
   client.on('ready', () => {
       let activities_list = [
-        `spr4y.xyz/resolute`,
+        ``,
         `Estou em ${client.guilds.cache.size} servidores!, com ${client.users.cache.size} pessoas!`,
+        `spr4y.xyz/resolute`,
       ];
       setInterval(() => {
           const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
@@ -20,9 +21,11 @@ const { APIMessage, Structures } = require("discord.js");
     });
 
   client.on('message', message=> {
-    if (message.mentions.has(client.user.id)) {
-      message.channel.send(`👾 Olá! ${message.author}\n > Meu prefixo é _**s.**_ \n > Me adicione! **https://top.gg/bot/764919941538775050**`);
-  };
+    if (message.content.startsWith('<')) {
+      if (message.content.endsWith('>')){
+          if (message.mentions.has(client.user.id)) { return message.inlineReply(`👾 Olá! ${message.author}\n > Meu prefixo é _**s.**_ \n > Me adicione! **https://top.gg/bot/764919941538775050**`).then(msg => msg.delete({ timeout: 5000 })).catch(err => { return }) }
+      }
+    }
   });
 
 client.on("message",t=>{if(t.author.bot)return;if("dm"==t.channel.type)return;if(!t.content.toLowerCase().startsWith(config.prefix.toLowerCase()))return;if(t.content.startsWith(`<@!${client.user.id}>`)||t.content.startsWith(`<@${client.user.id}>`))return;const e=t.content.trim().slice(config.prefix.length).split(/ +/g),r=e.shift().toLowerCase();try{require(`./commands/fun/${r}.js`).run(client,t,e)}catch(t){console.error("Erro:"+t)}});
@@ -33,36 +36,6 @@ client.on("message",t=>{if(t.author.bot)return;if("dm"==t.channel.type)return;if
 client.on("message",t=>{if(t.author.bot)return;if("dm"==t.channel.type)return;if(!t.content.toLowerCase().startsWith(config.prefix.toLowerCase()))return;if(t.content.startsWith(`<@!${client.user.id}>`)||t.content.startsWith(`<@${client.user.id}>`))return;const e=t.content.trim().slice(config.prefix.length).split(/ +/g),r=e.shift().toLowerCase();try{require(`./commands/random/${r}.js`).run(client,t,e)}catch(t){console.error("Erro:"+t)}});
 client.on("message",t=>{if(t.author.bot)return;if("dm"==t.channel.type)return;if(!t.content.toLowerCase().startsWith(config.prefix.toLowerCase()))return;if(t.content.startsWith(`<@!${client.user.id}>`)||t.content.startsWith(`<@${client.user.id}>`))return;const e=t.content.trim().slice(config.prefix.length).split(/ +/g),r=e.shift().toLowerCase();try{require(`./commands/interacao/${r}.js`).run(client,t,e)}catch(t){console.error("Erro:"+t)}});
 client.on("message",t=>{if(t.author.bot)return;if("dm"==t.channel.type)return;if(!t.content.toLowerCase().startsWith(config.prefix.toLowerCase()))return;if(t.content.startsWith(`<@!${client.user.id}>`)||t.content.startsWith(`<@${client.user.id}>`))return;const e=t.content.trim().slice(config.prefix.length).split(/ +/g),r=e.shift().toLowerCase();try{require(`./commands/games/${r}.js`).run(client,t,e)}catch(t){console.error("Erro:"+t)}});
-
-class Message extends Structures.get("Message") {
-    async inlineReply(content, options) {
-        const mentionRepliedUser = typeof ((options || content || {}).allowedMentions || {}).repliedUser === "undefined" ? true : ((options || content).allowedMentions).repliedUser;
-        delete ((options || content || {}).allowedMentions || {}).repliedUser;
-
-        const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options).resolveData();
-        Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
-    
-        if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0)
-            apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
-        if (typeof apiMessage.data.allowed_mentions.replied_user === "undefined")
-            Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
-
-        if (Array.isArray(apiMessage.data.content)) {
-            return Promise.all(apiMessage.split().map(x => {
-                x.data.allowed_mentions = apiMessage.data.allowed_mentions;
-                return x;
-            }).map(this.inlineReply.bind(this)));
-        }
-
-        const { data, files } = await apiMessage.resolveFiles();
-        return this.client.api.channels[this.channel.id].messages
-            .post({ data, files })
-            .then(d => this.client.actions.MessageCreate.handle(d).message);
-    }
-}
-
-Structures.extend("Message", () => Message)
-
-
+class Message extends(Structures.get("Message")){async inlineReply(e,s){const a=void 0===((s||e||{}).allowedMentions||{}).repliedUser||(s||e).allowedMentions.repliedUser;delete((s||e||{}).allowedMentions||{}).repliedUser;const t=e instanceof APIMessage?e.resolveData():APIMessage.create(this.channel,e,s).resolveData();if(Object.assign(t.data,{message_reference:{message_id:this.id}}),t.data.allowed_mentions&&0!==Object.keys(t.data.allowed_mentions).length||(t.data.allowed_mentions={parse:["users","roles","everyone"]}),void 0===t.data.allowed_mentions.replied_user&&Object.assign(t.data.allowed_mentions,{replied_user:a}),Array.isArray(t.data.content))return Promise.all(t.split().map(e=>(e.data.allowed_mentions=t.data.allowed_mentions,e)).map(this.inlineReply.bind(this)));const{data:n,files:l}=await t.resolveFiles();return this.client.api.channels[this.channel.id].messages.post({data:n,files:l}).then(e=>this.client.actions.MessageCreate.handle(e).message)}}Structures.extend("Message",()=>Message);
 
 client.login(config.token);
