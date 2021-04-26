6179c70d1e87acf3bfaa0d35a2d15f9b2665e06f
sprayx <71204668+sprayx@users.noreply.github.com>
2021-04-26 15:28:10 -0300

    Comando de help aprimorado

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,150 @@
 const Discord = require('discord.js');
+exports.run = async (bot, message, args) => {
+  let embed = new Discord.MessageEmbed()
+    .setColor('#e1ff00')
+    .setDescription(`> ***Olá ${message.author}, aqui estão minhas categorias com comandos!***
+      \n » **Comandos**:
+      > Moderação - <a:1__:836268679263027230>
+      > Fun - <a:2_:836268689484546088>
+      > Outros - <a:3___:836268637257990184>
+      > Random - <a:4_:836268669516251136>
+      \n » **Links**:
+      > http://spr4y.xyz/resolute
+    `)
+    .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833806087702446181/image0.gif")
+  //
+  message.channel.send(message.author, embed).then(msg => {
+    msg.react(`⬅️`).then(() => {
+      msg.react(`<a:1__:836268679263027230>`);
+      msg.react(`<a:2_:836268689484546088>`);
+      msg.react(`<a:3___:836268637257990184>`);
+      msg.react(`<a:4_:836268669516251136>`);
+      msg.react(`<a:5___:836268658795347990>`);
+    })
+    
+    //Emojis
+    const voltar = msg.createReactionCollector((reaction, user) => reaction.emoji.name == `⬅️` && user.id == message.author.id, { time: 20000 })
+    const moderacao = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268679263027230` && user.id == message.author.id, { time: 20000 })
+    const fun = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268689484546088` && user.id == message.author.id, { time: 20000 })
+    const outros = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268637257990184` && user.id == message.author.id, { time: 20000 })
+    const random = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268669516251136` && user.id == message.author.id, { time: 20000 })
+    const games = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268658795347990` && user.id == message.author.id, { time: 20000 })
 
-exports.run = async (client, message, args) => {
-  let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author; 
-  let avatar = user.displayAvatarURL({ dynamic: true, size: 4096 })
+    ////////////
+    moderacao.on(`collect`, r => {
+       let embed_1 = new Discord.MessageEmbed()
+      .setColor('#e1ff00')
+      .setDescription(`**» Categoria de Moderação:**\n
+      > **s.aviso**
+      > **s.ban**
+      > **s.banlist**
+      > **s.botinfo**
+      > **s.clear**
+      > **s.inviteblock**
+      > **s.kick**
+      > **s.lock**
+      > **s.report**
+      > **s.servericon**
+      > **s.serverinfo**
+      > **s.userinfo**
+      > **s.unlock**
+      > **s.warn**
+      > **s.welcome**
+      `)
+      .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833824277832663040/a_9d9bbd759d56a3bedbf6aaedf9ef1507.gif")
+      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
+      msg.edit(embed_1)
+      //r.users.remove(message.author.id)
+    })
 
-  let embed_3 = new Discord.MessageEmbed()
-  .setColor('#e1ff00')
-  .setDescription(`> **<a:__:835643988022263879> Olá ${message.author}, aqui estão algumas informações!**\n\n
-    > <a:__:835643988022263879> Website
-    **http://spr4y.xyz/resolute**
-
-    > <:info:835206734225473546> Lista de comandos
-    **http://spr4y.xyz/resolute#commands**
-  `)
-  .setImage(avatar)
-  .setThumbnail(avatar)
-  message.channel.send(embed_3)
+    ////////////
+    fun.on(`collect`, r => {
+      let embed_2 = new Discord.MessageEmbed()
+      .setColor('#e1ff00')
+      .setDescription(`**» Categoria de Diversão:**\n
+      > **s.carinho**
+      > **s.coinflip**
+      > **s.highfive**
+      > **s.hug**
+      > **s.kiss**
+      > **s.matar**
+      > **s.meme**
+      > **s.morder**
+      > **s.pisar**
+      > **s.say**
+      > **s.ship**
+      `)
+      .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833824277832663040/a_9d9bbd759d56a3bedbf6aaedf9ef1507.gif")
+      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
+      msg.edit(embed_2)
+      //r.users.remove(message.author.id)
+    })
   
-}
+    /////////////
+    outros.on(`collect`, r => {
+      let embed_3 = new Discord.MessageEmbed()
+      .setColor('#e1ff00')
+      .setDescription(`**» Outros comandos:**\n
+      > **s.ascii**
+      > **s.avatar**
+      > **s.clima**
+      > **s.covid**
+      > **s.emoji**
+      > **s.horas**
+      > **s.ping**
+      > **s.sorteador**
+      > **s.status**
+      > **s.sugestão**
+      > **s.uptime**
+      > **s.votacao**
+      `)
+      .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833824277832663040/a_9d9bbd759d56a3bedbf6aaedf9ef1507.gif")
+      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
+      msg.edit(embed_3)
+      //r.users.remove(message.author.id)
+    })
+
+    random.on(`collect`, r => {
+      let embed_4 = new Discord.MessageEmbed()
+      .setColor('#e1ff00')
+      .setDescription(`**» Categoria de Random:**\n
+      > **s.stonks**
+      > **s.notstonks**
+      `)
+      .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833824277832663040/a_9d9bbd759d56a3bedbf6aaedf9ef1507.gif")
+      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
+      msg.edit(embed_4)
+    })
+    
+    games.on(`collect`, r => {
+      let embed_5 = new Discord.MessageEmbed()
+      .setColor('#e1ff00')
+      .setDescription(`**» Categoria de Games:**\n
+      > **s.minecraft**
+      > **s.valorant**
+      > **s.steam**
+      `)
+      .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833824277832663040/a_9d9bbd759d56a3bedbf6aaedf9ef1507.gif")
+      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
+      msg.edit(embed_5)
+    })
+    
+  
+
+    voltar.on(`collect`, r => {
+      let embed = new Discord.MessageEmbed()
+      .setColor('#e1ff00')
+      .setDescription(`> ***Olá ${message.author}, aqui estão minhas categorias com comandos!***
+        \n » **Comandos**:
+        > Moderação - <a:1__:836268679263027230>
+        > Fun - <a:2_:836268689484546088>
+        > Outros - <a:3___:836268637257990184>
+        > Random - <a:4_:836268669516251136>
+        \n » **Links**:
+        > http://spr4y.xyz/resolute
+      `)
+      .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833806087702446181/image0.gif")
+      msg.edit(embed)
+  });
+})
+} 
