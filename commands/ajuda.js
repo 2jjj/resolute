c108b9a11c498e7ac49cd1cfd42ff34a3892676e
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-01 12:08:15 -0300

    [Update] Economia

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -7,8 +7,7 @@ exports.run = async (bot, message, args) => {
       <:info:835206734225473546> | Moderação - <a:1__:836268679263027230>
       <:info:835206734225473546> | Fun - <a:2_:836268689484546088>
       <:info:835206734225473546> | Outros - <a:3___:836268637257990184>
-      <:info:835206734225473546> | Random - <a:4_:836268669516251136>
-      <:info:835206734225473546> | Games - <a:5___:836268658795347990>
+      <:info:835206734225473546> | Economia - <a:4_:836268669516251136>
       <:info:835206734225473546> | Voltar - <:Voltar:836330128073687092>
       \n » **Links:**
       > http://spr4y.xyz/resolute
@@ -21,7 +20,6 @@ exports.run = async (bot, message, args) => {
       msg.react(`<a:2_:836268689484546088>`);
       msg.react(`<a:3___:836268637257990184>`);
       msg.react(`<a:4_:836268669516251136>`);
-      msg.react(`<a:5___:836268658795347990>`);
     })
     
     //Emojis
@@ -29,8 +27,7 @@ exports.run = async (bot, message, args) => {
     const moderacao = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268679263027230` && user.id == message.author.id, { time: 20000 })
     const fun = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268689484546088` && user.id == message.author.id, { time: 20000 })
     const outros = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268637257990184` && user.id == message.author.id, { time: 20000 })
-    const random = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268669516251136` && user.id == message.author.id, { time: 20000 })
-    const games = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268658795347990` && user.id == message.author.id, { time: 20000 })
+    const economia = msg.createReactionCollector((reaction, user) => reaction.emoji.id == `836268669516251136` && user.id == message.author.id, { time: 20000 })
 
     ////////////
     moderacao.on(`collect`, r => {
@@ -74,6 +71,8 @@ exports.run = async (bot, message, args) => {
       > **<:spr4yxyz:837798446584168468> | s.pisar**
       > **<:spr4yxyz:837798446584168468> | s.say**
       > **<:spr4yxyz:837798446584168468> | s.ship**
+      > **<:spr4yxyz:837798446584168468> | s.stonks**
+      > **<:spr4yxyz:837798446584168468> | s.notstonks**
       `)
       .setImage("https://i.pinimg.com/originals/58/58/97/58589775e6dfe9aad63363e06a38a3ea.gif")
       .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
@@ -98,6 +97,9 @@ exports.run = async (bot, message, args) => {
       > **<:spr4yxyz:837798446584168468> | s.sugestão**
       > **<:spr4yxyz:837798446584168468> | s.uptime**
       > **<:spr4yxyz:837798446584168468> | s.votar**
+      > **<:spr4yxyz:837798446584168468> | s.minecraft**
+      > **<:spr4yxyz:837798446584168468> | s.valorant**
+      > **<:spr4yxyz:837798446584168468> | s.steam**
       `)
       .setImage("https://i.imgur.com/VhLyg2r.gif")
       .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
@@ -105,34 +107,7 @@ exports.run = async (bot, message, args) => {
       //r.users.remove(message.author.id)
     })
 
-    random.on(`collect`, r => {
-      let embed_4 = new Discord.MessageEmbed()
-      .setColor('#e1ff00')
-      .setDescription(`**» Categoria de Random:**\n
-      > **<:spr4yxyz:837798446584168468> | s.stonks**
-      > **<:spr4yxyz:837798446584168468> | s.notstonks**
-      `)
-      .setImage("http://pa1.narvii.com/5763/85377e06886cbaa577b87952dd985919f3ad0e38_00.gif")
-      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
-      msg.edit(embed_4)
-    })
     
-    games.on(`collect`, r => {
-      let embed_5 = new Discord.MessageEmbed()
-      .setColor('#e1ff00')
-      .setDescription(`**» Categoria de Games:**\n
-      > **<:spr4yxyz:837798446584168468> | s.minecraft**
-      > **<:spr4yxyz:837798446584168468> | s.valorant**
-      > **<:spr4yxyz:837798446584168468> | s.valorant**
-      > **<:spr4yxyz:837798446584168468> | s.steam**
-      `)
-      .setImage("https://i.pinimg.com/originals/cc/1e/4a/cc1e4a2ec356aee8ed91a2ffd99a3862.gif")
-      .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")
-      msg.edit(embed_5)
-    })
-    
-  
-
     voltar.on(`collect`, r => {
       let embed = new Discord.MessageEmbed()
       .setColor('#e1ff00')
@@ -141,8 +116,7 @@ exports.run = async (bot, message, args) => {
         <:info:835206734225473546> | Moderação - <a:1__:836268679263027230>
         <:info:835206734225473546> | Fun - <a:2_:836268689484546088>
         <:info:835206734225473546> | Outros - <a:3___:836268637257990184>
-        <:info:835206734225473546> | Random - <a:4_:836268669516251136>
-        <:info:835206734225473546> | Games - <a:5___:836268658795347990>
+        <:info:835206734225473546> | Economia - <a:4_:836268669516251136>
         <:info:835206734225473546> | Voltar - <:Voltar:836330128073687092>
         \n » **Links:**
         > http://spr4y.xyz/resolute
