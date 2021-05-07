cb5c04d929d276dd05701cd16dfb822870743c8d
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-07 19:28:16 -0300

    💻 Fix

--- commands/admin/clear.js
+++ commands/admin/clear.js
@@ -3,18 +3,18 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
   if (!message.member.permissions.has("MANAGE_MESSAGES"))
     return message.reply(
-      "<:info:835206734225473546> | Você não tem a permissão de `Gerenciar Mensagens` para usar esse comando"
+      "<:info:835206734225473546> **|** Você não tem a permissão de `Gerenciar Mensagens` para usar esse comando"
     );
   const deleteCount = parseInt(args[0], 10);
-  if (!deleteCount || deleteCount < 1 || deleteCount > 200)
+  if (!deleteCount || deleteCount < 1 || deleteCount > 100)
     return message.reply(
-      "<:info:835206734225473546> | Forneça um número de até **200 mensagens** a serem excluídas"
+      "<:info:835206734225473546> **|** Forneça um número de até **100 mensagens** a serem excluídas"
     );
 
   const amountDelete = parseInt(args[0]);
   message.channel.bulkDelete(amountDelete);
   message.channel
-    .send(`**<:info:835206734225473546> | ${args[0]} Mensagens limpas nesse chat!**`).then(msg => msg.delete({timeout: 5000}))
+    .send(`<:info:835206734225473546> **|** ${args[0]} Mensagens limpas nesse chat`).then(msg => msg.delete({timeout: 5000}))
     .catch(error => 
       console.log(`<:info:835206734225473546> | Não foi possível deletar mensagens devido a: ${error}`)
     );
