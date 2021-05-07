66c2201f4b236e070f400605f08fe68f5d01bf1b
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-07 20:17:03 -0300

    ✨ Bug fixes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on("guildMemberRemove", (member) => {
 	db.delete(`work_${member.guild.id}_${member.id}`); 
 	db.delete(`daily_${member.guild.id}_${member.id}`); 
 	db.delete(`rob_${member.guild.id}_${member.id}`); 
-	console.log(`[ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os Status de Economia removidos!`);
+	console.log(`[ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
 })
 
 client.login(config.token);
