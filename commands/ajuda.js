971dd2ea0c387544aa9f15da2f4cd2083f293286
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-08 23:08:33 -0300

    Update

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -11,6 +11,8 @@ exports.run = async (bot, message, args) => {
       <:info:835206734225473546> | Voltar - <:Voltar:836330128073687092>
       \n » **Links:**
       > http://spr4y.xyz/resolute
+      \n » 
+      > Feito por Spray#7194
     `)
     .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833806087702446181/image0.gif")
   //
@@ -34,20 +36,20 @@ exports.run = async (bot, message, args) => {
        let embed_1 = new Discord.MessageEmbed()
       .setColor('#e1ff00')
       .setDescription(`**» Categoria de Moderação:**\n
-      > <:spr4yxyz:837798446584168468> | **s.aviso**
-      > <:spr4yxyz:837798446584168468> | **s.ban**
+      > <:spr4yxyz:837798446584168468> | **s.aviso ⇝ \`s.aviso <@user>\`**
+      > <:spr4yxyz:837798446584168468> | **s.ban ⇝ \`s.ban <@user>\`**
       > <:spr4yxyz:837798446584168468> | **s.banlist**
       > <:spr4yxyz:837798446584168468> | **s.botinfo**
       > <:spr4yxyz:837798446584168468> | **s.clear**
       > <:spr4yxyz:837798446584168468> | **s.inviteblock**
-      > <:spr4yxyz:837798446584168468> | **s.kick**
+      > <:spr4yxyz:837798446584168468> | **s.kick ⇝ \`s.kick <@user>\`**
       > <:spr4yxyz:837798446584168468> | **s.lock**
       > <:spr4yxyz:837798446584168468> | **s.report(Desenvolvimento)**
-      > <:spr4yxyz:837798446584168468> | **s.servericon**
-      > <:spr4yxyz:837798446584168468> | **s.serverinfo**
-      > <:spr4yxyz:837798446584168468> | **s.userinfo**
+      > <:spr4yxyz:837798446584168468> | **s.servericon ⇝ \`s.servericon <ID>\`**
+      > <:spr4yxyz:837798446584168468> | **s.serverinfo ⇝ \`s.serverinfo <ID>\`**
+      > <:spr4yxyz:837798446584168468> | **s.userinfo ⇝ \`s.userinfo <ID>\`**
       > <:spr4yxyz:837798446584168468> | **s.unlock**
-      > <:spr4yxyz:837798446584168468> | **s.warn**
+      > <:spr4yxyz:837798446584168468> | **s.warn ⇝ \`s.warn <@user>\`**
       > <:spr4yxyz:837798446584168468> | **s.welcome(Desenvolvimento)**
       `)
       .setImage("https://i.pinimg.com/originals/dd/16/ab/dd16ab8cb777b7ea951dec7092006fce.gif")
@@ -61,16 +63,16 @@ exports.run = async (bot, message, args) => {
       let embed_2 = new Discord.MessageEmbed()
       .setColor('#e1ff00')
       .setDescription(`**» Categoria de Diversão:**\n
-      > **<:spr4yxyz:837798446584168468> | s.carinho**
+      > **<:spr4yxyz:837798446584168468> | s.carinho \`s.carinho <@user>\`**
       > **<:spr4yxyz:837798446584168468> | s.coinflip**
-      > **<:spr4yxyz:837798446584168468> | s.highfive**
-      > **<:spr4yxyz:837798446584168468> | s.hug**
-      > **<:spr4yxyz:837798446584168468> | s.kiss**
+      > **<:spr4yxyz:837798446584168468> | s.highfive ⇝ \`s.highfive <@user>\`**
+      > **<:spr4yxyz:837798446584168468> | s.hug ⇝ \`s.hug <@user>\`**
+      > **<:spr4yxyz:837798446584168468> | s.kiss ⇝ \`s.kiss <@user>\`**
       > **<:spr4yxyz:837798446584168468> | s.meme**
-      > **<:spr4yxyz:837798446584168468> | s.morder**
-      > **<:spr4yxyz:837798446584168468> | s.pisar**
-      > **<:spr4yxyz:837798446584168468> | s.say**
-      > **<:spr4yxyz:837798446584168468> | s.ship**
+      > **<:spr4yxyz:837798446584168468> | s.morder ⇝ \`s.morder <@user>\`**
+      > **<:spr4yxyz:837798446584168468> | s.pisar ⇝ \`s.pisar <@user>\`**
+      > **<:spr4yxyz:837798446584168468> | s.say ⇝ \`s.say <frase>\`**
+      > **<:spr4yxyz:837798446584168468> | s.ship ⇝ \`s.ship <@user>\`**
       > **<:spr4yxyz:837798446584168468> | s.stonks**
       > **<:spr4yxyz:837798446584168468> | s.notstonks**
       `)
@@ -86,15 +88,15 @@ exports.run = async (bot, message, args) => {
       .setColor('#e1ff00')
       .setDescription(`**» Outros comandos:**\n
       > **<:spr4yxyz:837798446584168468> | s.ascii**
-      > **<:spr4yxyz:837798446584168468> | s.avatar**
+      > **<:spr4yxyz:837798446584168468> | s.avatar \`s.avatar <@user>\`**
       > **<:spr4yxyz:837798446584168468> | s.clima**
-      > **<:spr4yxyz:837798446584168468> | s.covid**
-      > **<:spr4yxyz:837798446584168468> | s.emoji**
+      > **<:spr4yxyz:837798446584168468> | s.covid \`s.covid <pais>\`**
+      > **<:spr4yxyz:837798446584168468> | s.emoji \`s.emoji <nome_emoji>\`**
       > **<:spr4yxyz:837798446584168468> | s.horas**
       > **<:spr4yxyz:837798446584168468> | s.ping**
-      > **<:spr4yxyz:837798446584168468> | s.sorteador**
+      > **<:spr4yxyz:837798446584168468> | s.sorteador \`s.sorteador <numero>\`**
       > **<:spr4yxyz:837798446584168468> | s.status**
-      > **<:spr4yxyz:837798446584168468> | s.sugestão**
+      > **<:spr4yxyz:837798446584168468> | s.sugestao \`s.sugestao <sugestão>\`**
       > **<:spr4yxyz:837798446584168468> | s.uptime**
       > **<:spr4yxyz:837798446584168468> | s.votar**
       > **<:spr4yxyz:837798446584168468> | s.minecraft**
@@ -112,13 +114,13 @@ exports.run = async (bot, message, args) => {
       .setColor('#e1ff00')
       .setDescription(`**» Categoria de Economia:**\n
       > **<:spr4yxyz:837798446584168468> | s.daily**
-      > **<:spr4yxyz:837798446584168468> | s.depositar**
+      > **<:spr4yxyz:837798446584168468> | s.depositar \`<quantidade>\`**
       > **<:spr4yxyz:837798446584168468> | s.leaderboard**
       > **<:spr4yxyz:837798446584168468> | s.money**
-      > **<:spr4yxyz:837798446584168468> | s.pay**
-      > **<:spr4yxyz:837798446584168468> | s.roubar**
-      > **<:spr4yxyz:837798446584168468> | s.saque**
-      > **<:spr4yxyz:837798446584168468> | s.setmoney (Admin)**
+      > **<:spr4yxyz:837798446584168468> | s.pay \`<@user>\`**
+      > **<:spr4yxyz:837798446584168468> | s.roubar \`<@user>\`**
+      > **<:spr4yxyz:837798446584168468> | s.saque \`<quantidade_dinheiro>\`**
+      > **<:spr4yxyz:837798446584168468> | s.setmoney (Admin+)**
       > **<:spr4yxyz:837798446584168468> | s.trabalhar**
       `)
       .setImage("http://pa1.narvii.com/5763/85377e06886cbaa577b87952dd985919f3ad0e38_00.gif")
@@ -139,7 +141,9 @@ exports.run = async (bot, message, args) => {
         <:info:835206734225473546> | Voltar - <:Voltar:836330128073687092>
         \n » **Links:**
         > http://spr4y.xyz/resolute
-      `)
+        \n » 
+        > Feito por Spray#7194
+    `)
       .setImage("https://cdn.discordapp.com/attachments/833789118986059836/833806087702446181/image0.gif")
       msg.edit(embed)
   });
