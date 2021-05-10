5929bcb8cbeef3a21c463e48c5d1d0c01cb1857a
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-10 13:47:26 -0300

    Fix

--- commands/admin/botinfo.js
+++ commands/admin/botinfo.js
@@ -8,15 +8,16 @@ exports.run = async (client, message, args) => {
 
   let embed = new Discord.MessageEmbed()
   .setTitle(" *Informações*")
-  .setDescription("⠀")
   .setColor("RANDOM")
   .setThumbnail(avatar)
-  .addField("<:developer:841128172299878401> | Desenvolvedor: **Spray.#0007**")
-  .addField(`<a:BP_alerta_gif:841129076672299108> | Bot criado no dia: **11/10/2020**`)
-  .addField(`<:iala:841129532773367808> | Estou em ${client.guilds.cache.size} servidores!, com ${client.users.cache.size} pessoas!`)
-  .addField(`<:dy_girlHello:841125764690739203> | Versão discord.js: **12.5.1**`)
-  .addField(`<a:pumpkinsmile:841130240352321557> | Estou na versão **1.2.5**`)
-  .addField(`<:staff:835643948151996446> | Me adicione: *https://resolutebot.xyz*`)
+	.addFields(
+		{ name: 'ﾠ', value: '<:developer:841128172299878401> | Desenvolvedor: **Spray.#0007**' },
+		{ name: 'ﾠ', value: `<a:BP_alerta_gif:841129076672299108> | Bot criado no dia: **11/10/2020**`, inline: false },
+		{ name: 'ﾠ', value: `<:iala:841129532773367808> | Estou em ${client.guilds.cache.size} servidores!, com ${client.users.cache.size} pessoas!`, inline: false },
+    { name: 'ﾠ', value: `<:dy_girlHello:841125764690739203> | Versão discord.js: **12.5.1**`, inline: false },
+    { name: 'ﾠ', value: `<a:pumpkinsmile:841130240352321557> | Estou na versão **1.2.5b**`, inline: false },
+		{ name: 'ﾠ', value: `<:staff:835643948151996446> | Me adicione: *https://resolutebot.xyz*`, inline: false },
+	)
 
   await message.channel.send(embed);
 }
