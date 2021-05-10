782d39185733bdcad2f29364d7671bc004bafae6
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-10 14:01:21 -0300

    arrumei

--- commands/outros/ascii.js
+++ commands/outros/ascii.js
@@ -1,16 +1,16 @@
 var figlet = require('figlet');
 
 exports.run = async (client, message, args) => {
-const ascii = args.join(" "); //pegar os argumentos
+const ascii = args.join(" "); 
 
-if(!ascii) { //se não colocar ascii
+if(!ascii) { 
   message.reply('<:info:835206734225473546> » Por favor, diga uma palavra de, no máximo, 18 palavras.')
   
 } else {
 if(ascii.length >= 18) {
   message.reply('<:info:835206734225473546> » Limite de 18 palavras atingido.')
 } else {
-figlet(ascii, function(err, data) { //coração
+figlet(ascii, function(err, data) {
    if (err) {
        console.dir(err);
        return;
