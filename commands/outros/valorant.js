a2d0ae0fbabb0a4fae6ec6f0ea21bc669e96ce39
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-09 22:39:10 -0300

    up

--- commands/outros/valorant.js
+++ commands/outros/valorant.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
 
-  if (args[0]) { return message.channel.send('Por favor, utilize apenas o comando, sem nada na frente. Informações desnecessárias atrapalham meu processamento.') }
+  if (args[0]) { return message.channel.send('<:bravinha:841126251741970452> Por favor, utilize apenas o comando, sem nada na frente. Informações desnecessárias atrapalham meu processamento.') }
 
   var game = 'Valorant'
   var link1 = 'https://playvalorant.com/pt-br/'
