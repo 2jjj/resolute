87bf2140a1a33f8ecc38f8883bba470fa8c6023d
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 13:35:21 -0300

    events

--- index.js
+++ index.js
@@ -41,8 +41,21 @@ client.on("guildMemberRemove", (member) => {
 	console.log(`[ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
 })
 
+//https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 client.on("guildCreate", async (guild) => {
-  console.log("oi")
+
+  const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
+  client.on("guildCreate", async (guild) => {
+    console.log("+1")
+    const dono = await client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`+1 server :sunglasses:`)
+    .setDescription(`Servidor: ${guild.name}\nID: ${guild.id}\nMembros: ${guild.memberCount} membros\nDono: ${dono}`)
+    .setThumbnail(client.user.displayAvatarURL())
+    .setTimestamp()
+    .setColor('#FFC4E7')
+    webhook.send(embed);
+  })
 })
 
 client.login(config.token);
