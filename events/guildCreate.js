52ed323d598b239dd127fc6b113c2812af302ced
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 14:45:09 -0300

    Events

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,6 +5,7 @@ module.exports = (client, guild, message, guilds, id) => {
 //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
     console.log(`Fui adicionado em um servidor! | ${client.guilds.cache.size} | ${client.users.cache.size}`)
+    client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${client.guilds.cache.size}`).catch((e) => console.log((e)))
     const dono = client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
     let embed = new Discord.MessageEmbed()
     .setTitle(`<a:__:835643988022263879> Fui adicionado em um servidor!`)
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,9 +4,10 @@ module.exports = (client, message, guild) => {
 //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
-    console.log("-1")
+    client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${client.guilds.cache.size}`).catch((e) => console.log((e)))
+    console.log("Fui removido de uma guild! | ${client.guilds.cache.size} | ${client.users.cache.size}")
     let embed = new Discord.MessageEmbed()
-    .setTitle(`-1 server 😔`)
+    .setTitle(`<:info:835206734225473546> | Fui removido de uma guild!`)
     .setTimestamp()
     .setColor('#FFC4E7')
     webhook.send(embed);
