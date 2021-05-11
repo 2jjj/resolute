95f00b104abfc6f203f2f27e3d35b69024a6a73f
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 14:21:26 -0300

    Event handling

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,15 +1,13 @@
 const Discord = require("discord.js")
 
-module.exports = (client, message) => {
+module.exports = (client, message, guild) => {
 //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
-client.on("guildDelete", async (guild) => {
     console.log("-1")
     let embed = new Discord.MessageEmbed()
     .setTitle(`-1 server 😔`)
     .setTimestamp()
     .setColor('#FFC4E7')
     webhook.send(embed);
-  })
 }
