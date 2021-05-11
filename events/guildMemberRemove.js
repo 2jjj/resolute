f741b2663754324187f35219e2393bf316b6c7d1
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 13:48:32 -0300

    Fix

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const db = require("quick.db");
-const client = new Discord.Client();
 const Discord = require("discord.js"); 
+const client = new Discord.Client();
 
 client.on("guildMemberRemove", (member) => {
 	db.delete(`money_${member.guild.id}_${member.id}`); 
