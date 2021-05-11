6d1cafbcb4bd11bb90a542b98a77d7000865a493
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 14:45:26 -0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -5,7 +5,7 @@ module.exports = (client, message, guild) => {
 
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
     client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${client.guilds.cache.size}`).catch((e) => console.log((e)))
-    console.log("Fui removido de uma guild! | ${client.guilds.cache.size} | ${client.users.cache.size}")
+    console.log(`Fui removido de uma guild! | ${client.guilds.cache.size} | ${client.users.cache.size}`)
     let embed = new Discord.MessageEmbed()
     .setTitle(`<:info:835206734225473546> | Fui removido de uma guild!`)
     .setTimestamp()
