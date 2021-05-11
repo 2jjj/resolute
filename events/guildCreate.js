0e11f86b5f6e646a0bec0cc315f1c3ca0af9bdf9
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 14:33:05 -0300

    Events

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,17 +1,16 @@
 const Discord = require("discord.js")
 
-module.exports = (client, message) => {
+module.exports = (client, guild, message, guilds, id) => {
 
 //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
-client.on("guildCreate", async (guild) => {
-    console.log("+1")
+    console.log(`Fui adicionado em um servidor! | ${client.guilds.cache.size} | ${client.users.cache.size}`)
     const dono = client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
     let embed = new Discord.MessageEmbed()
-    .setTitle(`+1 server :sunglasses:`)
-    .setDescription(`Servidor: ${guild.name}\nID: ${guild.id}\nMembros: ${guild.memberCount} membros\nDono: ${dono}`)
+    .setTitle(`<a:__:835643988022263879> Fui adicionado em um servidor!`)
+    .setDescription(`<:info:835206734225473546> | Servidor: ${guild.name}\n<:info:835206734225473546> | ID: ${guild.id}\n<:info:835206734225473546> | Membros: ${guild.memberCount} membros\n<:info:835206734225473546> | Dono: ${dono}`)
     .setThumbnail(client.user.displayAvatarURL())
     .setTimestamp()
     .setColor('#FFC4E7')
     webhook.send(embed);
-})}
+}
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,12 +1,11 @@
 const db = require("quick.db");
-module.exports = (client, message) => {
+const Discord = require("discord.js")
 
-client.on("guildMemberRemove", (member) => {
+module.exports = (client, message, member) => {
 	db.delete(`money_${member.guild.id}_${member.id}`); 
 	db.delete(`bank_${member.guild.id}_${member.id}`); 
 	db.delete(`work_${member.guild.id}_${member.id}`); 
 	db.delete(`daily_${member.guild.id}_${member.id}`); 
 	db.delete(`rob_${member.guild.id}_${member.id}`); 
 	console.log(`[ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
-})
 }
