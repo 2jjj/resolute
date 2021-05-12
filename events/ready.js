6f87a169be23d0486203bcff8e8c300e81ddb6d2
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-12 10:27:58 -0300

    Update

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const m = require('moment');
+const colors = require('colors');
+const config = require('../config.json');
+
+module.exports = (client, shard) => {
+
+client.on('ready', async () => {
+    console.log("on")
+})
+}
