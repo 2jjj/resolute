a9e0d04966cea6b10c16e971f59aed9fffd300b5
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-13 13:48:57 -0300

    ta

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
-module.exports = (client, message, guild) => {
-//https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
+module.exports = async (client, guild) => {
+    //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
     client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${client.guilds.cache.size}`).catch((e) => console.log((e)))
