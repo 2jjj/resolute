fdf7f6cd899ceaaae95fbdbdc1d9f1bc33a62d66
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-13 13:48:44 -0300

    ta

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 
-module.exports = (client, guild, message, guilds, id) => {
+module.exports = async (client, guild) => {
 
 //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
