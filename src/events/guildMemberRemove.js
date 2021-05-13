a36d59b16405b126b1f38890389277a5edf11977
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-13 13:49:41 -0300

    ta

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,7 +1,8 @@
 const db = require("quick.db");
 const Discord = require("discord.js")
 
-module.exports = (client, member) => {
+module.exports = async (client, member) => {
+
 	db.delete(`money_${member.guild.id}_${member.id}`); 
 	db.delete(`bank_${member.guild.id}_${member.id}`); 
 	db.delete(`work_${member.guild.id}_${member.id}`); 
