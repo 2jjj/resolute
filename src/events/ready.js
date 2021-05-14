e3219d605903cd1d45a9eec5d9923ca2affaf141
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-14 10:50:19 -0300

    V 1.3

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,9 +3,12 @@ const Discord = require("discord.js")
 module.exports = async (client) => {
 
     const status = [  
-        {name: `👾 resolutebot.xyz | SHARD 0`, type: 'LISTENING'}, 
-        {name: `❤️ Novo comando -> s.setprefix | SHARD 0`, type: 'LISTENING'}, 
-        {name: `🔫 Versão 1.3(Beta) | SHARD 0`, type: 'LISTENING'}
+        {name: `👾 resolutebot.xyz`, type: 'LISTENING'}, 
+        {name: `👾 Slash commands em breve!`, type: 'LISTENING'}, 
+        {name: `❤️ ${client.guilds.cache.size} Servidores `, type: 'LISTENING'}, 
+        {name: `❤️ ${client.channels.cache.size} Canais `, type: 'LISTENING'},
+        {name: `❤️ ${client.users.cache.size} Usuários `, type: 'LISTENING'},
+        {name: `🔫 Versão 1.3(Beta)`, type: 'LISTENING'}
       ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
