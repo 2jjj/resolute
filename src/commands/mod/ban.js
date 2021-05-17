e9a959bd277dc25d34af606835f2cdf3842214cb
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-16 22:03:37 -0300

    Update ban.js

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -1,28 +1,67 @@
-const Discord = require ("discord.js")
-const db = require("quick.db");
+const Discord = require("discord.js");
+const db = require("quick.db")
 
-exports.run = (bot,message,args) => {
+exports.run = (client, message, args) => {
 
   let prefix = db.get(`prefix_${message.guild.id}`)
   if (prefix === null) prefix = "s."
 
-  
-  if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("<:info:835206734225473546> » Você não tem permissão para executar o comando.");
-  if(args.lenght === 0) return message.reply("<:info:835206734225473546> » Use **s.ban <@Pessoa> <Motivo>** para banir alguém.");
-  let banMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-  if(!banMember) return message.reply("<:info:835206734225473546> » Use **s.ban <@Pessoa> <Motivo>** para banir alguém.");
-  let banReason = args.join(" ").slice(22) || args.slice(1).join(" ");
-  if(!banReason){
-    banReason = "<:info:835206734225473546> » O motivo não foi especificado."
-  }
-  
-  try {
-    banMember.ban({reason: banReason})
-    message.channel.send(`:hammer: ${banMember} **foi** \`Banido\` **pelo** \`Motivo:\`**${banReason}**`);
-  } catch (error) {
-    message.reply(`${error}`)
-  }
+      var list = [
+        'https://imgur.com/ZNuAcum.gif',
+        'https://imgur.com/xlD7P3N.gif',
+        'https://imgur.com/cT6TUwv.gif',
+        'https://imgur.com/7l7n5un.gif',
+        'https://imgur.com/NYZsPRx.gif',
+        'https://imgur.com/gVAiCX6.gif',
+        'https://imgur.com/usOD4UR.gif',
+        'https://imgur.com/4uDadjQ.gif'
+      ]
+    
+      var rand = list[Math.floor(Math.random() * list.length)]
 
-message.channel.send(`<:info:835206734225473546> » Você foi banido do servidor ${message.guild.name} pelo motivo: ${banReason}`)
-}
+        message.delete();
 
+        if(!args.length) {
+            const help = new Discord.MessageEmbed()
+            .setTitle("Comando de ban")
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+            .setDescription("Execute um ban em algum usuário específico utilizando este comando!")
+            .addField(`<:information:843590035848429579> Forma de Utilização:`, `<:y_pontinho:843648515695444019> \`${prefix}ban @usuario <motivo>\``)
+            .addField(`<:information:843590035848429579> Exemplo:`, `<:y_pontinho:843648515695444019> \`${prefix}ban @Spray#0007 Ofensa a staff\``)
+            .setFooter(`Comando executado por: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
+            .setImage(rand)
+            .setTimestamp();
+            return message.channel.send(help);
+        }
+
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send({embed: {color: "#ff0000", description: "<a:erradogst:842084346970112041> Você não tem permissão para utilizar este comando!"}})
+
+        const usuario = message.mentions.members.first()
+    
+        const motivo = args.slice(1).join(" ");
+
+        if(!usuario.bannable) return message.channel.send({embed: {color: "#ff0000", description: "<a:erradogst:842084346970112041> Eu não tenho permissão para banir este usuário!"}})
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Novo Banimento!")
+        .setColor("#ff0000")
+        .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+        .addField(`<:information:843590035848429579> Usuário:`, `${usuario} (${usuario.id})`)
+        .addField(`<:y_pontinho:843648515695444019> Autor:`, `${message.author} (${message.author.id})`)
+        .addField(`<:poker_bang:843648860651913268> Motivo:`, `${motivo}`)
+        .setFooter("Resolute - Punições", message.author.displayAvatarURL())
+        .setTimestamp();
+        message.channel.send(embed);
+
+        const pv = new Discord.MessageEmbed()
+        .setTitle("Você foi banido!")
+        .setColor("#ff0000")
+        .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+        .addField("<:staff:843586666845044736> Autor:", `${message.author} (${message.author.id})`)
+        .addField("<:info:835206734225473546> Motivo:", `${motivo}`)
+        .setImage(rand)
+        .setFooter("Resolute - Punições", message.guild.iconURL({dynamic: true}))
+        .setTimestamp();
+        usuario.send(pv);
+        usuario.ban();
+    }
