d2de0f6d5dea7814a1634dd1d56255878a90a6bf
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-17 13:16:32 -0300

    Update

--- index.js
+++ index.js
@@ -86,67 +86,6 @@ console.error('Erro:' + err);
 });
    
 
-//inlinereply
-class Message extends Structures.get("Message") {
-  async inlineReply(content, options) {
-      const mentionRepliedUser = typeof ((options || content || {}).allowedMentions || {}).repliedUser === "undefined" ? true : ((options || content).allowedMentions).repliedUser;
-      delete ((options || content || {}).allowedMentions || {}).repliedUser;
-
-      const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options).resolveData();
-      Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
-  
-      if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0)
-          apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
-      if (typeof apiMessage.data.allowed_mentions.replied_user === "undefined")
-          Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
-
-      if (Array.isArray(apiMessage.data.content)) {
-          return Promise.all(apiMessage.split().map(x => {
-              x.data.allowed_mentions = apiMessage.data.allowed_mentions;
-              return x;
-          }).map(this.inlineReply.bind(this)));
-      }
-
-      const { data, files } = await apiMessage.resolveFiles();
-      return this.client.api.channels[this.channel.id].messages
-          .post({ data, files })
-          .then(d => this.client.actions.MessageCreate.handle(d).message);
-  }
-}
-Structures.extend("Message", () => Message)
-
-
-//quote message
-Message.prototype.quote = async function (content, options) {
-  const reference = {
-    message_id: (
-      !!content && !options
-        ? typeof content === 'object' && content.messageID
-        : options && options.messageID
-    ) || this.id,
-    message_channel: this.channel.id
-  }
-
-  const { data: parsed, files } = await APIMessage
-    .create(this, content, options)
-    .resolveData()
-    .resolveFiles()
-
-  let msg = await this.client.api.channels[this.channel.id].messages.post({
-    data: { ...parsed, message_reference: reference },
-    files
-  })
-
-  await this.channel.messages.fetch(msg.id)
-            .then(message => msg = message)
-            .catch((err) => {
-                console.log(err.stack)
-            });
-
-  return msg
-}
-
-
 //Carregando eventos
 fs.readdir(__dirname + "/src/events/", (err, files) => {
   if (err) return console.error(err);
