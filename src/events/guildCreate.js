59bde46f69f62b9169138395356dd25517558c8b
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-18 17:17:40 -0300

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -7,7 +7,7 @@ const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNb
     console.log(`Fui adicionado em um servidor! | ${client.guilds.cache.size} | ${client.users.cache.size}`)
     client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${client.guilds.cache.size}`).catch((e) => console.log((e)))
     client.channels.cache.get("841733238165995570").setName(`📝│Pessoas: ${client.users.cache.size}`).catch((e) => console.log((e)))
-    const dono = client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
+    const dono = await client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
     let embed = new Discord.MessageEmbed()
     .setTitle(`<a:__:835643988022263879> Fui adicionado em um servidor!`)
     .setDescription(`<:info:835206734225473546> | Servidor: ${guild.name}\n<:info:835206734225473546> | ID: ${guild.id}\n<:info:835206734225473546> | Membros: ${guild.memberCount} membros\n<:info:835206734225473546> | Dono: ${dono}`)
