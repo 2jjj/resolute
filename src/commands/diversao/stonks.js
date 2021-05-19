26375ade79151b3e0d9b4c2c727c1cfa2f4f6727
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 18:01:24 -0300

    Canary

--- src/commands/diversao/stonks.js
+++ src/commands/diversao/stonks.js
@@ -1,21 +1,35 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js")
 const db = require("quick.db");
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message, args, idioma) =>  {
 
-        let prefix = db.get(`prefix_${message.guild.id}`)
-        if (prefix === null) prefix = "s."
+      const { createCanvas, loadImage } = require('canvas');
+      if((args.join(" ").length) > 300) return message.quote(idioma.image.long.replace("%u", message.author))
 
-        
-        var list = [
-                'https://imgur.com/jVL0mbR.gif',
-                'https://cdn.dicionariopopular.com/imagens/image-347.jpg'
-        ]
+      if(!args[0]) return message.quote(`${idioma.image.args.replace("%u", message.author)}`)
 
-        var rand = list[Math.floor(Math.random() * list.length)]
+      message.quote(`${idioma.image.editando.replace("%u", message.author)}`).then(async m => {
+        message.channel.startTyping()
+          const canvas = createCanvas(685, 494); 
+          const ctx = canvas.getContext('2d');
+          
+          const DIG = require("discord-image-generation");
+          let img = await new DIG.Stonk().getImage(message.author.displayAvatarURL({ dynamic: false, format: 'png', size: 4096 }))
 
-        var embed = new Discord.MessageEmbed()
-                .setColor('BLUE')
-                .setImage(rand)
-        await message.inlineReply(embed)
-}
+          //const im = `https://vacefron.nl/api/stonks?user=${img}`
+
+          const background = await loadImage(img)//await loadImage('https://i.imgur.com/AUzBCnR.png');
+          ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+          
+          ctx.font = '30px sans-serif';
+          ctx.fillStyle = '#FFFFFF';
+          ctx.fillText(`${args.join(" ")}`.match(/.{1,50}/g).join("\n"), canvas.width / 50.9, canvas.height / 15.9, 655);
+          
+          const attachment = new (require("discord.js")).MessageAttachment(canvas.toBuffer(), `stonks-${message.author.id}.png`);
+       
+          await message.quote(`${message.author}`, attachment).then(m2 => {
+            message.channel.stopTyping()
+              m.delete()   
+          })
+      })
+    }
