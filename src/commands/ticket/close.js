0f4a018c26c8dc2ff6d252a741dfaf31c7d6be32
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 12:08:27 -0300

    Update close.js

--- src/commands/ticket/close.js
+++ src/commands/ticket/close.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
         
         message.delete();
 
-        if(message.channel.name !== `${message.author.id}`) return message.channel.send(`:x: | ${message.author} Você pode utilizar este comando apenas para fechar o seu ticket!`).then(msg => msg.delete({timeout:15000}));
+        if(message.channel.name !== `${message.author.id}`) return message.channel.send(`<:check_no:844591218545524788> | ${message.author} Você pode utilizar este comando apenas para fechar o seu ticket!`).then(msg => msg.delete({timeout:15000}));
         
         message.channel.delete()
     }
