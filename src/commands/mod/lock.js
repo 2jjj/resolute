3ea3ea8c4bf66ac2a664509dd4e83b95f4e3d7e3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 15:21:22 -0300

    Update lock.js

--- src/commands/mod/lock.js
+++ src/commands/mod/lock.js
@@ -3,10 +3,34 @@ const db = require("quick.db")
 
 module.exports.run = async(client,message,args)=> {
 
+    const embed1 = new Discord.MessageEmbed()
+    .setTitle("Sem permissão.")
+    .setColor("#ff0000")
+    .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+    .addField(`Você não possui a permissão de`, `GERENCIAR CANAIS`)
+    .setFooter("Resolute - By MrSprayX#0012", message.author.displayAvatarURL())
+    .setTimestamp();
+
+    const embed2 = new Discord.MessageEmbed()
+    .setTitle("Este canal já está bloqueado.")
+    .setColor("#ff0000")
+    .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+    .setFooter("Resolute - By MrSprayX#0012", message.author.displayAvatarURL())
+    .setTimestamp();
+
+    const embed3 = new Discord.MessageEmbed()
+    .setTitle("Este canal foi bloqueado.")
+    .setColor("#ff0000")
+    .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+    .addField(`<:bloqueado:844640110203895829> » Este canal foi bloqueado.`, `Foi bloqueado por ${message.author}`)
+    .setFooter("Resolute - By MrSprayX#0012", message.author.displayAvatarURL())
+    .setTimestamp();
+
+
     let prefix = db.get(`prefix_${message.guild.id}`)
     if (prefix === null) prefix = "s."
 
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("<:2637settings:843854352867262504> | Você não possui permissões para usar este comando | `MANAGE_CHANNELS`");
+    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply(embed1);
 
     if(db.fetch(`lock.${message.channel.id}`)) return message.reply("<:7300lock:843854352653484084> | Este canal já está bloqueado.")
 
