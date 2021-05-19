868154577b56c4a3dea093aa75ceec8fdfd713db
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 12:10:25 -0300

    Update votar.js

--- src/commands/outros/votar.js
+++ src/commands/outros/votar.js
@@ -9,11 +9,11 @@ exports.run = async (client, message, args) => {
 
     var content = args.join(' ')
 
-    if (content.length > 600) { return message.inlineReply('<:4693_pink_hair_popcorn:843542215708114994> O conteúdo a ser votado não pode passar de **600 caracteres.**') }
+    if (content.length > 600) { return message.inlineReply('<:Information:844591555225714688> O conteúdo a ser votado não pode passar de **600 caracteres.**') }
 
     var embed = new Discord.MessageEmbed()
         .setColor('BLUE')
-        .setTitle(`<:4693_pink_hair_popcorn:843542215708114994> Votação aberta por ${message.author.username}`)
+        .setTitle(`<:Time:844591047719125012> Votação aberta por ${message.author.username}`)
         .setDescription(content)
 
     if (!content) { return message.inlineReply('`' + prefix + 'votar o que você quer que seja votado.`') }
