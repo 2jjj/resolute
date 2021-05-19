ab0f89cf2e77b10989a9dd6f20ab69a63c979292
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 12:33:39 -0300

    Update steam.js

--- src/commands/outros/steam.js
+++ src/commands/outros/steam.js
@@ -10,14 +10,14 @@ exports.run = (bot, message, args) => {
     if (prefix === null) prefix = "s."
  
 let arg = args.join(' ') //Puxa os argumentos do  usuário
-if(!arg) return message.channel.send(`${message.author}, você precisa colocar um jogo!`) //retorna quando o usuário não coloca um jogo
+if(!arg) return message.channel.send(`<:y_pontinho:843648515695444019> ${message.author}, Você precisa colocar um jogo!`) //retorna quando o usuário não coloca um jogo
 provider.search(arg).then(result => { //vai mostrar o resultado
     provider.detail(result[0].id, 1, "portuguese", "pt").then(results => { //tenta mostrar o resultado em Português (Brasil)
         let other = results.otherData //vai pegar os dados do jogo
         const embed = new Discord.MessageEmbed() //vai mostrar para o usuário todas as informações do jogo
                     .setTitle(results.name)
                     .setColor('RANDOM')
-                    .setDescription(`\n\n**__GÊNERO__**: ${results.genres.join(', ')} \n**__PLATAFORMA__**: ${other.platforms.join(', ')}\n**__CARACTERÍSTICAS__**: ${other.features.join(', ')}\n\n**__DEVELOPER__**: ${other.developer.join(', ')}`)
+                    .setDescription(`\n\n<:pxdro:844591434191732798> Gênero: ${results.genres.join(', ')} \n<:pxdro:844591434191732798> Plataforma: ${other.platforms.join(', ')}\n<:pxdro:844591434191732798> Características: ${other.features.join(', ')}\n\n<:pxdro:844591434191732798> Desenvolvedor: ${other.developer.join(', ')}`)
                     .setThumbnail(other.imageUrl)
                     .setFooter(message.author.tag, message.author.displayAvatarURL(),message.author.displayAvatarURL)
         return message.channel.send(embed)
