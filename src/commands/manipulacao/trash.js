663c677705cb87ebe46f2e7657e19f4f8a1d8c4a
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 21:39:25 -0300

    Update trash.js

--- src/commands/manipulacao/trash.js
+++ src/commands/manipulacao/trash.js
@@ -3,18 +3,16 @@ const Discord = require("discord.js");
 const db = require("quick.db")
 
 exports.run = async (bot, message, args) => {
-    
+
 let prefix = db.get(`prefix_${message.guild.id}`)
 if (prefix === null) prefix = "s."
 
- let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
- let avatar = user.user.displayAvatarURL({
-      dynamic: false,
-      format: "png",
-    });
+    let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+
+    let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
 
     let img = new DIG.Trash().getImage(avatar);
 
-    let attach = new Discord.MessageAttachment(img, "delete.png");
+    let attach = new Discord.MessageAttachment(img, "trash.png");
     message.channel.send(attach);
   }
