b985b7d0e0cc10f1942f63a3376dc27f250e9101
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 21:36:56 -0300

    Update trash.js

--- src/commands/manipulacao/trash.js
+++ src/commands/manipulacao/trash.js
@@ -3,7 +3,10 @@ const Discord = require("discord.js");
 const db = require("quick.db")
 
 exports.run = async (bot, message, args) => {
-   
+    
+let prefix = db.get(`prefix_${message.guild.id}`)
+if (prefix === null) prefix = "s."
+
  let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
  let avatar = user.user.displayAvatarURL({
       dynamic: false,
