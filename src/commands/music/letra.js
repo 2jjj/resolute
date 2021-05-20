93710379f075be30743477d2044afdce95667dcb
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-20 17:44:47 -0300

    Create letra.js

---
+++ src/commands/music/letra.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const AssistaAoVideo = require("AssistaAoVideo");
+
+exports.run = async(client, message, args) => {
+
+    const queue = message.client.queue.get(message.guild.id);
+    if (!queue) return message.channel.send("Não tem nada tocando no momento").catch(console.error);
+
+    let lyrics = null;
+
+    try {
+      lyrics = await lyricsFinder(queue.songs[0].title, "");
+      if (!lyrics) lyrics = `Não encontrei letras para essa ${queue.songs[0].title} :(`;
+    } catch (error) {
+      lyrics = `Não encontrei letras para essa ${queue.songs[0].title} :(`;
+    }
+
+    let lyricsEmbed = new MessageEmbed()
+      .setTitle(`Letras da **__${queue.songs[0].title}__**`)
+      .setDescription(lyrics)
+      .setColor("GREEN")
+      .setTimestamp();
+
+    if (lyricsEmbed.description.length >= 2048)
+      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+    return message.channel.send(lyricsEmbed).catch(console.error);
+}
