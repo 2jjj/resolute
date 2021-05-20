ce7ded8d90322213844462542c8ff4a703b30dd8
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-20 19:36:10 -0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,59 @@
 const Discord = require("discord.js")
 
 module.exports = async (client) => {
+    client.api.applications(client.user.id).guilds('699722023508770836').commands.post({
+        data: {
+            name: "hello",
+            description: "Replies with Hello World!"
+        }
+    });
 
+    client.api.applications(client.user.id).guilds('699722023508770836').commands.post({
+        data: {
+            name: "echo",
+            description: "Echos your text as an embed!",
+
+            options: [
+                {
+                    name: "content",
+                    description: "Content of the embed",
+                    type: 3,
+                    required: true
+                }
+            ]
+        }
+    });
+
+    client.ws.on('INTERACTION_CREATE', async interaction => {
+        const command = interaction.data.name.toLowerCase();
+        const args = interaction.data.options;
+
+        if(command == 'hello') {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: "Hello World!"
+                    }
+                }
+            });
+        }
+
+        if(command == "echo") {
+            const description = args.find(arg => arg.name.toLowerCase() == "content").value;
+            const embed = new discord.MessageEmbed()
+                .setTitle("Echo!")
+                .setDescription(description)
+                .setAuthor(interaction.member.user.username);
+
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: await createAPIMessage(interaction, embed)
+                }
+            });
+        }
+    });
     const status = [  
         {name: `• Dev: MrSprayX#0012`, type: 'LISTENING'}, 
         {name: `• resolutebot.xyz`, type: 'LISTENING'}, 
