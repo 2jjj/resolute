4a1b7326adc489c36777865b1b69584be801db5c
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-19 21:44:24 -0300

    Update trash.js

--- src/commands/manipulacao/trash.js
+++ src/commands/manipulacao/trash.js
@@ -9,7 +9,6 @@ exports.run = async (bot, message, args) => {
  //   const m = client.findMember(message, args, true);
    
     let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
-    let m = await message.channel.send("<a:carregando:843662352549543936>");   
     let avatar = user.user.displayAvatarURL({
       dynamic: false,
       format: "png",
@@ -18,6 +17,5 @@ exports.run = async (bot, message, args) => {
     let img = await new DIG.Trash().getImage(avatar);
 
     let attach = new Discord.MessageAttachment(img, "delete.png");
-    m.delete({ timeout: 5000 });
     message.channel.send(attach);
   }
