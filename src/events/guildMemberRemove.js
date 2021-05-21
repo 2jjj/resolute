237d69ea91d2b7bc14b74e885f37e13155afc852
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-21 15:37:40 -0300

    💻 Versão 0.0.5

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -8,5 +8,5 @@ module.exports = async (client, member) => {
 	db.delete(`work_${member.guild.id}_${member.id}`); 
 	db.delete(`daily_${member.guild.id}_${member.id}`); 
 	db.delete(`rob_${member.guild.id}_${member.id}`); 
-	console.log(`[ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
+	console.log(`[LOGS] - [ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
 }
