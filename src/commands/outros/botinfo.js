0138d78e53d55d208f3e81365587813907cf4914
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 18:55:51 -0300

    Update botinfo.js

--- src/commands/outros/botinfo.js
+++ src/commands/outros/botinfo.js
@@ -10,18 +10,19 @@ exports.run = async (client, message, args) => {
   let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
 
   let { version } = require("discord.js");
+  var numWorkers = require('os').cpus().length;
 
   let embed = new Discord.MessageEmbed()
   .setTitle(" *Informações*")
   .setColor("RANDOM")
   .setThumbnail(avatar)
 	.addFields(
-    { name: 'ﾠ', value: '<:info:845781463843602454> | Shards: <@836345581424738354>, <@798574984582791208>' },
-		{ name: 'ﾠ', value: '<:developer:843586491434401792> | Desenvolvedor: <@836345581424738354>, <@798574984582791208>' },
-		{ name: 'ﾠ', value: `<a:BP_alerta_gif:841129076672299108> | Bot criado no dia: **11/10/2020**`, inline: false },
-		{ name: 'ﾠ', value: `<:Resoluteinfo:844971535927083088> | Estou em ${client.guilds.cache.size} servidores!, com ${client.users.cache.size} pessoas!`, inline: false },
-    { name: 'ﾠ', value: `<:3434_Discord_js_logo:845779460899733553> | Versão discord.js: **${version}**`, inline: false },
-    { name: 'ﾠ', value: `<:node:845780252940959744> | Versão Node: **${process.version}**`, inline: false },
+    { name: 'ﾠ', value: `<:info:845781463843602454> | **Shards**: ${numWorkers}` },
+		{ name: 'ﾠ', value: '<:developer:843586491434401792> | **Desenvolvedor:** <@836345581424738354>, <@798574984582791208>' },
+		{ name: 'ﾠ', value: `<a:BP_alerta_gif:841129076672299108> | **Bot criado no dia:** **11/10/2020**`, inline: false },
+		{ name: 'ﾠ', value: `<:Resoluteinfo:844971535927083088> | **Estou em ${client.guilds.cache.size} servidores!**, **com ${client.users.cache.size} pessoas!**`, inline: false },
+    { name: 'ﾠ', value: `<:3434_Discord_js_logo:845779460899733553> | **Versão discord.js:** **${version}**`, inline: false },
+    { name: 'ﾠ', value: `<:node:845780252940959744> | **Versão Node:** **${process.version}**`, inline: false },
     { name: 'ﾠ', value: `<:N3UP_Servers:845357935155478548> | Estou na versão **0.0.5**`, inline: false },
 		{ name: 'ﾠ', value: `<:tj_add_1:845780583165722625> | [Me adicione!](https://resolutebot.xyz)`, inline: false },
 	)
