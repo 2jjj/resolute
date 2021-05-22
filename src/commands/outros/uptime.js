5691b5b29aad0e1d43252a2386ff449817a542c5
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 18:28:18 -0300

    Update uptime.js

--- src/commands/outros/uptime.js
+++ src/commands/outros/uptime.js
@@ -18,7 +18,7 @@ module.exports.run = async (client, message, args) => {
 
   const embed = new Discord.MessageEmbed()
     .setTitle(`<:Resoluteinfo:844971535927083088> Tempo de atividade`)
-    .setThumbnail("https://imgur.com/WZMylbw.gif")
+    //.setThumbnail("https://imgur.com/WZMylbw.gif")
     .setColor("#FF0000")
     .setDescription(`**<:N3UP_Servers:845357935155478548> Estou online a **\n${uptime}`)
 
