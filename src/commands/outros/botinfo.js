f09ae026ae2333fc5c450d5ecc3a667986e6931c
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 18:51:51 -0300

    Update botinfo.js

--- src/commands/outros/botinfo.js
+++ src/commands/outros/botinfo.js
@@ -22,7 +22,7 @@ exports.run = async (client, message, args) => {
     { name: 'ﾠ', value: `<:3434_Discord_js_logo:845779460899733553> | Versão discord.js: **${version}**`, inline: false },
     { name: 'ﾠ', value: `<:node:845780252940959744> | Versão Node: **${process.version}**`, inline: false },
     { name: 'ﾠ', value: `<:N3UP_Servers:845357935155478548> | Estou na versão **0.0.5**`, inline: false },
-		{ name: 'ﾠ', value: `<:tj_add_1:845780583165722625> | [Me adicione](https://resolutebot.xyz)`, inline: false },
+		{ name: 'ﾠ', value: `<:tj_add_1:845780583165722625> | [Me adicione!](https://resolutebot.xyz)`, inline: false },
 	)
   await message.channel.send(embed);
 }
