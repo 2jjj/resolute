5ca9d282bc8f64e2fdbafb46ffa454838919e7b2
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 14:00:34 -0300

    update

--- src/events/message.js
+++ src/events/message.js
@@ -7,5 +7,5 @@ module.exports = async (client, message) => {
     let prefix = db.get(`prefix_${message.guild.id}`)
     if (prefix === null) { prefix = "s." }
 
-    if (message.mentions.has(client.user.id)) { return message.inlineReply(`<:dy_girlHello:841125764690739203> Olá! ${message.author}\n > Meu prefixo é \`s.\`, use \`s.ajuda\` para ajuda!\n > Me adicione -> **https://resolutebot.xyz**`).then(msg => msg.delete({ timeout: 5000 })).catch(err => { return }) }
+    if (message.mentions.has(client.user.id)) { return message.inlineReply(`<:dy_girlHello:841125764690739203> Olá! ${message.author}\n > Meu prefixo é \`s.\`, use \`s.ajuda\` para ajuda!\n > Me adicione -> **https://resolutebot.xyz**`).then(msg => msg.delete({ timeout: 15000 })).catch(err => { return }) }
 }
