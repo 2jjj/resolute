1058e389178f2e5c6c77592986ed8ea1b0370861
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 14:00:15 -0300

    Create message.js

---
+++ src/events/message.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+require("../../inlineReply")
+const db = require("quick.db")
+
+module.exports = async (client, message) => {
+
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) { prefix = "s." }
+
+    if (message.mentions.has(client.user.id)) { return message.inlineReply(`<:dy_girlHello:841125764690739203> Olá! ${message.author}\n > Meu prefixo é \`s.\`, use \`s.ajuda\` para ajuda!\n > Me adicione -> **https://resolutebot.xyz**`).then(msg => msg.delete({ timeout: 5000 })).catch(err => { return }) }
+}
