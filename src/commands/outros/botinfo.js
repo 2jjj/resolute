97bab583b79e9ffa0e987450311d925955fc9e30
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 18:45:09 -0300

    Update botinfo.js

--- src/commands/outros/botinfo.js
+++ src/commands/outros/botinfo.js
@@ -9,6 +9,7 @@ exports.run = async (client, message, args) => {
   let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
   let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
 
+  let { version } = require("discord.js");
 
   let embed = new Discord.MessageEmbed()
   .setTitle(" *Informações*")
