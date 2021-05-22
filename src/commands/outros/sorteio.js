e7e73588ac4a7cad8bb48d871cb6a6b55e5ccd1b
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-22 18:19:59 -0300

    Update sorteio.js

--- src/commands/outros/sorteio.js
+++ src/commands/outros/sorteio.js
@@ -25,7 +25,7 @@ module.exports = {
  
  let prize = args.slice(2).join(" ");
  
- if (!prize) return message.channel.send(`<:setaazul:843588568605523969> Você precisa escrever o prêmio!`);
+ if (!prize) return message.channel.send(`<:spr4yxyz:837798446584168468> Você precisa escrever o prêmio!`);
  
  message.channel.send(`*<:setaazul:843588568605523969> Sorteio criado em ${channel}*`);
  
