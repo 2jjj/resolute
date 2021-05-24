b54e9f616775da8b96eb1db0d9a205f36b32f564
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-24 19:39:26 -0300

    Create conquista.js

---
+++ src/commands/diversao/conquista.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports = {
+name: 'conquista',
+run: async(client, message, args) => {
+
+const a = args.join("+")
+if(!a) return message.channel.send(`**Por favor diga algo para colocar na conquista!**`);
+if(args.join("+").length > 20) return message.channel.send(`**Use no maximo 20 caracteres!**`);
+
+const embed_conqu = new Discord.MessageEmbed()
+.setColor("RANDOM")
+.setImage(`https://minecraftskinstealer.com/achievement/${Math.floor(Math.random() * 39) + 1}/Achievement+Get%21/${a}`);
+
+message.channel.send(`${message.author}`,embed_conqu)
+}
+
+}
