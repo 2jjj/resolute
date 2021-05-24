dabea92817115d8faed3685f9e28925726a35440
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-23 22:06:27 -0300

    Update volume.js

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -15,12 +15,12 @@ module.exports = {
     if (prefix === null) prefix = "s."
 
     const channel = message.member.voice.channel;
-    if (!channel)return sendError("Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
+    if (!channel)return sendError("<:Resoluteinfo:844971535927083088> Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("<:information:843590035848429579> | Não ha nada tocando.", message.channel);
-    if (!args[0])return message.channel.send(`<:volume:845039532712656916> O volume atual é: **${serverQueue.volume}**`);
-     if(isNaN(args[0])) return message.channel.send('<:information:843542771814236170> Somente números!').catch(err => console.log(err));
-    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('<:information:843590035848429579> | Você não pode definir o volume para mais de 150 ou menor que zero. <:bravinha:841126251741970452>',message.channel).catch(err => console.log(err));
+    if (!serverQueue) return sendError("<:Resoluteinfo:844971535927083088> Não ha nada tocando.", message.channel);
+    if (!args[0])return message.channel.send(`<:Resoluteinfo:844971535927083088> O volume atual é: **${serverQueue.volume}**`);
+     if(isNaN(args[0])) return message.channel.send('<:Resoluteinfo:844971535927083088> Somente números!').catch(err => console.log(err));
+    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('<:Resoluteinfo:844971535927083088> Você não pode definir o volume para mais de 150 ou menor que zero. <:bravinha:841126251741970452>',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let spray = new MessageEmbed()
