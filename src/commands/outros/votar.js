f5ab140381c0689baba096207810aa6d682dded8
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-24 00:35:35 -0300

    Update votar.js

--- src/commands/outros/votar.js
+++ src/commands/outros/votar.js
@@ -16,7 +16,7 @@ exports.run = async (client, message, args) => {
         .setTitle(`<:Time:844591047719125012> Votação aberta por ${message.author.username}`)
         .setDescription(content)
 
-    if (!content) { return message.inlineReply('Você está fazendo errado, maneira correta <:spr4yxyz:837798446584168468> `' + prefix + 'votar <conteúdo>`') }
+    if (!content) { return message.inlineReply(`<:1icon_x:846184439403118624> | **Maneira correta <:spr4yxyz:837798446584168468> ${prefix}votar <conteudo>**`) }
 
     if (content) {
         return message.channel.send(embed).then(msg => {
