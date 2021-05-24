c21b1424069382d4432bcd7ed377d14f24bfdce8
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-23 22:05:31 -0300

    Update skip.js

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -4,11 +4,11 @@ exports.run = async(client, message) => {
     if (prefix === null) prefix = "s."
 
     const channel = message.member.voice.channel;
-    if (!channel) return message.channel.send('<:music:843602147051700284> | Você deve estar em um canal de voz para utlizar esse comando!');
+    if (!channel) return message.channel.send('<:music:843602147051700284> Você deve estar em um canal de voz para utlizar esse comando!');
     let queue = message.client.queue.get(message.guild.id)
     if(!queue){ return message.channel.send({
         embed: {
-            description: '<:music:843602147051700284> | Nada está tocando no momento para eu poder pular!',
+            description: '<:music:843602147051700284> Nada está tocando no momento para eu poder pular!',
             color: 'BLACK'
         }
     })
@@ -16,6 +16,6 @@ exports.run = async(client, message) => {
 
     if(queue.songs.length !== 0) {
         message.react('<a:check_ravena:843602746543439902>')
-        queue.connection.dispatcher.end('A música foi pulada!')
+        queue.connection.dispatcher.end('A música foi pulada.')
     }
 }
