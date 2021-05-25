22eb5ce3a8dd2ef468e428cdeaea366f30774452
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-25 12:03:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ fs.readdir(__dirname + "/events/", (err, files) => {
     const event = require(__dirname + `/events/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
-    console.log(cor.red("[INFO] - [EVENTO] - "+eventName))
+    console.log(cor.red("[LOGS] - [EVENTO] - "+eventName))
   });
 });
 
