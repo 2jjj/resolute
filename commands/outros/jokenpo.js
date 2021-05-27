8f6ee54f2c8b7ece6b81da06560aa0aa5ca9712b
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-27 11:40:30 -0300

    Create jokenpo.js

---
+++ commands/outros/jokenpo.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js')
+
+
+
+module.exports = {
+    name: "jokenpo",
+    aliases: ['pedrapapeltesoura'],
+    cooldown: 1000 * 2, 
+    description: "horas",
+    category: "outros",
+
+    async run (bot, message, args) {
+        let embed = new Discord.MessageEmbed()
+        .setTitle("pedra, papel, tesoura")
+        .setDescription("Reaja pra joga")
+        .setTimestamp()
+        let msg = await message.channel.send(embed)
+        await msg.react("🗻")
+        await msg.react("✂")
+        await msg.react("📰")
+
+        const filter = (reaction, user) => {
+            return ['🗻', '✂', '📰'].includes(reaction.emoji.name) && user.id === message.author.id;
+        }
+
+        const choices = ['🗻', '✂', '📰']
+        const me = choices[Math.floor(Math.random() * choices.length)]
+        msg.awaitReactions(filter, {max: 1, time: 60000, error: ["time"]}).then(
+            async(collected) => {
+                const reaction = collected.first()
+                let result = new Discord.MessageEmbed()
+                .setTitle("Resultado")
+                .addField("Sua escolha", `${reaction.emoji.name}`)
+                .addField("Escolha de bot", `${me}`)
+                await msg.edit(result)
+
+                if((me === "🗻" && reaction.emoji.name === "✂") ||
+                (me === "✂" && reaction.emoji.name === "📰") ||
+                (me === "📰" && reaction.emoji.name === "🗻")) {
+                    message.reply("você perdeu!");
+                } else if (me === reaction.emoji.name) {
+                    return message.reply("É um empate!");
+                } else {
+                    return message.reply("você ganhou!");
+                }
+            })
+            .catch(collected => {
+                message.reply('O processo foi cancelado, você não respondeu a tempo!');
+            }) 
+
+    }
+}
