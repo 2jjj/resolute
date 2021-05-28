5ef96101cd758446ab3f0b9d5258f5b699d556e4
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-27 21:52:17 -0300

    Update

---
+++ commands/manipulacao/clyde.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+const { MessageAttachment } = require('discord.js')
+const fetch = require('node-fetch')
+module.exports = {
+    name: "clyde",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    description: "Faça nosso querido bot Clyde falar algo!",
+    category: "manipulacao",
+  
+    async run (client, message, args) {
+        const text = args.slice().join(' ');
+		if (!text) {
+			return message.channel.send(
+				' :x: Favor fornecer texto válido.',
+			);
+		}
+
+		const url = `https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`;
+
+		let response;
+		try {
+			response = await fetch(url).then(res => res.json());
+		}
+		catch (e) {
+			return message.channel.send('❎ Ocorreu um erro, por favor tente novamente!');
+		}
+		const attachment = new MessageAttachment(response.message, 'clyde.png');
+		return message.channel.send(attachment);
+  
+    }
+
+}
--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ var numWorkers = require('os').cpus().length;
 
 module.exports = async (client) => {
     const status = [  
-        {name: `• Dev: MrSprayX#0012 | resolutebot.xyz | Shard 0 [${numWorkers}]`, type: 'LISTENING'}, 
+        {name: `• Dev: SprayX#0009 | resolutebot.xyz | Shard 0 [${numWorkers}]`, type: 'LISTENING'}, 
         {name: `• ${client.guilds.cache.size} guilds | Versão 0.0.6 | Shard 0 [${numWorkers}]`, type: 'LISTENING'}
       ] 
       function Presence() { 
