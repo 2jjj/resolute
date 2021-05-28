c7c7295c66538352a342259105a97ccdf5776941
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-28 18:01:16 -0300

    Update

---
+++ commands/manipulacao/comment.js
@@ -0,0 +1,31 @@
+const canvacord = require('canvacord')
+const Discord = require('discord.js');
+const db = require("quick.db");
+
+module.exports = {
+    name: "comment",
+    aliases: [],
+    description: 'Ultiliza seu texto para um comentario do youtube!',
+    category: "fun",
+    cooldown: 1000 * 2, 
+}
+
+module.exports.run = async (client, message, args) => {
+
+	let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) prefix = "s."
+	
+        const comment = args.join('');
+        if(!comment) return message.channel.send(`:x: Dê algo para Comentar!`)
+        try {    
+        let yt = await canvacord.Canvas.youtube({"avatar":message.author.displayAvatarURL({format: "png"}),"username":message.author.username, "content":args.join(" ")})
+        let attachment = new Discord.MessageAttachment(yt, 'comment.png')
+        message.channel.send(attachment)
+    }catch(err) {
+        const embed2 = new Discord.MessageEmbed()
+    .setTitle(`${client.emotes.error} Algo correu mal.\n:x: Nota : Não funcionará se o Utilizador contiver caracteres Indesejados no seu nome de utilizador.`)
+    .setColor("RED")
+    message.channel.send(embed2)
+    }
+
+}
---
+++ commands/manipulacao/tweet.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js')
+const fetch = require('node-fetch')
+const db = require("quick.db");
+
+module.exports = {
+    name: "tweet",
+    aliases: [],
+    description: 'Escreva um tweet',
+    category: "fun",
+    cooldown: 1000 * 2, 
+
+
+async run (client, message, args) {
+
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) prefix = "s."
+  
+
+    fetch(`https://nekobot.xyz/api/imagegen?type=tweet&username=${message.author.username}&text=${args.join(' ')}`)
+            .then((res) => res.json())
+            .then((data) => {
+                let embed = new MessageEmbed()
+                    .setTitle("Tweet!")
+                    .setImage(data.message)
+                    .setTimestamp()
+                message.channel.send(embed)
+            })
+        }}
