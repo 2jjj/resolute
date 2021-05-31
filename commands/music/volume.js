ce041a4f230619feb507a591343a3bce0c8995c0
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-30 21:19:33 -0300

    Deixar mais bonito

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -15,16 +15,16 @@ module.exports = {
     if (prefix === null) prefix = "s."
 
     const channel = message.member.voice.channel;
-    if (!channel)return sendError("<:Resoluteinfo:844971535927083088> Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
+    if (!channel)return sendError("➥ Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("<:Resoluteinfo:844971535927083088> Não ha nada tocando.", message.channel);
-    if (!args[0])return message.channel.send(`<:Resoluteinfo:844971535927083088> O volume atual é: **${serverQueue.volume}**`);
-     if(isNaN(args[0])) return message.channel.send('<:Resoluteinfo:844971535927083088> Somente números!').catch(err => console.log(err));
-    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('<:Resoluteinfo:844971535927083088> Você não pode definir o volume para mais de 150 ou menor que zero. <:bravinha:841126251741970452>',message.channel).catch(err => console.log(err));
+    if (!serverQueue) return sendError("➥ Não ha nada tocando.", message.channel);
+    if (!args[0])return message.channel.send(`🎵 O volume atual é: **${serverQueue.volume}**`);
+     if(isNaN(args[0])) return message.channel.send('🎵 Somente números!').catch(err => console.log(err));
+    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('> Você não pode definir o volume para mais de 150 ou menor que zero. <:bravinha:841126251741970452>',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let spray = new MessageEmbed()
-    .setDescription(`<:Resoluteinfo:844971535927083088> Eu ajustei o volume para: **${args[0]/1}/100**`)
+    .setDescription(`🎵 Eu ajustei o volume para: **${args[0]/1}/100**`)
     .setAuthor("Gerenciamento do volume", `${message.author.displayAvatarURL({format: "png"})}`)
     .setColor("BLUE")
     return message.channel.send(spray);
