a011b0ebf83b1fe5b6fc3cabfb339e8b153506d0
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-30 21:21:35 -0300

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -15,11 +15,11 @@ module.exports = {
     if (prefix === null) prefix = "s."
 
     const channel = message.member.voice.channel;
-    if (!channel)return sendError("➥ Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
+    if (!channel)return sendError("> Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("➥ Não ha nada tocando.", message.channel);
+    if (!serverQueue) return sendError("> Não ha nada tocando.", message.channel);
     if (!args[0])return message.channel.send(`🎵 O volume atual é: **${serverQueue.volume}**`);
-     if(isNaN(args[0])) return message.channel.send('🎵 Somente números!').catch(err => console.log(err));
+     if(isNaN(args[0])) return message.channel.send('> Apenas números!').catch(err => console.log(err));
     if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('> Você não pode definir o volume para mais de 150 ou menor que zero. <:bravinha:841126251741970452>',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
