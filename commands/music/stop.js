403e55e44394a1fc10594b40653b4f186f4366ca
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-30 21:20:07 -0300

    Bonito

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -15,9 +15,9 @@ module.exports = {
     if (prefix === null) prefix = "s."
 
     const channel = message.member.voice.channel
-    if (!channel)return sendError("<:Resoluteinfo:844971535927083088> Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
+    if (!channel)return sendError("➥ Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("<:Resoluteinfo:844971535927083088> Não há nada que eu possa pular para você.", message.channel);
+    if (!serverQueue)return sendError("➥ Não há nada que eu possa pular para você.", message.channel);
    if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      try{
@@ -25,7 +25,7 @@ if(!serverQueue.connection.dispatcher)return
       } catch (error) {
         message.guild.me.voice.channel.leave();
         message.client.queue.delete(message.guild.id);
-        return sendError(`<:Resoluteinfo:844971535927083088> A fila foi liberada. ${error}`, message.channel);
+        return sendError(`> A fila foi liberada. ${error}`, message.channel);
       }
     message.client.queue.delete(message.guild.id);
     serverQueue.songs = [];
