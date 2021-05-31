fa20b3c8762eb2872cd3d4ba656c47a1cec8f240
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-30 21:15:52 -0300

    branch errada

---
+++ commands/manipulacao/conquista.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "conquista",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    description: "dev",
+    category: "manipulacao",
+  
+async run (client, message, args) {
+
+const a = args.join("+")
+if(!a) return message.channel.send(`**Por favor diga algo para colocar na conquista!**`);
+if(args.join("+").length > 20) return message.channel.send(`**Use no maximo 20 caracteres!**`);
+
+
+const embed_conqu = new Discord.MessageEmbed()
+.setColor("RANDOM")
+.setImage(`https://minecraftskinstealer.com/achievement/${Math.floor(Math.random() * 39) + 1}/Achievement+Get%21/${a}`);
+
+message.channel.send(`${message.author}`,embed_conqu)
+}
+
+}
