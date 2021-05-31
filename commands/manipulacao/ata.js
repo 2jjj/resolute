b6b0d5f8e53f186f60cfb6723469de9496ab0eb4
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-30 21:13:55 -0300

    Arrumei o bug do ata

--- commands/manipulacao/ata.js
+++ commands/manipulacao/ata.js
@@ -18,7 +18,7 @@ module.exports = {
       const user = args[0] ? message.mentions.users.first() ||  client.users.cache.find(a => a.username === args.slice(0).join(' ')) || await client.users.fetch(args[0]).catch(_ => message.author) : message.author
       
       const img = user.displayAvatarURL({ dynamic: false, format: 'png', size: 4096 })
-      const background = await Canvas.loadImage(`${__dirname}/../../../images/ata-meme.png`)
+      const background = await Canvas.loadImage(`${__dirname}../../../images/ata-meme.png`)
       const avatar = await Canvas.loadImage(img)
       ctx.drawImage(avatar, 100, 0, 200, 250);
       ctx.drawImage(background, 0, 0);
