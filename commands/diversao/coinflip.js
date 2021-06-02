c575d91b749f9c60a4a2566d18d3082759adb918
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-02 15:33:13 -0300

    certinho

--- commands/diversao/coinflip.js
+++ commands/diversao/coinflip.js
@@ -10,12 +10,15 @@ module.exports = {
 
   async run (client, message, args) {
 
+  let prefix = db.get(`prefix_${message.guild.id}`)
+  if (prefix === null) { prefix = "s." }
+
   var array1 = ["cara", "coroa"];
 
   var rand = Math.floor(Math.random() * array1.length);
 
   if (!args[0] || (args[0].toLowerCase() !== "cara" && args[0].toLowerCase() !== "coroa")) {
-    message.reply("Insira **cara** ou **coroa** na frente do comando.");
+    message.reply(`Insira **cara** ou **coroa**. **| ${prefix}coinflip cara**`);
   } 
 else if (args[0].toLowerCase() == array1[rand]) {
     message.channel.send("Deu **" + array1[rand] + "**, <:info:835206734225473546> | Você ganhou dessa vez!");
