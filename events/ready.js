87545be22a019d989c66f200f2e6010d3901ac74
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-04 23:21:46 -0300

    v1

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 module.exports = async (client) => {
 
     const status = [  
-        {name: `s.help • ${client.guilds.cache.size} guilds. | Shards: 4`, type: 'PLAYING'}, 
+        {name: `s.help • ${client.guilds.cache.size} guilds.`, type: 'PLAYING'}, 
       ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
