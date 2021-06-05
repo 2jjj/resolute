ce1d345f2430d6b83b46a9ab0dc2e4ba5aedf0f3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-04 23:25:12 -0300

    v1

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js")
 
 module.exports = async (client) => {
+    var numWorkers = require('os').cpus().length;
 
     const status = [  
-        {name: `s.help • ${client.guilds.cache.size} guilds.`, type: 'PLAYING'}, 
+        {name: `s.help • ${client.guilds.cache.size} guilds. | Cluster [${numWorkers}]`, type: 'PLAYING'}, 
       ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
