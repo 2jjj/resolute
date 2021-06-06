289a8b45568804d1a3cbcd50a8e2cab683b5197c
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-06 13:28:39 -0300

    Sem música - help

--- commands/outros/ajuda.js
+++ commands/outros/ajuda.js
@@ -53,7 +53,7 @@ module.exports = {
         let economia = client.commands.filter((cmd) => cmd.category === 'economia');
         let manipulacao = client.commands.filter((cmd) => cmd.category === 'manipulacao');
         let mod = client.commands.filter((cmd) => cmd.category === 'mod');
-        let music = client.commands.filter((cmd) => cmd.category === 'musica');
+        //let music = client.commands.filter((cmd) => cmd.category === 'musica');
         let outros = client.commands.filter((cmd) => cmd.category === 'outros');
         let config = client.commands.filter((cmd) => cmd.category === 'config');
         let minecraft = client.commands.filter((cmd) => cmd.category === 'minecraft');
@@ -65,7 +65,7 @@ module.exports = {
         .addField(`<:money1:846828402350489640> **Economia** [${economia.size}]:`, `\`${economia.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:3624personframe:843854352934633542> **Manipulação de imagens** [${manipulacao.size}]:`, `\`${manipulacao.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:staff:843586666845044736> **Moderação** [${mod.size}]:`, `\`${mod.map(cmd => cmd.name).join(' | ')}\``)
-        .addField(`<:music:843602147051700284> **Música** [${music.size}]:`, `\`${music.map(cmd => cmd.name).join(' | ')}\``)
+        //.addField(`<:music:843602147051700284> **Música** [${music.size}]:`, `\`${music.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:y_pontinho:843648515695444019> **Outros** [${outros.size}]:`, `\`${outros.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:livro:850192025986138123> **Minecraft** [${minecraft.size}]:`, `\`${minecraft.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:2637settings:843854352867262504> **Configuráveis** [${config.size}]:`, `\`${config.map(cmd => cmd.name).join(' | ')}\``)
