725b79010b1ad502f21c5200633207383cff1cb2
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-06 00:39:51 -0300

    Sistema de logs

--- commands/mod/avisar.js
+++ commands/mod/avisar.js
@@ -10,6 +10,9 @@ module.exports = {
   usage: "@user <motivo>",
 
 async run (client, message, args) {
+
+  console.log(`[LOGS] - Comando ${module.exports.name} usado por ${message.author.tag}.`)
+
   let prefix = db.get(`prefix_${message.guild.id}`)
   if (prefix === null) { prefix = "s." }
   
