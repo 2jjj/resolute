ebb7486344c9eb7ad27156ba2e94afdc152fb563
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-07 16:01:45 -0300

    Insta

---
+++ commands/outros/instagram.js
@@ -0,0 +1,49 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const Discord = require("discord.js");
+
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: "instagram",
+    aliases: ["insta"],
+    category: "outros",
+    description: "insta",
+    usage: "<name>",
+    run: async (client, message, args) => {
+        const name = args.join(" ");
+
+        if (!name) {
+            return message.reply("Talvez seja útil realmente procurar por alguém ...!")
+                .then(m => m.delete(5000));
+        }
+
+        const url = `https://instagram.com/${name}/?__a=1`;
+        
+        let res; 
+
+        try {
+            res = await fetch(url).then(url => url.json());
+        } catch (e) {
+            return message.reply("Eu não achei essa conta!")
+                .then(m => m.delete(5000));
+        }
+
+        const account = res.graphql.user;
+
+        const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle(account.full_name)
+            .setURL(`https://instagram.com/${name}`)
+            .setThumbnail(account.profile_pic_url_hd)
+            .addField("Profile information", stripIndents`**- Username:** ${account.username}
+            **- Nome:** ${account.full_name}
+            **- Biografia:** ${account.biography.length == 0 ? "Nenhuma" : account.biography}
+            **- Posts:** ${account.edge_owner_to_timeline_media.count}
+            **- Seguidores:** ${account.edge_followed_by.count}
+            **- Seguindo:** ${account.edge_follow.count}
+            **- Conta privada?:** ${account.is_private ? "Sim 🔐" : "Não 🔓"}`);
+
+        message.channel.send(embed);
+    }
+}
