d4c7e23d3ac6e4f5670053025bf4e4791a189c43
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-08 20:54:07 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,8 @@ module.exports = async (client) => {
     //var numWorkers = require('os').cpus().length;
 
     const status = [  
-        {name: `s.help • ${client.guilds.cache.size} guilds,`, type: 'PLAYING'}, 
-        {name: `Spray#0007 [${client.guilds.cache.size}] []`, type: 'PLAYING'}, 
+        {name: `s.help • ${client.guilds.cache.size} guilds & ${client.users.cache.size} users.`, type: 'PLAYING'}, 
+        {name: `Spray#0007 [${client.guilds.cache.size}] [${client.users.cache.size}]`, type: 'PLAYING'}, 
       ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
