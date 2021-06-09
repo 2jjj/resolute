288cb5f77beed46d529c7b9071c62be08d577699
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-09 15:29:51 -0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -2,7 +2,7 @@ const config = require("./config.json");
 const { ShardingManager } = require("discord.js"),
   manager = new ShardingManager(`./index.js`, {
     token: config.token,
-    totalShards: 'auto',
+    totalShards: 8,
     respawn: true,
   });
 
