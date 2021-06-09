af338e5e6ad3700b85b9345e92c9c774e14728f3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-09 15:20:59 -0300

    Shards

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,8 @@ module.exports = async (client) => {
     //var numWorkers = require('os').cpus().length;
 
     const status = [  
-        {name: `s.help • ${client.guilds.cache.size} guilds & ${client.users.cache.size} users.`, type: 'PLAYING'}, 
-        {name: `Shard ${message.guild.shard.id} [${client.shard.count}]`, type: 'PLAYING'}, 
+        {name: `s.help • ${client.guilds.cache.size} guilds & ${client.users.cache.size} users • Shards [${client.shard.count}]`, type: 'PLAYING'}, 
+        {name: `Shards [${client.shard.count}]`, type: 'PLAYING'}, 
       ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
