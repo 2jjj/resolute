70fd588a517d03f15f3315e8843a3d8fe70efab3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-09 18:07:34 -0300

    Create mchead.js

---
+++ commands/minecraft/mchead.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "mcbust",
+    description: "Comando para pegar o Head de uma Skin de Minecraft.",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    category: "minecraft",
+    usage: "<nickname>",
+
+  async run({ message, args, prefix, author }, t) {
+    const nick = args[0];
+
+    if (!nick)
+      return message.channel.send(
+        `${message.author}, você deve inserir um nick de uma skin de minecraft.`
+      );
+
+    const EMBED = new ClientEmbed(author)
+
+      .setTitle(`${Emojis.Minecraft} - Nick: ${nick}`)
+      .setImage(`https://mc-heads.net/head/${nick}/200`);
+
+    message.channel.send(EMBED);
+  }
+};
