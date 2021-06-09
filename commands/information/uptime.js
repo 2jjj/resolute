9cd43ab3bebb7cea936754d649263829cbb5b8a6
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-09 11:53:45 -0300

    Emojis

--- commands/information/uptime.js
+++ commands/information/uptime.js
@@ -20,13 +20,13 @@ module.exports = {
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
 
-    let uptime = `<:setaazul:843588568605523969> ${days.toFixed()} dias\n<:setaazul:843588568605523969> ${hours.toFixed()} horas\n<:setaazul:843588568605523969> ${minutes.toFixed()} minutos\n<:setaazul:843588568605523969> ${seconds.toFixed()} segundos`;
+    let uptime = `<a:SETA:852194614927818812> ${days.toFixed()} dias\n<a:SETA:852194614927818812> ${hours.toFixed()} horas\n<a:SETA:852194614927818812> ${minutes.toFixed()} minutos\n<a:SETA:852194614927818812> ${seconds.toFixed()} segundos`;
 
     const embed = new Discord.MessageEmbed()
       .setTitle(`Tempo de atividade`)
       //.setThumbnail("https://imgur.com/WZMylbw.gif")
       .setColor("#FF0000")
-      .setDescription(`<:N3UP_Servers:845357935155478548> Uptime \n${uptime}`)
+      .setDescription(`<:information:843542771814236170> Uptime \n${uptime}`)
     message.channel.send(embed);
 }
 }
