177564dcc76017a8756a3e2ca66b61d38b68910d
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-09 17:46:37 -0300

    Create nick.js

---
+++ commands/mod/nick.js
@@ -0,0 +1,30 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "servericon",
+    aliases: ['serveravatar'],
+    cooldown: 1000 * 2, 
+    description: "icone do servidor.",
+    category: "mod",
+    usage: "",
+
+    async run (client, message, args) {
+
+    const member = message.mentions.members.first();
+
+    if (!member) return message.reply("Especifique o usuário.");
+
+    const arguments = args.slice(1).join(" ");
+
+    if (!arguments) return message.reply("Especifique o nickname!");
+
+    try {
+      member.setNickname(arguments);
+    } catch (err) {
+      console.log(err);
+      message.reply(
+        "eu não tenho permissão " + member.toString() + " nickname!"
+      );
+    }
+  },
+};
