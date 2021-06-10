cf07f538a9e5c4f3385babcfbece9cfb10369448
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-10 10:10:00 -0300

    Create drake.js

---
+++ commands/manipulacao/drake.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+const fetch = require('node-fetch')
+module.exports = {
+    name: 'drake',
+    aliases: [],
+    description: 'drake meme maker',
+    usage: '$drake <texto1>, <texto2>',
+    category: "manipulacao",
+    cooldown: 1000 * 2, 
+
+    run: async (client, message, args) => {
+
+        const split = args.join(" ").split(",")
+        const user = split[0];
+        const user2 = split[1]
+        const res = await fetch(`https://frenchnoodles.xyz/api/endpoints/drake/?text1=${user}&text2=${user2}`, {
+
+        });
+        let Image = await res.buffer();
+        const drakememe = new Discord.MessageAttachment(Image);
+        message.channel.send(drakememe);
+
+    }
+}
