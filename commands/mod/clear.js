925dbf1b3abdbff4cd4519963324df99c371105f
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-09 21:11:00 -0300

    Update clear.js

--- commands/mod/clear.js
+++ commands/mod/clear.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-const db = require("quick.db") 
 
 module.exports = {
   name: "clear",
@@ -11,8 +9,6 @@ module.exports = {
 
   async run (client, message, args) {
 
-    let prefix = db.get(`prefix_${message.guild.id}`)
-    if (prefix === null) { prefix = "s." }
     
     let user = message.author.username
     if (!message.member.permissions.has("MANAGE_MESSAGES"))
