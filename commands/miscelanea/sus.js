fec46716121982bf10d0360af4994b10952f25d3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-10 10:31:24 -0300

    Create sus.js

---
+++ commands/miscelanea/sus.js
@@ -0,0 +1,28 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const { MessageButton } = require('discord-buttons');
+module.exports = {
+    name: "sus",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    description: "SUSKKK",
+    category: "outros",
+    usage: "",
+
+    async run (client, message, args) {
+
+        const butn = new MessageButton()
+            .setLabel(`SUS!?`)
+            .setStyle('gray')
+            .setID('amogus');
+
+        let msg = await message.channel.send(`Sussy bruh`, {
+            button: butn,
+        });
+
+        client.on('clickButton', async (button) => {
+            if (button.id === 'amogus') {
+                await button.reply.send(`SUS! THE IMPOSTER IS SUS!!!`)
+            }
+        });
+    },
+};
