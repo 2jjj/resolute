419d1442bc9fbabc2320f00af1304d674a0f74c5
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-10 10:20:03 -0300

    Create nuke.js

---
+++ commands/mod/nuke.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: "nuke",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    description: "Nukar um canal!",
+    category: "mod",
+    usage: "",
+
+    async run (client, message, args) {
+
+		let filter = (m) => m.author.id === message.author.id
+		message.channel.send(
+			'Você tem certeza? | Responda com s / n '
+		)
+		message.channel
+			.awaitMessages(filter, {
+				max: 1,
+				time: 20000,
+				errors: ['time'],
+			})
+			.then((message) => {
+				message = message.first()
+				if (
+					message.content.toLowerCase() == 'Sim' ||
+					message.content.toLowerCase() == 's' 
+				) {
+					let channel = client.channels.cache.get(message.channel.id)
+					let posisi = channel.position
+					channel.clone().then((channel2) => {
+						channel2.setPosition(posisi)
+						channel.delete()
+						channel2.send(
+							' **O canal foi nukado!** | https://i.pinimg.com/originals/06/c3/92/06c392b847166a9a671bfcd590d8fff7.gif'
+						)
+					})
+				} else if (
+					message.content.toLowerCase() == 'Não' ||
+					message.content.toLowerCase() == 'n'
+				) {
+					return message.channel.send('Comando de nuke cancelado!')
+				} else {
+					return message.channel.send(`Resposta inválida.`)
+				}
+			})
+	}
+}
