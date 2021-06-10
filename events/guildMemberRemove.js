63d690b27ae9427041cbf22946dbc03542910f7f
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-10 12:28:20 -0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,4 @@
 const db = require("quick.db");
-const Discord = require("discord.js")
 
 module.exports = async (client, member) => {
 
@@ -8,5 +7,5 @@ module.exports = async (client, member) => {
 	db.delete(`work_${member.guild.id}_${member.id}`); 
 	db.delete(`daily_${member.guild.id}_${member.id}`); 
 	db.delete(`rob_${member.guild.id}_${member.id}`); 
-	console.log(`[LOGS] - [ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
+	//console.log(`[LOGS] - [ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`);
 }
