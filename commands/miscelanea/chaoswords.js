111705b439881d2160c96615aec6032ac01f8406
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-11 13:37:48 -0300

    Update

---
+++ commands/miscelanea/chaoswords.js
@@ -0,0 +1,22 @@
+const { ChaosWords } = require("weky")
+var randomWords = require('random-words');
+module.exports = {
+    name: "chaoswords",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    description: "",
+    category: "outros",
+    usage: "",
+  
+    async run (client, message, args) {
+        const words = randomWords(2) 
+        await new ChaosWords({
+            message: message,
+            maxTries: 8, 
+            charGenerated: 20, 
+            words: words, 
+            embedTitle: 'Chaos words!',
+            embedFooter: 'Ache as palavras!',
+            embedColor: 'RANDOM'
+        }).start()
+}}
