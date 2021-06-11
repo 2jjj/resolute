11b3883bc5903c269a68da49a1e1dbf0cbc85204
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-11 14:38:15 -0300

    Create fasttype.js

---
+++ commands/diversao/fasttype.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "fasttype",
+    aliases: [],
+    cooldown: 1000 * 2, 
+    description: " ",
+    category: "fun",
+    usage: "",
+  
+  async run (client, message, args) {
+    const { FastType } = require('weky');
+    const game = new FastType({
+        message: message,
+        winMessage: "Você ganhou!", // message sent when user types perfectly
+        sentence: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s', // sentence-to-be-typed
+        loseMessage: 'Perdeu ;(', // message sent when user misspell it
+        time: 50000, // time that user has in ms
+        startMessage: 'Boa sorte!!' // message sent when user starts playing
+    });
+    game.start(); 
+}}
