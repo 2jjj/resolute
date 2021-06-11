0c0a899c653e1d39db474396644c8835f28aa140
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-11 11:55:12 -0300

    Fix - LOGS

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,11 +2,10 @@ const db = require("quick.db");
 const crystol = require("crystolnetwork-log");
 
 module.exports = async (client, member) => {
-
 	db.delete(`money_${member.guild.id}_${member.id}`); 
 	db.delete(`bank_${member.guild.id}_${member.id}`); 
 	db.delete(`work_${member.guild.id}_${member.id}`); 
 	db.delete(`daily_${member.guild.id}_${member.id}`); 
 	db.delete(`rob_${member.guild.id}_${member.id}`); 
-	cystol.log(`[LOGS] - [ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`, "economia_member_remove.log", "America/Sao_Paulo");
+	crystol.log(`[LOGS] - [ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`, "economia_member_remove.log", "America/Sao_Paulo");
 }
