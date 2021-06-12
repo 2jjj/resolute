55fe51a886db9df3b27a50b04dfa7221af0adf60
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-12 00:06:32 -0300

    Heroku

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ fs.readdir(__dirname + "/events/", (err, files) => {
 });
 });
 
-//NzY0OTE5OTQxNTM4Nzc1MDUw.X4NRNQ.pkl0V44wfk_FAGTx0QcKZbtmdRU
+//NzY0OTE5OTQxNTM4Nzc1MDUw.X4NRNQ.ACmB_7hwECCMfCrO-2TyX9k_UZY
 //ODM3Nzg1MjA1MDYxOTc2MDk2.YIxmRg.fUm4cDC90dHY-_tQQ4GsXgD2w0o
 client.login(config.token)
