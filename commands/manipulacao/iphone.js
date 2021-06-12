662f153e2d65f5b0de7d4d0c71e673670ea22f7a
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-11 22:18:53 -0300

    Create iphone.js

---
+++ commands/manipulacao/iphone.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch')//npm i node-fetch if you have it then dont do it :\
+                 
+module.exports = {
+    name: 'iphonex',
+    aliases: [],
+    category: 'manipulacao',
+    description: 'Iphone X User Profile',
+    usage: 'iphonex <user>',
+    cooldown: 1000 * 2, 
+    run: async (client, message, args) => {
+        message.channel.startTyping();
+        let mention = message.mentions.members.first();
+
+        let m = await message.channel.send("**Aguarde...**");
+
+        if (!mention) {
+            m.edit("⚠ Mencione alguém.");
+            return message.channel.stopTyping();
+        }
+        try {
+            let res = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=iphonex&url=${mention.user.displayAvatarURL({ size: 1024 })}`));
+            let json = await res.json();
+            let attachment = new Discord.MessageAttachment(json.message, "iphonex-syze.png");
+            await message.channel.send(attachment);
+            message.channel.stopTyping();
+            m.delete();
+        } catch (e) {
+            m.edit("⚠ Erro!");
+            return message.channel.stopTyping();
+        }
+    }
+};
