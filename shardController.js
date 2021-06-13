9fed6d9ff7e49439d48a1d0186bc777725ffdc3a
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-13 20:11:37 -0300

    Update shardController.js

--- shardController.js
+++ shardController.js
@@ -1,1 +1,13 @@
-module.exports = shardguild;
+//By spray (que ganbiarra KKKKKKKKKKKKKKKKKKKKK)
+
+const client = require("./index");
+const { ShardingManager } = require('discord.js');
+//var numWorkers = require('os').cpus().length;
+
+var shardsguilds = client.on("message", (message) => {
+    var shardidguild = message.guild.shard.id;
+})
+
+module.exports = shardsguilds;
+
+//console.log(shardsguilds.shardidguild)
