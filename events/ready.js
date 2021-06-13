af9f15c0f998b135514fcb967dfb0d37e6b20cec
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-13 01:09:12 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = async (client) => {
     var numWorkers = require('os').cpus().length;
 
     const status = [  
-        {name: `s.help • ${client.guilds.cache.size} guilds • Developed by Spray#0007 [${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB]`, type: 'PLAYING'}, 
+        {name: `s.help • ${client.guilds.cache.size} guilds • Developed by Spray#0007 || [${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB]`, type: 'PLAYING'}, 
         {name: `Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}, 
       ] 
       function Presence() { 
