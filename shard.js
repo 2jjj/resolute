77b8cab514e96a805ecd6a2996a5d5cc1538654c
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-13 19:54:15 -0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,26 +1,10 @@
-/*
-    The following code goes into it's own file, and you run this file
-    instead of your main bot file.
-*/
-
-// Include discord.js ShardingManger
 const { ShardingManager } = require('discord.js');
 
-// Create your ShardingManger instance
 const manager = new ShardingManager('./index.js', {
-    // for ShardingManager options see:
-    // https://discord.js.org/#/docs/main/v11/class/ShardingManager
-
-    // 'auto' handles shard count automatically
-    totalShards: 'auto', 
-
-    // your bot token
+    totalShards: numWorkers, 
     token: 'NzY0OTE5OTQxNTM4Nzc1MDUw.X4NRNQ.ACmB_7hwECCMfCrO-2TyX9k_UZY'
 });
 
-// Spawn your shards
 manager.spawn();
 
-// The shardCreate event is emitted when a shard is created.
-// You can use it for something like logging shard launches.
 manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
