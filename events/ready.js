ef90aa423968ee53d93c22a3a4b24c4ce02039d3
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-13 22:14:17 -0300

    Shards

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,12 @@
 const os = require('os');
+const config = require("../config.json");
 
 module.exports = async (client) => {
-    var numWorkers = require('os').cpus().length;
+    //var numWorkers = require('os').cpus().length;
 
     const status = [  
-        {name: `s.help • Shards Online: ${numWorkers} || Desenvolvido pelo Spray#0007`, type: 'PLAYING'}, 
-        {name: `Shards Online: ${numWorkers} || Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}, 
+        {name: `s.help • Shards Online: ${config.shards} || Desenvolvido pelo Spray#0007`, type: 'PLAYING'}, 
+        {name: `Shards Online: ${config.shards} || Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}, 
       ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
