9fa66c1a2a195bfb0f3868b655897b3b47c71e6d
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-14 20:28:55 -0300

    Certo.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,8 +4,9 @@ module.exports = async (client, guild) => {
 
 //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
 const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
-
-    client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${client.guilds.cache.size}`).catch((e) => console.log((e)))
+    const guilds = await client.shard.broadcastEval("this.guilds.cache.size")
+    const botGuilds = guilds.reduce((prev, val) => prev + val)
+    client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${botGuilds}`).catch((e) => console.log((e)))
     //client.channels.cache.get("841733238165995570").setName(`📝│Users: ${client.users.cache.size}`).catch((e) => console.log((e)))
     const dono = await client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
     let embed = new Discord.MessageEmbed()
