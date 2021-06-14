220889fcf79d096a80d765497ce39b7672661801
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-14 13:22:05 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,15 +4,14 @@ const config = require("../config.json");
 module.exports = async (client) => {
     //var numWorkers = require('os').cpus().length;
     const guilds = await client.shard.broadcastEval("this.guilds.cache.size")
-    
+
     console.log(`Fui iniciado com ${guilds}`)
 
     const status = [  
-        {name: `s.help | Desenvolvido pelo Spray#0007 || Shards Online: ${config.shards}`, type: 'PLAYING'}, 
-        {name: `Shards Online: ${config.shards} || Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}, 
-        {name: `Vamos transformar está plataforma! || Shards Online: ${config.shards}`},
-        {name: `Me adicione: invite.resolutebot.xyz || Shards Online: ${config.shards}`},
-        {name: `Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB || Shards Online: ${config.shards}`},      
+        {name: `s.help | ${guilds} guilds | Shards: ${config.shards}`, type: 'PLAYING'}, 
+        {name: `Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB || Shards Online: ${config.shards}`},
+        {name: `Dev: Spray#0007 | [${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)}] | [${config.shards}]`},
+        {name: `Vamos transformar está plataforma! | [${config.shards}]`},
     ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
