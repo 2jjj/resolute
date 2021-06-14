d0a3f5ac300012ae5fb5b67b6b52c7d98e25beae
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-14 12:38:41 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,9 +5,12 @@ module.exports = async (client) => {
     //var numWorkers = require('os').cpus().length;
 
     const status = [  
-        {name: `s.help • Shards Online: ${config.shards} || Desenvolvido pelo Spray#0007`, type: 'PLAYING'}, 
+        {name: `s.help | Desenvolvido pelo Spray#0007 || Shards Online: ${config.shards}`, type: 'PLAYING'}, 
         {name: `Shards Online: ${config.shards} || Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}, 
-      ] 
+        {name: `Vamos transformar está plataforma! || Shards Online: ${config.shards}`},
+        {name: `Me adicione: invite.resolutebot.xyz || Shards Online: ${config.shards}`},
+        {name: `Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB || Shards Online: ${config.shards}`},      
+    ] 
       function Presence() { 
               const base = status[Math.floor(Math.random() * status.length)] 
               client.user.setActivity(base)
