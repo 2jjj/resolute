e00d4c8148ba3c19e3dec1977f9415c98c46281f
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-13 21:34:26 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,7 @@ const os = require('os');
 
 module.exports = async (client) => {
     //var numWorkers = require('os').cpus().length;
-
-    var guilds = client.shard.fetchClientValues('guilds.cache.size').then(results => {console.log(`${results.reduce((acc, guildCount) => acc + guildCount, 0)} total guilds`);})
-    var users = client.shard.fetchClientValues('users.cache.size')
-
+    
     const status = [  
         {name: `s.help • Developed by Spray#0007 || [${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB]`, type: 'PLAYING'}, 
         {name: `Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`}, 
