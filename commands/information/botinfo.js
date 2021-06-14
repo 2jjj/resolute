100e4f95a550d1bfccc25af2121dea492c0b2f62
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-14 14:11:39 -0300

    Fix

--- commands/information/botinfo.js
+++ commands/information/botinfo.js
@@ -12,9 +12,9 @@ module.exports = {
     
     let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
     let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
-
     let { version } = require("discord.js");
-    var numWorkers = require('os').cpus().length;
+    const guilds = await client.shard.broadcastEval("this.guilds.cache.size")
+    const botGuilds = guilds.reduce((prev, val) => prev + val)
 
     let embed = new Discord.MessageEmbed()
     .setTitle("**Resolute - Botinfo**")
@@ -23,7 +23,7 @@ module.exports = {
     .addFields(
       { name: 'ﾠ', value: '<a:SETA:852194614927818812> **Desenvolvedor:** <@836345581424738354>' },
       { name: 'ﾠ', value: `<a:SETA:852194614927818812> **Bot criado no dia:** **11/10/2020**`, inline: false },
-      { name: 'ﾠ', value: `<a:SETA:852194614927818812> **Estou em ${client.guilds.cache.size} servidores!**`, inline: false },
+      { name: 'ﾠ', value: `<a:SETA:852194614927818812> **Estou em ${botGuilds} servidores!**`, inline: false },
       { name: 'ﾠ', value: `<a:SETA:852194614927818812> **Versão Discord.js:** **${version}**`, inline: false },
       { name: 'ﾠ', value: `<a:SETA:852194614927818812> **Versão Node.JS:** **${process.version}**`, inline: false },
       { name: 'ﾠ', value: `<a:SETA:852194614927818812> **Estou na versão** **1.7.0B**`, inline: false },
--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
 const os = require('os');
 const config = require("../config.json");
-require("../commands/information/shards")
 
-module.exports = async (client, botGuilds) => {
+module.exports = async (client) => {
     //var numWorkers = require('os').cpus().length;
+    const guilds = await client.shard.broadcastEval("this.guilds.cache.size")
+    const botGuilds = guilds.reduce((prev, val) => prev + val)
+
     const status = [  
-        {name: `s.help | 98 guilds | Shards: ${config.shards}`, type: 'PLAYING'}, 
+        {name: `s.help | ${botGuilds} guilds | Shards: ${config.shards}`, type: 'PLAYING'}, 
         {name: `Memória ultilizada ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB || Shards Online: ${config.shards}`},
         {name: `Dev: Spray#0007 | [${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)}] | [${config.shards}]`},
         {name: `Vamos transformar está plataforma! | [${config.shards}]`},
