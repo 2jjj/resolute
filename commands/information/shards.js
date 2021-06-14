54fa8f2e2e7d595eda017593614092726d2fd9f8
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-13 22:02:34 -0300

    Update shards.js

---
+++ commands/information/shards.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "shards",
+  aliases: [],
+  cooldown: 1000 * 2, 
+  description: "Shards.",
+  category: "info",
+  usage: "",
+
+  async run (client, message, args) {
+
+  }
+}
