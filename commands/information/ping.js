df47155d98eddaf3c95dbaa454a575f99fe53c89
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-15 11:37:17 -0300

    Update ping.js

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -11,7 +11,7 @@ module.exports = {
 
   async run (client, message, args) {
 
-    //var numWorkers = require('os').cpus().length;
+    var numWorkers = require('os').cpus().length;
 
     let svPing = Date.now() - message.createdTimestamp
     
@@ -19,7 +19,7 @@ module.exports = {
     
         message.inlineReply('Ping?').then(msg => {
         
-        msg.edit(`🏓 **|** Pong!\n:satellite: **|** Shard: ${message.guild.shard.id}/${config.shards}\n:stopwatch: **|** ${client.translate('API LATENCY:', message)} ${svPing}\n:zap: **|** Ping: ${client.ws.ping}\n🌏 **|** Ping da shard: ${shardPing}ms`) 
+        msg.edit(`🏓 **|** Pong!\n:satellite: **|** Shard: ${message.guild.shard.id}/${config.shards}\n:stopwatch: **|** Latência da API: ${svPing}\n:zap: **|** Ping: ${client.ws.ping}\n🌏 **|** Ping da shard: ${shardPing}ms`) 
     });
   }
 }
