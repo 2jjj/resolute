0b80ea5447db8f27dc8a627db5383fd062ae4a07
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-15 13:47:20 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,10 +9,11 @@ module.exports = async (client) => {
     .then(async results => { 	
         const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0); 	
         const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-        
+        const botGuilds = guilds.reduce((prev, val) => prev + val)
+
         const arrayOfStatus = [
         `s.help | Shards: ${config.shards} | ${totalMembers} users`,
-        `${totalGuilds} guilds & ${totalMembers} users`,
+        `${botGuilds} guilds & ${totalMembers} users`,
         ];
 
         let index= 0;
