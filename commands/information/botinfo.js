b68d89fc74f1d48df6fc66a9eefcc923108789fe
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-16 11:26:46 -0300

    Bot Info

--- commands/information/botinfo.js
+++ commands/information/botinfo.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const config = require("../../config.json")
+const os = require('os')
 
 module.exports = {
   name: "botinfo",
@@ -9,23 +11,28 @@ module.exports = {
   usage: "",
 
   async run (client, message, args) {
-    
-    let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-    let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
-    let { version } = require("discord.js");
-    const guilds = await client.shard.broadcastEval("this.guilds.cache.size")
-    const botGuilds = guilds.reduce((prev, val) => prev + val)
-
-    let embed = new Discord.MessageEmbed()
-    .setTitle("**Minhas informações!**")
-    .setColor("RANDOM")
-    .setThumbnail(avatar)
-    .setDescription(`Me chamo Resolute, fui criado no dia **11 de outubro de 2020** pelo **Spray#0007** na linguagem **Javascript** com o discord.js\n\n\n**Algumas informações:
-    dsa
-    da
-    da
-    da
-    **`)
-    await message.channel.send(embed);
-}
-}
+    const promises = [ client.shard.fetchClientValues('guilds.cache.size'), 
+    client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)')];
+    Promise.all(promises) 	
+    .then(async results => { 	
+        const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0); 	
+        const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);   
+        //**//
+        let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+        let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
+        let { version } = require("discord.js");
+        //**//
+        let embed = new Discord.MessageEmbed()
+        .setTitle("**Minhas informações!**")
+        .setColor("RANDOM")
+        .setThumbnail(avatar)
+        .setDescription(`Me chamo Resolute, fui criado no dia **11 de outubro de 2020** pelo **Spray#0007** na linguagem **Javascript** com o discord.js\n\n\n**Algumas informações:\n
+        <:early_developer_badge:854716150076538901> **|** Developer: ${config.spray}
+        <:pontin:852197383974551582> **|** Estou em ${totalGuilds} servidores.
+        <:pontin:852197383974551582> **|** ${totalMembers} de usuários.
+        <:cpu:854137097521987624> **|** Shard: ${message.guild.shard.id}/${config.shards}
+        <:memoryram:854135087037153280> **|** Memória ultilizada: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB
+        **`)
+        await message.channel.send(embed);
+    })
+}}
