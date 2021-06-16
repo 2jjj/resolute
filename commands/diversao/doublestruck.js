7c38265d1a40bcb446961d45c1094a0b075e015c
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-16 13:56:43 -0300

    Gifs V2

---
+++ commands/diversao/doublestruck.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fetch = require('node-fetch');
+module.exports = {
+    name: "doublestruck",
+    aliases: ["ds"],
+    cooldown: 1000 * 2, 
+    description: "doublestruck",
+    category: "fun",
+    usage: "<texto>",
+  
+  async run (client, message, args) {
+  
+    let text = args.join("+")
+    if(!text) return message.reply('Por favor coloque um texto!')
+    let res = await fetch('https://api.popcatdev.repl.co/doublestruck?text=' + text);
+    let json = await res.json();
+    message.channel.send(json.text)
+ }
+}
