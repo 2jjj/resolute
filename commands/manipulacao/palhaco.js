9c1f45b652dbaa85bf6d67f73dfe51964595587a
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-18 12:19:24 -0300

    Create palhaco.js

---
+++ commands/manipulacao/palhaco.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "palhaco",
+    aliases: ["clown"],
+    cooldown: 1000 * 2, 
+    description: "Palhaço!",
+    category: "manipulacao",
+    usage: "@user",
+  
+    async run (client, message, args) {
+
+     const user =
+      message.mentions.members.first() || message.member;
+let msg = await message.channel.send('loading...')
+      let att = new Discord.MessageAttachment(`https://api.popcatdev.repl.co/clown?image=${user.user.displayAvatarURL({ dynamic: false, format: "png"})}`, `${user.user.username}_clown.png`)
+
+      message.channel.send(att)
+      msg.delete()
+  }
+}
