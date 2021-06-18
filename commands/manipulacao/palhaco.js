f1e4755a00f9cc3b20afb991f5f10a1ed67f687e
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-18 12:19:33 -0300

    Update palhaco.js

--- commands/manipulacao/palhaco.js
+++ commands/manipulacao/palhaco.js
@@ -12,7 +12,7 @@ module.exports = {
 
      const user =
       message.mentions.members.first() || message.member;
-let msg = await message.channel.send('loading...')
+let msg = await message.channel.send('Carregando...')
       let att = new Discord.MessageAttachment(`https://api.popcatdev.repl.co/clown?image=${user.user.displayAvatarURL({ dynamic: false, format: "png"})}`, `${user.user.username}_clown.png`)
 
       message.channel.send(att)
