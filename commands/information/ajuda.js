be29a27f6ffa7a1d66fddac8748e8d51365b8930
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-18 23:04:47 -0300

    Update ajuda.js

--- commands/information/ajuda.js
+++ commands/information/ajuda.js
@@ -57,6 +57,7 @@ module.exports = {
         let minecraft = client.commands.filter((cmd) => cmd.category === 'minecraft');
         let info = client.commands.filter((cmd) => cmd.category === 'info');
         let botoes = client.commands.filter((cmd) => cmd.category === 'botoes');
+        let acao = client.commands.filter((cmd) => cmd.category === 'gifs');
 
       const embed = new MessageEmbed()
         .addField(`Desenvolvido por Spray#0007`, `**Prefixo atual: ${prefix}\nMeus Comandos[${client.commands.size}]:\n**`)
@@ -65,11 +66,12 @@ module.exports = {
         .addField(`<:ybs_defesa:852215040110362675> **Moderação** [${mod.size}]:`, `\`${mod.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:ybs_status:851954702840627200> **Miscelânea** [${miscelanea.size}]:`, `\`${miscelanea.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:ybs_dinheiro:852213985356939314> **Economia** [${economia.size}]:`, `\`${economia.map(cmd => cmd.name).join(' | ')}\``)
-        .addField(`<:ybs_usuarios:843619560316993567> **Manipulação de imagens** [${manipulacao.size}]:`, `\`${manipulacao.map(cmd => cmd.name).join(' | ')}\``)
+        .addField(`<:user_StarLab:855629091344744448> **Manipulação de imagens** [${manipulacao.size}]:`, `\`${manipulacao.map(cmd => cmd.name).join(' | ')}\``)
         //.addField(`<:music:843602147051700284> **Música** [${music.size}]:`, `\`${music.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:ybs_config:851954339735666729> **Configuráveis** [${config.size}]:`, `\`${config.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`😂 **Diversão** [${fun.size}]:`, `\`${fun.map(cmd => cmd.name).join(' | ')}\``)
-        .addField(`<:mine_foguete:852197847754604565>**Minecraft** [${minecraft.size}]:`, `\`${minecraft.map(cmd => cmd.name).join(' | ')}\``)
+        .addField(`<:mine_foguete:852197847754604565> **Minecraft** [${minecraft.size}]:`, `\`${minecraft.map(cmd => cmd.name).join(' | ')}\``)
+        .addField(`<:ybs_usuarios:851954718724980757> **Ação** [${minecraft.size}]:`, `\`${minecraft.map(cmd => cmd.name).join(' | ')}\``)
         //.addFields(categories)
         .setDescription(`Use \`${prefix}help\` seguido por um nome de comando para obter mais informações adicionais sobre um comando.`, `Por exemplo: \`${prefix}help ban\`.`)
         .setFooter(
