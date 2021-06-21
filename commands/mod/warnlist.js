6a0dffef861bd053ac749bea645e501ca982eec4
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-20 21:59:51 -0300

    Agora os comandos estão bons

--- commands/mod/warnlist.js
+++ commands/mod/warnlist.js
@@ -11,19 +11,22 @@ module.exports = {
   
 async run (client, message, args) {
 
-let user = message.mentions.members.first() || message.author;
-if(!user) {
-return message.channel.send("<a:SETA:852194614927818812> Mencione um user para ver os warn's dele.")
-}
+    let user = message.mentions.members.first() || message.author;
+    if(!user) {
+    return message.channel.send("<a:SETA:852194614927818812> Mencione um user para ver os warn's dele.")
+    }
 
-let warns = await db.get(`warnsCount_${message.guild.id}-${user.id}`) || 0;
+    let warns = await db.get(`warnsCount_${message.guild.id}-${user.id}`) || 0;
 
-const embed = new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
 
-.setTitle(' :scales:  **|** Warns')
-.setColor("RANDOM")
-.setDescription (`<a:SETA:852194614927818812> **${user} Tem ${warns} Warns \n<a:SETA:852194614927818812> ** Siga as regras para não sofrer advertencia!`)
+    .setTitle(':scales:  **|** Warnlist')
+    .setColor("RANDOM")
+    .setDescription (`<a:SETA:852194614927818812> **${user} Tem ${warns} Warns \n<a:SETA:852194614927818812> ** Siga as regras para não sofrer advertencia!`)
+    .setFooter(
+        `Requisitado por ${message.author.tag}`,
+        message.author.displayAvatarURL({ dynamic: true })
+    )
 
-message.channel.send(embed);
-}
-}
+    message.channel.send(embed);
+}}
