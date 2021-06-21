6d74b6c4a997a7ca6c2f285125bece3667e865dc
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-21 10:06:15 -0300

    V.2

--- commands/mod/unmute.js
+++ commands/mod/unmute.js
@@ -11,12 +11,12 @@ module.exports = {
     async run (client, message, args) {
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 
-        if(!Member) return message.channel.send('Usuário não encontrado.')
+        if(!Member) return message.channel.send('<:1926blurplecross:856520144872407060> **|** O usuário que você mencionou não foi encontrado.')
 
         const role = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted');
 
         await Member.roles.remove(role)
 
-        message.channel.send(`${Member.displayName} está desmutado.`)
+        message.channel.send(`<:3169blurpleverified:856520145254088714> **|** ${Member.displayName} foi desmutado com sucesso!`)
     }
 }
