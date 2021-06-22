0700c34208286d4d5efa4e414482b1e7c608f2c0
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-22 14:33:51 -0300

    V2

--- commands/economia/daily.js
+++ commands/economia/daily.js
@@ -11,27 +11,21 @@ module.exports = {
     usage: "",
 
     async run (client, message, args) {
-        
         let user = message.author;
-
         let timeout = 86400000;
-
         let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
-        
         let amount = Math.floor(Math.random() * 10000) + 1000;
-        
+
         if (daily !== null && timeout - (Date.now() - daily) > 0) {
             let time = ms(timeout - (Date.now() - daily));
-    
+
             let timeEmbed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setDescription(`Você já recebeu sua recompensa diária!\n\nColete novamente daqui a **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
             .setFooter(`Requisitado por: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))        
             .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
             .setTimestamp();
-            
             message.channel.send(`${user}`, timeEmbed);
-
         } else {
             let time = ms(timeout - (Date.now() - daily));
 
@@ -43,6 +37,7 @@ module.exports = {
             .setFooter(`Requisitado por: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
             .setTimestamp();        
             message.channel.send(`${user}`, moneyEmbed);
+            //Adicionando o dinheiro
             db.add(`money_${message.guild.id}_${user.id}`, amount);
             db.set(`daily_${message.guild.id}_${user.id}`, Date.now());
         }
