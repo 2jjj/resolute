9928a9756e9740ad9b6f6c0e93ac4f24c46cb6f9
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-23 10:11:52 -0300

    V.2 || Sistema de usage

--- commands/acoes/kiss.js
+++ commands/acoes/kiss.js
@@ -11,31 +11,42 @@ module.exports = {
 
 async run (client, message, args) {
 
-var list = [
-  'https://imgur.com/qALwmUW.gif',
-  'https://imgur.com/Su2mFPY.gif',
-  'https://imgur.com/ZzF0olN.gif',
-  'https://imgur.com/cuMVXHS.gif',
-  'https://imgur.com/3i2RdCa.gif',
-  'https://imgur.com/Yjk1OQF.gif',
-  'https://imgur.com/x9ex9O3.gif',
-  'https://imgur.com/Me1o6YB.gif'
-];
+  var list = [
+    'https://imgur.com/qALwmUW.gif',
+    'https://imgur.com/Su2mFPY.gif',
+    'https://imgur.com/ZzF0olN.gif',
+    'https://imgur.com/cuMVXHS.gif',
+    'https://imgur.com/3i2RdCa.gif',
+    'https://imgur.com/Yjk1OQF.gif',
+    'https://imgur.com/x9ex9O3.gif',
+    'https://imgur.com/Me1o6YB.gif'
+  ];
 
-var rand = list[Math.floor(Math.random() * list.length)];
-let user = message.mentions.users.first() || client.users.cache.get(args[0]);
-if (!user) {
-return message.reply('lembre-se de mencionar um usuário válido para beijar!');
+  var rand = list[Math.floor(Math.random() * list.length)];
+  let user = message.mentions.users.first() || client.users.cache.get(args[0]);
+
+  if (!user) {
+    const help = new Discord.MessageEmbed()
+    .setTitle("Comando de kiss")
+    .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+    .setDescription("Beije alguém virtualmente 😳")
+    .addField(`Forma de Utilização:`, ` \`${prefix}kiss @usuario\``)
+    .setFooter(`Comando executado por: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
+    .setImage(rand)
+    .setTimestamp();
+    return message.channel.send(help);
+  }
+
+  let avatar = message.author.displayAvatarURL({format: 'png'});
+    const embed = new Discord.MessageEmbed()
+      .setTitle('Kiss')
+      .setColor('#000000')
+      .setDescription(`${message.author} acaba de beijar o ${user}`)
+      .setImage(rand)
+      .setTimestamp()
+      .setThumbnail(avatar)
+      .setFooter('Resolute')
+      .setAuthor(message.author.tag, avatar);
+    await message.channel.send(embed);
+  } 
 }
-let avatar = message.author.displayAvatarURL({format: 'png'});
-  const embed = new Discord.MessageEmbed()
-        .setTitle('Kiss')
-        .setColor('#000000')
-        .setDescription(`${message.author} acaba de beijar o ${user}`)
-        .setImage(rand)
-        .setTimestamp()
-        .setThumbnail(avatar)
-        .setFooter('Resolute')
-        .setAuthor(message.author.tag, avatar);
-  await message.channel.send(embed);
-}}
