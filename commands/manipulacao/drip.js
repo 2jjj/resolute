73a098cd1f44f9469399956884aedf2d027b9092
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-26 12:26:18 -0300

    Create drip.js

---
+++ commands/manipulacao/drip.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+module.exports = {
+	name: "drip",
+	aliases: [],
+	cooldown: 1000 * 2,
+	description: "",
+	category: "manipulacao",
+	usage: "",
+
+	async run(client, message, args) {
+        const user = message.mentions.users.first() || message.author;
+        const img = `https://api.popcatdev.repl.co/drip?image=${user.displayAvatarURL({ dynamic: false, format: "png" })}`
+        const attachment = new Discord.MessageAttachment(img, `Drip_${user.username}.jpg`);
+        message.channel.send(attachment);
+  }
+}
