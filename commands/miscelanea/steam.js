2925c1c5a0e8e2ef4ddc8d2af8ac0f86b8197db9
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-26 14:32:13 -0300

    v2

--- commands/miscelanea/steam.js
+++ commands/miscelanea/steam.js
@@ -1,6 +1,4 @@
-const Discord = require('discord.js');
 var steam = require('steam-provider') 
-
 var provider = new steam.SteamProvider();
 
 module.exports = {
@@ -14,7 +12,26 @@ module.exports = {
     async run (client, message, args) {
                         
         let arg = args.join(' ') 
-        if(!arg) return message.channel.send(`> ${message.author}, Você precisa falar um jogo para eu pesquisar.`) 
+
+        if(!arg) {
+            const db = require("quick.db");
+            const Discord = require("discord.js");
+
+            let prefix = db.get(`prefix_${message.guild.id}`)
+            if (prefix === null) { prefix = "s." }
+  
+            const help = new Discord.MessageEmbed()
+            .setTitle("Comando da steam")
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
+            .setDescription("Busque por informações do seu jogo favorito da steam!")
+            .addField(`Forma de Utilização:`, ` \`${prefix}steam <game>\``)
+            .addField(`Exemplo:`, ` \`${prefix}steam csgo\``)
+            .setFooter(`Comando executado por: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
+            .setTimestamp();
+            return message.channel.send(help);
+
+        }
+
         provider.search(arg).then(result => { 
             provider.detail(result[0].id, 1, "portuguese", "pt").then(results => { 
                 let other = results.otherData 
