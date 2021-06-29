971749d0d80407d8e84b68b1c0d7ea75b16a2709
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-28 21:55:07 -0300

    Update discordjs.js

--- commands/miscelanea/discordjs.js
+++ commands/miscelanea/discordjs.js
@@ -1,23 +1,24 @@
-const Discord = require("discord.js")
-const fetch = require("node-fetch") 
-const db = require("quick.db")
+const fetch = require("node-fetch")
 
 module.exports = {
-    name: "discordjs",
-    aliases: ['docs', 'discord.js'],
-    cooldown: 1000 * 2, 
-    description: "Pesquisar algo nas docs do discord.js",
-    category: "outros",
-    usage: "<docs_name>",
+	name: "discordjs",
+	aliases: ['docs', 'discord.js'],
+	cooldown: 1000 * 2,
+	description: "Comando para pesquisar algo nas docs do discord.js",
+	category: "outros",
+	usage: "<docs_name>",
 
-async run (client, message, args) {
+	async run(client, message, args) {
 
-    const query = args.join("") 
+		const query = args.join("")
 
-    fetch(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${query}`) 
-    .then(res => res.json()) 
-    .then(json => {
-    message.channel.send({ embed: json }).catch(() => message.reply("Não achei nenhum resultado para sua pesquisa!"))
+		fetch(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${query}`)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send({
+					embed: json
+				}).catch(() => message.reply("Não achei nenhum resultado para sua pesquisa!"))
 
-}) 
-}}
+			})
+	}
+}
