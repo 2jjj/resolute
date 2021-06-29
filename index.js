637ac04828d3b670c2474c39164510df5d66cea4
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-29 10:43:34 -0300

    fds

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ client.on("message", async message => {
             .setTitle(`Comando de \`${command.name}\``)
             .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
             .setDescription(`${command.description}`)
+            .addField(`Aliases:`, ` \`${command.aliases}\``)
             .addField(`Forma de Utilização:`, ` \`${prefix}${command.name} ${command.usage}\``)
             .addField(`Exemplo:`, ` \`${command.example !== undefined ? `${prefix}${command.example}` : `Sem exemplos para este comando.` }\``)
             .setFooter(`Comando executado por: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
