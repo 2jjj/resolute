e527497cc2c0af55f264b2452e46f46b81c84187
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-29 13:31:25 -0300

    Update emojify.js

--- commands/diversao/emojify.js
+++ commands/diversao/emojify.js
@@ -5,27 +5,13 @@ module.exports = {
     name: "emojify",
     aliases: [],
     cooldown: 1000 * 2,
-    description: "Emojify, fale por emojis!",
+    description: ":regional_indicator_f::regional_indicator_a::regional_indicator_c::regional_indicator_a:   :regional_indicator_a::regional_indicator_s:   :regional_indicator_p::regional_indicator_a::regional_indicator_l::regional_indicator_a::regional_indicator_v::regional_indicator_r::regional_indicator_a::regional_indicator_s:   :regional_indicator_f::regional_indicator_i::regional_indicator_c::regional_indicator_a::regional_indicator_r::regional_indicator_e::regional_indicator_m:   :regional_indicator_a::regional_indicator_s::regional_indicator_s::regional_indicator_i::regional_indicator_m:",
     category: "fun",
     usage: "<texto>",
 
     async run(client, message, args) {
 
-        if (!args.length) {
-            let prefix = db.get(`prefix_${message.guild.id}`)
-            if (prefix === null) prefix = "s."
-
-            const help = new Discord.MessageEmbed()
-                .setTitle("Comando de doublestruck")
-                .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
-                .setDescription(":regional_indicator_f::regional_indicator_a::regional_indicator_c::regional_indicator_a:   :regional_indicator_a::regional_indicator_s:   :regional_indicator_p::regional_indicator_a::regional_indicator_l::regional_indicator_a::regional_indicator_v::regional_indicator_r::regional_indicator_a::regional_indicator_s:   :regional_indicator_f::regional_indicator_i::regional_indicator_c::regional_indicator_a::regional_indicator_r::regional_indicator_e::regional_indicator_m:   :regional_indicator_a::regional_indicator_s::regional_indicator_s::regional_indicator_i::regional_indicator_m:")
-                .addField(`Forma de Utilização:`, ` \`${prefix}emojify <texto>\``)
-                .setFooter(`Comando executado por: ${message.author.username}`, message.author.displayAvatarURL({
-                    dynamic: true
-                }))
-                .setTimestamp();
-            return message.channel.send(help);
-        }
+        if (!args[0]) return;
 
         const specialCodes = {
             '0': ':zero:',
