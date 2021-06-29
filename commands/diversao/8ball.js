92235b0f9a73017c37751af5d3af97360f2caff8
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-29 13:28:45 -0300

    Update 8ball.js

--- commands/diversao/8ball.js
+++ commands/diversao/8ball.js
@@ -15,21 +15,7 @@ module.exports = {
         let respostas = Respostas8Ball[Math.floor(Math.random() * Respostas8Ball.length)]
         let pergunta = args.join(" ")
 
-        if (!pergunta) {
-            let prefix = db.get(`prefix_${message.guild.id}`)
-            if (prefix === null) prefix = "s."
-
-            const help = new Discord.MessageEmbed()
-                .setTitle("Comando de 8ball")
-                .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
-                .setDescription("Faça um pergunta ao 8ball.")
-                .addField(`Forma de Utilização:`, ` \`${prefix}8ball <pergunta>\``)
-                .setFooter(`Comando executado por: ${message.author.username}`, message.author.displayAvatarURL({
-                    dynamic: true
-                }))
-                .setTimestamp();
-            return message.channel.send(help);
-        }
+        if (!pergunta) return;
 
         setTimeout(function () {
             message.inlineReply(respostas)
