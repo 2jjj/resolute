757a7df51d16ebd2c269512ba5d25b68ae50fc73
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-28 21:56:26 -0300

    Update color.js

--- commands/miscelanea/color.js
+++ commands/miscelanea/color.js
@@ -2,35 +2,36 @@ const Discord = require('discord.js')
 const fetch = require("node-fetch")
 
 module.exports = {
-    name: "color",
-    aliases: ['docs'],
-    cooldown: 1000 * 2, 
-    description: "Color HEX",
-    category: "outros",
-    usage: "",
+	name: "color",
+	aliases: ['docs'],
+	cooldown: 1000 * 2,
+	description: "Color HEX",
+	category: "outros",
+	usage: "<Cor>",
+    example: "color #000",
 
-async run (client, message, args) {
-        let color = args[0]
-        if (color.includes("#")) {
-            color = args[0].split("#")[1]
-        }
-        const url = (`https://api.alexflipnote.dev/colour/${color}`)
-        let json
-            try{
-                json = await fetch(url).then(res => res.json())
-            }
-            catch(e) {
-                return message.reply('Erro.')
-            }
-        if (json.description) return message.reply("Cor inválida.")
-        let embed = new Discord.MessageEmbed()
-        .setTitle(json.name)
-        .addField("RGB", json.rgb, true)
-        .addField("Brightness", json.brightness, true)
-        .addField("Hex", json.hex, true)
-        .setThumbnail(json.image)
-        .setImage(json.image_gradient, true)
-        .setColor(json.hex)
-        message.channel.send(embed)
-    }
+	async run(client, message, args) {
+
+		let color = args[0]
+		if (color.includes("#")) {
+			color = args[0].split("#")[1]
+		}
+		const url = (`https://api.alexflipnote.dev/colour/${color}`)
+		let json
+		try {
+			json = await fetch(url).then(res => res.json())
+		} catch (e) {
+			return message.reply('Erro.')
+		}
+		if (json.description) return message.reply("Cor inválida.")
+		let embed = new Discord.MessageEmbed()
+			.setTitle(json.name)
+			.addField("RGB", json.rgb, true)
+			.addField("Brightness", json.brightness, true)
+			.addField("Hex", json.hex, true)
+			.setThumbnail(json.image)
+			.setImage(json.image_gradient, true)
+			.setColor(json.hex)
+		message.channel.send(embed)
+	}
 }
