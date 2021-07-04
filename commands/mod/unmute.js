1bb76f0974b325895ad0a0ffdb043fc215e24a36
sprayx <sprayhasswag@gmail.com>
2021-07-04 13:41:11 -0300

    Update unmute.js

--- commands/mod/unmute.js
+++ commands/mod/unmute.js
@@ -9,9 +9,7 @@ module.exports = {
 
 	async run(client, message, args) {
 
-		if(!args[0]){
-			return;
-		}
+		if(!args[0]) return;
 
 		const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 		const role = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted');
