2a175e31cca6cb58f67d2344cdc517d7f41f5592
sprayx <sprayhasswag@gmail.com>
2021-07-05 14:57:32 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,9 +4,9 @@ ECHO Starting Lavalink
 ECHO ==========================
 start cmd /k java -jar ./Lavalink.jar
 ECHO ==========================
-@ECHO Taking a 10 Second Break for Lavalink
+@ECHO Taking a 5 Second Break for Lavalink
 ECHO ==========================
-timeout /T 10 /nobreak
+timeout /T 5 /nobreak
 ECHO ==========================
 @ECHO Starting BOT
 ECHO ==========================
