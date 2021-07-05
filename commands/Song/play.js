9156f56ce1f40bee001d9312e88648971ab5f140
sprayx <sprayhasswag@gmail.com>
2021-07-05 15:55:52 -0300

    Update play.js

--- commands/Song/play.js
+++ commands/Song/play.js
@@ -1,37 +1,43 @@
-const { MessageEmbed } = require(`discord.js`);
 const playermanager = require(`../../handlers/lavalink/playermanager`);
+
 module.exports = {
-    name: `play`,
-    category: `Song`,
-    aliases: [`p`],
-    description: `Plays a song from youtube`,
-    usage: `play <link/query>`,
-    run: async (client, message, args, cmduser, text, prefix) => {
+  name: "play",
+  aliases: [],
+  cooldown: 2000 * 2,
+  description: "",
+  category: "music",
+  usage: "",
+  example: "",
+
+  async run(client, message, args) {
+
       const { channel } = message.member.voice;
-      if (!channel) return message.channel.send(`:x: **You have to be in a voice channel to use this command.**`);
-      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **You cannot run this command while deafened**`);
+      if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+
+      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
       const botchannel = message.guild.me.voice.channel;
 
       const player = client.manager.players.get(message.guild.id);
       if(player && channel.id !== player.voiceChannel)
-        return message.channel.send(`**:x: You need to be in the same voice channel as Milrato x Rythm to use this command**`);
+        return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
+
       if(player && botchannel && channel.id !== botchannel.id){
         player.destroy();
       }
       if(message.content.includes("youtu")){
-        message.channel.send(`<:youtube:826100274095194132> **Searching** :mag_right: \`${args.join(" ")}\``)
+        message.channel.send(`<:youtube:826100274095194132> **Procurando** :mag_right: \`${args.join(" ")}\``)
         playermanager(client, message, args, `play:youtube`);
       } else if(message.content.includes("spotify")){
-        message.channel.send(`<:spotify:818555971873013761>**Searching** :mag_right: \`${args.join(" ")}\``)
+        message.channel.send(`<:spotify:818555971873013761>**Procurando** :mag_right: \`${args.join(" ")}\``)
         playermanager(client, message, args, `play:youtube`);
       } else if(message.content.includes("soundcloud")){
-        message.channel.send(`<:soundcloud:818555972079321128> **Searching** :mag_right: \`${args.join(" ")}\``)
+        message.channel.send(`<:soundcloud:818555972079321128> **Procurando** :mag_right: \`${args.join(" ")}\``)
         playermanager(client, message, args, `play:soundcloud`);
     } else if(message.content.includes("http")){
-      message.channel.send(`<:rythm:826519647347539990> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:rythm:826519647347539990> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     } else {
-      message.channel.send(`<:youtube:826100274095194132> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:youtube:826100274095194132> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     }
   }
