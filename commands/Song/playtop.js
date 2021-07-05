fab53720daa501fde1d559cd83a0e161b770e241
sprayx <sprayhasswag@gmail.com>
2021-07-05 15:41:19 -0300

    Update playtop.js

--- commands/Song/playtop.js
+++ commands/Song/playtop.js
@@ -12,29 +12,17 @@ module.exports = {
     const {
       channel
     } = message.member.voice;
-    if (!channel) return message.channel.send(`:x: **You have to be in a voice channel to use this command.**`);
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
     //send error if member is Deafed
-    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **You cannot run this command while deafened**`);
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
     const botchannel = message.guild.me.voice.channel;
     //if no args added return error message if allowed to send an embed
-    if (!args[0]) {
-      let string = `${prefix}playtop <link/query>`
-      let embed = new MessageEmbed()
-        .setTitle("**:x: Invalid usage**")
-        .setDescription(string)
-        .setColor("#ff0000")
-      if (message.guild.me.hasPermission("EMBED_LINKS")) {
-        message.channel.send(embed)
-      } else {
-        message.channel.send("**:x: Invalid usage**\n" + string)
-      }
-      return;
-    }
+
     ///get the player
     const player = client.manager.players.get(message.guild.id);
     //if user is not in the right channel as bot, then return error
     if (player && channel.id !== player.voiceChannel)
-      return message.channel.send(`**:x: You need to be in the same voice channel as Milrato x Rythm to use this command**`);
+      return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
     //if bot connected bot not with the lavalink player then try to delete the player
     if (player && botchannel && channel.id !== botchannel.id) {
       player.destroy();
@@ -42,30 +30,30 @@ module.exports = {
     //IF YOUTUBE SEND INFO WITH YOUTUBE
     if (message.content.includes("youtu")) {
       //send searching
-      message.channel.send(`<:youtube:826100274095194132> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:youtube:826100274095194132> **Procurando** :mag_right: \`${args.join(" ")}\``)
       //play the song from our playermanager
       playermanager(client, message, args, `play:youtube`);
       //IF SPOTIFY SEARCH SEND INFO WITH SPOTIFY
     } else if (message.content.includes("spotify")) {
       //send searching
-      message.channel.send(`<:spotify:818555971873013761>**Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:spotify:818555971873013761>**Procurando** :mag_right: \`${args.join(" ")}\``)
       //play the song from our playermanager
       playermanager(client, message, args, `play:youtube`);
       //IF SOUNDCLOUD SEARCH SEND INFO WITH SOUNDCLOUD
     } else if (message.content.includes("soundcloud")) {
       //send searching
-      message.channel.send(`<:soundcloud:818555972079321128> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:soundcloud:818555972079321128> **Procurando** :mag_right: \`${args.join(" ")}\``)
       //play the song from our playermanager
       playermanager(client, message, args, `play:soundcloud`);
       //ELSE SEND RYTHM INFO
     } else if (message.content.includes("http")) {
       //send searching
-      message.channel.send(`<:rythm:826519647347539990> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:rythm:826519647347539990> **Procurando** :mag_right: \`${args.join(" ")}\``)
       //play the song from our playermanager
       playermanager(client, message, args, `play:youtube`);
     } else {
       //send searching
-      message.channel.send(`<:youtube:826100274095194132> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:youtube:826100274095194132> **Procurando** :mag_right: \`${args.join(" ")}\``)
       //play the song from our playermanager
       playermanager(client, message, args, `play:youtube`);
     }
