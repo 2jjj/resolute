077468b2895e7b042165680d63507a44184bb26d
sprayx <sprayhasswag@gmail.com>
2021-07-05 15:54:47 -0300

    Update pause.js

--- commands/Song/pause.js
+++ commands/Song/pause.js
@@ -8,13 +8,19 @@ module.exports = {
   example: "",
 
   async run(client, message, args) {
+
       const { channel } = message.member.voice;
+
       if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+
       if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+
       const botchannel = message.guild.me.voice.channel;
       const player = client.manager.players.get(message.guild.id);
+
       if(!player || !botchannel) return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
       if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
+
       if(player && channel.id !== player.voiceChannel)
         return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
       if(player && botchannel && channel.id !== botchannel.id){
