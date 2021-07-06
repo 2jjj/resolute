5410b2818821bda103f96e3802efd6976aa529b3
sprayx <sprayhasswag@gmail.com>
2021-07-06 15:11:55 -0300

    Update soundcloud.js

--- commands/Song/soundcloud.js
+++ commands/Song/soundcloud.js
@@ -23,20 +23,19 @@ module.exports = {
       player.destroy();
     }
     if (message.content.includes("youtu")) {
-      message.channel.send(`<:youtube:826100274095194132> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:yt:861682089049325598> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     } else if (message.content.includes("spotify")) {
-      message.channel.send(`<:spotify:818555971873013761>**Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:spotify:861682475809505310> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     } else if (message.content.includes("soundcloud")) {
-      message.channel.send(`<:soundcloud:818555972079321128> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:spr4y:861682475393482764> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:soundcloud`);
     } else if (message.content.includes("http")) {
-      message.channel.send(`<:rythm:826519647347539990> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`🎵 **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:soundcloud`);
     } else {
-      message.channel.send(`<:soundcloud:818555972079321128> **Searching** :mag_right: \`${args.join(" ")}\``)
+      message.channel.send(`<:yt:861682089049325598> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:soundcloud`);
-    }
-  }
+    }}
 };
