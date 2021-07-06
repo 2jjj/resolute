06ed629826c46e60bc317e2154a13404374bceec
sprayx <sprayhasswag@gmail.com>
2021-07-06 14:21:27 -0300

    v2

--- commands/Song/play.js
+++ commands/Song/play.js
@@ -11,29 +11,33 @@ module.exports = {
 
   async run(client, message, args) {
 
-      const { channel } = message.member.voice;
-      if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (!args[0]) return;
 
-      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
-      const botchannel = message.guild.me.voice.channel;
+    const { channel } = message.member.voice;
 
-      const player = client.manager.players.get(message.guild.id);
-      if(player && channel.id !== player.voiceChannel)
-        return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
 
-      if(player && botchannel && channel.id !== botchannel.id){
-        player.destroy();
-      }
-      if(message.content.includes("youtu")){
-        message.channel.send(`<:yt:861682089049325598> **Procurando** :mag_right: \`${args.join(" ")}\``)
-        playermanager(client, message, args, `play:youtube`);
-      } else if(message.content.includes("spotify")){
-        message.channel.send(`<:spotify:861682475809505310> **Procurando** :mag_right: \`${args.join(" ")}\``)
-        playermanager(client, message, args, `play:youtube`);
-      } else if(message.content.includes("soundcloud")){
-        message.channel.send(`<:spr4y:861682475393482764> **Procurando** :mag_right: \`${args.join(" ")}\``)
-        playermanager(client, message, args, `play:soundcloud`);
-    } else if(message.content.includes("http")){
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+    const botchannel = message.guild.me.voice.channel;
+
+    const player = client.manager.players.get(message.guild.id);
+    if (player && channel.id !== player.voiceChannel)
+      return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
+
+    if (player && botchannel && channel.id !== botchannel.id) {
+      player.destroy();
+    }
+    
+    if (message.content.includes("youtu")) {
+      message.channel.send(`<:yt:861682089049325598> **Procurando** :mag_right: \`${args.join(" ")}\``)
+      playermanager(client, message, args, `play:youtube`);
+    } else if (message.content.includes("spotify")) {
+      message.channel.send(`<:spotify:861682475809505310> **Procurando** :mag_right: \`${args.join(" ")}\``)
+      playermanager(client, message, args, `play:youtube`);
+    } else if (message.content.includes("soundcloud")) {
+      message.channel.send(`<:spr4y:861682475393482764> **Procurando** :mag_right: \`${args.join(" ")}\``)
+      playermanager(client, message, args, `play:soundcloud`);
+    } else if (message.content.includes("http")) {
       message.channel.send(`🎵 **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     } else {
