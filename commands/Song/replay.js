ac1715a749a42223d55a631e6779e2f28e961722
sprayx <sprayhasswag@gmail.com>
2021-07-06 15:17:56 -0300

    Update replay.js

--- commands/Song/replay.js
+++ commands/Song/replay.js
@@ -1,10 +1,14 @@
 module.exports = {
-  name: `replay`,
-  category: `Song`,
-  aliases: [``],
-  description: `Resets the progress of the current song.`,
-  usage: `replay`,
-  run: async (client, message, args, cmduser, text, prefix) => {
+  name: "replay",
+  aliases: [],
+  cooldown: 2000 * 2,
+  description: "",
+  category: "music",
+  usage: "",
+  example: "",
+
+  async run(client, message, args) {
+
       const { channel } = message.member.voice;
       if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
       if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
@@ -17,7 +21,7 @@ module.exports = {
 
       if(player && channel.id !== player.voiceChannel)
         return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
-        
+
       player.seek(0);
       return message.channel.send(`**:musical_note: O tempo do som foi resetado :track_previous:**`);
   }
