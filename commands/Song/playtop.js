2efdc6acc5836cb95b5ac9e59af2891d2538704f
sprayx <sprayhasswag@gmail.com>
2021-07-06 15:08:17 -0300

    v2

--- commands/Song/playtop.js
+++ commands/Song/playtop.js
@@ -12,48 +12,33 @@ module.exports = {
     const { channel } = message.member.voice;
 
     if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
-    //send error if member is Deafed
+
     if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
     const botchannel = message.guild.me.voice.channel;
-    //if no args added return error message if allowed to send an embed
 
-    ///get the player
     const player = client.manager.players.get(message.guild.id);
-    //if user is not in the right channel as bot, then return error
+
     if (player && channel.id !== player.voiceChannel)
       return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
-    //if bot connected bot not with the lavalink player then try to delete the player
-    if (player && botchannel && channel.id !== botchannel.id) {
+
+      if (player && botchannel && channel.id !== botchannel.id) {
       player.destroy();
     }
-    //IF YOUTUBE SEND INFO WITH YOUTUBE
+
     if (message.content.includes("youtu")) {
-      //send searching
-      message.channel.send(`<:youtube:826100274095194132> **Procurando** :mag_right: \`${args.join(" ")}\``)
-      //play the song from our playermanager
+      message.channel.send(`<:yt:861682089049325598> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
-      //IF SPOTIFY SEARCH SEND INFO WITH SPOTIFY
     } else if (message.content.includes("spotify")) {
-      //send searching
-      message.channel.send(`<:spotify:818555971873013761>**Procurando** :mag_right: \`${args.join(" ")}\``)
-      //play the song from our playermanager
-      playermanager(client, message, args, `play:youtube`);
-      //IF SOUNDCLOUD SEARCH SEND INFO WITH SOUNDCLOUD
+      message.channel.send(`<:spotify:861682475809505310> **Procurando** :mag_right: \`${args.join(" ")}\``)
+      playermanager(client, message, args, `play:spotify`);
     } else if (message.content.includes("soundcloud")) {
-      //send searching
-      message.channel.send(`<:soundcloud:818555972079321128> **Procurando** :mag_right: \`${args.join(" ")}\``)
-      //play the song from our playermanager
+      message.channel.send(`<:spr4y:861682475393482764> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:soundcloud`);
-      //ELSE SEND RYTHM INFO
     } else if (message.content.includes("http")) {
-      //send searching
-      message.channel.send(`<:rythm:826519647347539990> **Procurando** :mag_right: \`${args.join(" ")}\``)
-      //play the song from our playermanager
+      message.channel.send(`🎵 **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     } else {
-      //send searching
-      message.channel.send(`<:youtube:826100274095194132> **Procurando** :mag_right: \`${args.join(" ")}\``)
-      //play the song from our playermanager
+      message.channel.send(`<:yt:861682089049325598> **Procurando** :mag_right: \`${args.join(" ")}\``)
       playermanager(client, message, args, `play:youtube`);
     }
   }
--- commands/Song/replay.js
+++ commands/Song/replay.js
@@ -5,26 +5,16 @@ module.exports = {
   description: `Resets the progress of the current song.`,
   usage: `replay`,
   run: async (client, message, args, cmduser, text, prefix) => {
-      //get the voice channel of the member
       const { channel } = message.member.voice;
-      //if he is not connected to a vc return error
       if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
-      //send error if member is Deafed
       if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
-      //get voice channel of the bot
       const botchannel = message.guild.me.voice.channel;
-      //get the music player
       const player = client.manager.players.get(message.guild.id);
-      //if no player or no botchannel return error
       if(!player || !botchannel) return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
-      //if queue size too small return error
       if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
-      //if user is not in the right channel as bot, then return error
       if(player && channel.id !== player.voiceChannel)
         return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
-      //seek to the new Seek position
       player.seek(0);
-      //Send Success Message
       return message.channel.send(`**:musical_note: O tempo do som foi resetado :track_previous:**`);
   }
 };
