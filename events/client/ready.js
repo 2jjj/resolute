a3578f86dfa1cacaa268fb1c726690eb48253a85
sprayx <sprayhasswag@gmail.com>
2021-07-06 11:26:34 -0300

    fix

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,7 +4,7 @@ module.exports = client => {
     console.log("\n")
     console.log(`     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`.bold.brightGreen)
     console.log(`     ┃ `.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃".bold.brightGreen)
-    console.log(`     ┃ `.bold.brightGreen + `Discord Bot is online!`.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length-`Discord Bot is online!`.length)+ "┃".bold.brightGreen)
+    console.log(`     ┃ `.bold.brightGreen + `Resolute está online!`.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length-`Resolute está online!`.length)+ "┃".bold.brightGreen)
     console.log(`     ┃ `.bold.brightGreen + ` /--/ ${client.user.tag} /--/ `.bold.brightGreen+ " ".repeat(-1+stringlength-` ┃ `.length-` /--/ ${client.user.tag} /--/ `.length)+ "┃".bold.brightGreen)
     console.log(`     ┃ `.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃".bold.brightGreen)
     console.log(`     ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`.bold.brightGreen)
