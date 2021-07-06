abd7143806e39eeb055e1d36399e9d0e4a17e28d
sprayx <sprayhasswag@gmail.com>
2021-07-06 15:15:38 -0300

    Update replay.js

--- commands/Song/replay.js
+++ commands/Song/replay.js
@@ -8,12 +8,16 @@ module.exports = {
       const { channel } = message.member.voice;
       if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
       if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+
       const botchannel = message.guild.me.voice.channel;
       const player = client.manager.players.get(message.guild.id);
+
       if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
       if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+
       if(player && channel.id !== player.voiceChannel)
         return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
+        
       player.seek(0);
       return message.channel.send(`**:musical_note: O tempo do som foi resetado :track_previous:**`);
   }
