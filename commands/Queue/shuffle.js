9367314f82b7003c3c3eadc783191a3342df42de
sprayx <sprayhasswag@gmail.com>
2021-07-06 15:13:49 -0300

    Update shuffle.js

--- commands/Queue/shuffle.js
+++ commands/Queue/shuffle.js
@@ -13,8 +13,8 @@ module.exports = {
       if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver surdo**`);
       const botchannel = message.guild.me.voice.channel;
       const player = client.manager.players.get(message.guild.id);
-      if(!player || !botchannel) return message.channel.send(`**:x: Nothing playing in this server**`);
-      if(!player.current < 1) return message.channel.send(`**:x: Nothing playing in this server**`);
+      if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+      if(!player.current < 1) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
       if(player && channel.id !== player.voiceChannel)
         return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
       if(player && botchannel && channel.id !== botchannel.id){
