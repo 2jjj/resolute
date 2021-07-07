e9214b91f2c3eba547658655b8c9f51ccebe17a6
sprayx <sprayhasswag@gmail.com>
2021-07-07 16:57:23 -0300

    Update grab.js

--- commands/music/grab.js
+++ commands/music/grab.js
@@ -1,5 +1,9 @@
-const { MessageEmbed } = require(`discord.js`);
-const { format } = require(`../../handlers/functions`);
+const {
+  MessageEmbed
+} = require(`discord.js`);
+const {
+  format
+} = require(`../../handlers/functions`);
 
 module.exports = {
   name: `grab`,
@@ -13,21 +17,21 @@ module.exports = {
   async run(client, message, args) {
 
     if (!args[0]) return;
-    
+
     const { channel } = message.member.voice;
-    if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
 
-    if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+
     const botchannel = message.guild.me.voice.channel;
-
     const player = client.manager.players.get(message.guild.id);
-    if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-
+    
+    if (!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
     if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-    if(player && channel.id !== player.voiceChannel) {
+    if (player && channel.id !== player.voiceChannel) {
       return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
     }
-    
+
     let date = `${new Date().getFullYear()}-${String(new Date().getMonth()).length ==1 ? "0" + new Date().getMonth() : new Date().getMonth()}-${String(new Date().getDate()).length ==1 ? "0" + new Date().getDate() : new Date().getDate()}`;
     message.author.send(new MessageEmbed()
       .setThumbnail(`https://img.youtube.com/vi/${player.queue.current.identifier}/mqdefault.jpg`)
@@ -36,10 +40,9 @@ module.exports = {
       .setTitle("Música salva :musical_note:")
       .setDescription(`[${player.queue.current.title.split("[").join("\[").split("]").join("\]")}](${player.queue.current.uri})\n\n\`Duração:\` ${format(player.queue.current.duration).split(" | ")[0]}\n\n\`Requisitado por:\` ${player.queue.current.requester.username} (${player.queue.current.requester.tag})`)
       .setFooter(`${date} - ${message.guild.name}`)
-    ).catch(e=>{
+    ).catch(e => {
       return message.channel.send("**:x: Sua DM está desabilitada**")
-    })    
-
-    message.react("📭").catch(e=>console.log("Could not react"))
+    })
+    message.react("📭").catch(e => console.log("Could not react"))
   }
 };
