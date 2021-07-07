a72494956fe16e20ccd31d5cf6296d841262bf0c
sprayx <sprayhasswag@gmail.com>
2021-07-07 17:38:27 -0300

    v2

--- events/guild/guildDelete.js
+++ events/guild/guildDelete.js
@@ -1,16 +1,13 @@
+const Discord = require("discord.js")
+const guildSchema = require('../../database/mongoDB/guild.js');
+
 module.exports = async (client, guild) => {
 
-    const guilds = await client.shard.broadcastEval("this.guilds.cache.size")
-    const botGuilds = guilds.reduce((prev, val) => prev + val)
-    client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${botGuilds}`).catch((e) => console.log((e)))
-    client.channels.cache.get("855968171855839242").setName(`💻│Shards: ${config.shards}`).catch((e) => console.log((e)))
-
-    const guildSchema = require('../../database/mongoDB/guild.js');
-
+    const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
+    
     guildSchema.findOneAndDelete({
         Guild: guild.id
     }, async (err, data) => {
-
         if (err) return console.log(err)
     })
 
@@ -18,35 +15,38 @@ module.exports = async (client, guild) => {
 
     client.shard.broadcastEval(`
     (async () => {
-    let channel = this.channels.cache.get("841393455694872597")
-    const webhooks = await channel.fetchWebhooks(); 		
-    const webhook = webhooks.first();
-    
-    let embed = {
-    color: "#5B00FF",
-    author: {
-     name: \`Fui removido de um servidor | (${guild.name}/${guild.id})\`,
-    icon_url: \`${icon}\`,
-    },
-    thumbnail: {
-    url: \`${icon}\`,
-    },
-    fields: [
-    {
-    name: \`Nome:\`,
-    value: \`(\\\`${guild.name}/${guild.id}\\\`)\`,
-    },
-    {
-    name: \`Total de membros\`,
-    value: \`${guild.memberCount}\`,
-    },
-    ],
-    timestamp: new Date(),
-    footer: {
-    text: \`${guild.id}\`, 
-    icon_url: \`${icon}\`, 	
-    },
-    }
-    webhook.send({embeds: [embed] })
+        let channel = this.channels.cache.get("841393455694872597")
+        const webhooks = await channel.fetchWebhooks(); 		
+        const webhook = webhooks.first();
+        let embed = {
+        color: "#5B00FF",
+        author: {
+         name: \`Sai de um servidor | (${guild.name}/${guild.id})\`,
+        icon_url: \`${icon}\`,
+        },
+        thumbnail: {
+        url: \`${icon}\`,
+        },
+        fields: [
+        {
+        name: \`Nome:\`,
+        value: \`(\\\`${guild.name}/${guild.id}\\\`)\`,
+        },
+        {
+        name: \`Dono:\`,
+        value: \`(\\\`${guild.owner.user.tag}/${guild.owner.id}\\\`)\`,
+        },
+        {
+        name: \`Total de membros\`,
+        value: \`${guild.memberCount}\`,
+        },
+        ],
+        timestamp: new Date(),
+        footer: {
+        text: \`${guild.id}\`, 
+        icon_url: \`${icon}\`, 	
+        },
+        }
+        webhook.send({embeds: [embed] })
     })()`, 0)
 };
