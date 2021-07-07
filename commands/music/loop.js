ead22ec73bc03a8dbb56d4954b6b8c6f3780cda5
sprayx <sprayhasswag@gmail.com>
2021-07-07 16:58:50 -0300

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -9,27 +9,26 @@ module.exports = {
 
   async run(client, message, args) {
 
-      if (!args[0]) return;
+    if (!args[0]) return;
 
-      const { channel } = message.member.voice;
-      if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    const { channel } = message.member.voice;
 
-      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
 
-      const botchannel = message.guild.me.voice.channel;
-      const player = client.manager.players.get(message.guild.id);
+    const botchannel = message.guild.me.voice.channel;
+    const player = client.manager.players.get(message.guild.id);
 
-      if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-      if(!player.current < 1) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+    if (!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+    if (!player.current < 1) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
 
-      if(player && channel.id !== player.voiceChannel)
-        return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
-      
-      if(player && botchannel && channel.id !== botchannel.id){
-        player.destroy();
-      }
+    if (player && channel.id !== player.voiceChannel)
+      return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
 
-      player.setTrackRepeat(!player.trackRepeat);
-      return message.channel.send(`**${player.trackRepeat ? `:repeat_one: Habilitado` : `:repeat_one: Desabilitado`}**`);
+    if (player && botchannel && channel.id !== botchannel.id) {
+      player.destroy();
+    }
+    player.setTrackRepeat(!player.trackRepeat);
+    return message.channel.send(`**${player.trackRepeat ? `:repeat_one: Habilitado` : `:repeat_one: Desabilitado`}**`);
   }
 };
