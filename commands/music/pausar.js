37d4b9f47d3e2102c44f470a02268657f79c228a
sprayx <sprayhasswag@gmail.com>
2021-07-07 16:59:52 -0300

    Update pausar.js

--- commands/music/pausar.js
+++ commands/music/pausar.js
@@ -9,29 +9,27 @@ module.exports = {
 
   async run(client, message, args) {
 
-      if(!args[0]) return;
+    if (!args[0]) return;
 
-      const { channel } = message.member.voice;
+    const { channel } = message.member.voice;
 
-      if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
 
-      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+    const botchannel = message.guild.me.voice.channel;
+    const player = client.manager.players.get(message.guild.id);
 
-      const botchannel = message.guild.me.voice.channel;
-      const player = client.manager.players.get(message.guild.id);
+    if (!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+    if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
 
-      if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-      if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-
-      if(player && channel.id !== player.voiceChannel)
-        return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
-      if(player && botchannel && channel.id !== botchannel.id){
-        player.destroy();
-      }
-      if (!player.playing)
-        return message.channel.send(`**:x: O Player já está pausado.**`);
-      player.pause(true);
-     return message.channel.send(`**Pausado :pause_button:**`);
-
+    if (player && channel.id !== player.voiceChannel)
+      return message.channel.send(`**:x: Não há nada tocando neste servidor**`);
+    if (player && botchannel && channel.id !== botchannel.id) {
+      player.destroy();
+    }
+    if (!player.playing)
+      return message.channel.send(`**:x: O Player já está pausado.**`);
+    player.pause(true);
+    return message.channel.send(`**Pausado :pause_button:**`);
   }
 };
