79bcaea76550b1ce0c6f3a6a5d6cc7a4676f60c6
sprayx <sprayhasswag@gmail.com>
2021-07-07 16:56:59 -0300

    Update disconnect.js

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -11,21 +11,20 @@ module.exports = {
 
     if (!args[0]) return;
 
-      const { channel } = message.member.voice;
+    const { channel } = message.member.voice;
 
-      if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
-      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver silenciado**`);
 
-      const botchannel = message.guild.me.voice.channel;
-      const player = client.manager.players.get(message.guild.id);
+    const botchannel = message.guild.me.voice.channel;
+    const player = client.manager.players.get(message.guild.id);
 
-      if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-      if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-      
-      if(player && channel.id !== player.voiceChannel){
-        return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para poder ultilizar este comando**`);
-      }
+    if (!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+    if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
 
-      return message.channel.send(`**:mailbox_with_no_mail: Desconectado com sucesso!**`).catch(e=>console.log(e))
+    if (player && channel.id !== player.voiceChannel) {
+      return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para poder ultilizar este comando**`);
+    }
+    return message.channel.send(`**:mailbox_with_no_mail: Desconectado com sucesso!**`).catch(e => console.log(e))
   }
 };
