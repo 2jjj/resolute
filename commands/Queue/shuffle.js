1fbe4e88724d85de82f01ffc83709fdc68b94fe1
sprayx <sprayhasswag@gmail.com>
2021-07-07 16:54:49 -0300

    v2

--- commands/Queue/shuffle.js
+++ commands/Queue/shuffle.js
@@ -8,21 +8,25 @@ module.exports = {
   example: "",
 
   async run(client, message, args) {
-      const { channel } = message.member.voice;
-      if (!channel)  return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
-      if(message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver surdo**`);
-      const botchannel = message.guild.me.voice.channel;
-      const player = client.manager.players.get(message.guild.id);
-      if(!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-      if(!player.current < 1) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
-      if(player && channel.id !== player.voiceChannel)
-        return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
-      if(player && botchannel && channel.id !== botchannel.id){
-        player.destroy();
-      }
-      player.set(`beforeshuffle`, player.queue.map(track => track));
-      player.queue.shuffle();
-     return message.channel.send(`**:boom: Liberado... :stop_button:**`);
+    const { channel } = message.member.voice;
+
+    if (!channel) return message.channel.send(`:x: **Você precisa estar em um canal de voz para usar este comando.**`);
+    if (message.member.voice.selfDeaf) return message.channel.send(`:x: **Você não pode executar este comando enquanto estiver surdo**`);
+
+    const botchannel = message.guild.me.voice.channel;
+    const player = client.manager.players.get(message.guild.id);
+
+    if (!player || !botchannel) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+    if (!player.current < 1) return message.channel.send(`**:x: Não a nada tocando neste servidor**`);
+    
+    if (player && channel.id !== player.voiceChannel)
+      return message.channel.send(`**:x: Você precisa estar no mesmo canal de voz que eu para usar este comando**`);
+    if (player && botchannel && channel.id !== botchannel.id) {
+      player.destroy();
+    }
+    player.set(`beforeshuffle`, player.queue.map(track => track));
+    player.queue.shuffle();
+    return message.channel.send(`**:boom: Liberado... :stop_button:**`);
 
   }
 };
