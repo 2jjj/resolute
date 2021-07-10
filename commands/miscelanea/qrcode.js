c6c10577baa20f786b59bbfee67b7c6ae0dd7b43
sprayx <sprayhasswag@gmail.com>
2021-07-10 15:07:05 -0300

    sim qrcode

---
+++ commands/miscelanea/qrcode.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: "qrcode",
+	aliases: [],
+	cooldown: 1000 * 2,
+	description: "Consiga um qrcode para seu link.",
+	category: "outros",
+	usage: "<link>",
+	example: "qrcode https://resolutebot.xyz",
+
+
+	async run(client, message, args) {
+    
+        let qrlink = `http://api.qrserver.com/v1/create-qr-code/?data=${args[0]}&size=200x200`
+
+        if (!args[0]) return;
+
+        if (require('is-url')(args[0])) {
+            const attachment = new Discord.MessageAttachment(qrlink, 'qrcode.png');
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Aqui está!')
+            .attachFiles(attachment)
+            .setImage('attachment://qrcode.png')
+            .setFooter(`Requisitado por ${message.author.username}`,  message.author.displayAvatarURL({ dynamic: true }))
+            .setTimestamp()
+
+            message.channel.send(embed)
+
+        } else {
+            message.channel.send("URL inválida.")
+         }
+    }
+}
