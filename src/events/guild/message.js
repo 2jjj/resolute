a329d1aa5f44094f05f9ccb3b5087285c7ef6e7b
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-13 13:09:59 -0300

    Update message.js

--- src/events/guild/message.js
+++ src/events/guild/message.js
@@ -61,6 +61,7 @@ module.exports = async (client, message) => {
         .addField(`:bulb: Modos de Uso:`, ` \`${command.usage.length !== 0 ? `${prefix}${command.name} ${command.usage}` : `${command.name}` }\``)
         .addField(`:thinking: Exemplo:`, ` \`${command.example !== undefined ? `${prefix}${command.name} ${command.example}` : `Sem exemplos para este comando.` }\``)
         .addField(`🔹 Aliases:`, ` \`${command.aliases.length !== 0 ? `${command.aliases}` : `Sem sinonimos para este comando.` }\``)
+        .addField(`🔹 Permissões necessárias:`, ` \`${command.permissoes.length !== 0 ? `${command.permissoes}` : `Não é necessário nenhuma permissão!` }\``)
         .setFooter(`Requisitado por: ${message.author.username}`, message.author.displayAvatarURL({
           dynamic: true
         }))
