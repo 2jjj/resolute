eeb21fa3dd89d6063fbd10c34c9efe82918660ee
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-13 13:11:20 -0300

    REMOVENDO STATUS

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,30 +1,4 @@
 module.exports = client => {
-  /*
-  const promises = [client.shard.fetchClientValues('guilds.cache.size'),
-    client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)')
-  ];
-  Promise.all(promises)
-    .then(async results => {
-      const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-      const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-
-      const status = [{
-          name: `resolutebot.xyz | s.help`,
-          type: 'PLAYING'
-        },
-        {
-          name: `resolutebot.xyz | Lavalink Online | s.play`,
-          type: 'PLAYING'
-        },
-      ]
-
-      function Presence() {
-        const base = status[Math.floor(Math.random() * status.length)]
-        client.user.setActivity(base)
-      }
-      Presence();
-      setInterval(() => Presence(), 5000)
-    })*/
   const stringlength = 69;
   console.log("\n")
   console.log(`     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`.bold.brightGreen)
