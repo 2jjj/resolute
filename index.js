c01311bb9a80b179c485c56527d0f7de5d4d211a
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-14 12:14:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -43,29 +43,23 @@ client.on("guildMemberAdd", async (member) => {
 
 client.on('shardReady', (shardid) => {
 
-  const promises = [client.shard.fetchClientValues('guilds.cache.size'),
-    client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)')
-  ];
-  Promise.all(promises)
-    .then(async results => {
-      const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-      const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-      const status = [{
-          name: `Online | Shard: ${shardid}`,
-          type: 'PLAYING'
-        },
-        {
-          name: `www.resolutebot.xyz | Shard: ${shardid}`,
-          type: 'PLAYING'
-        },
-      ]
-      function Presence() {
-        const base = status[Math.floor(Math.random() * status.length)]
-        client.user.setActivity(base)
-      }
-      Presence();
-      setInterval(() => Presence(), 5000)
-    })
+  const status = [{
+      name: `Online | Shard: ${shardid}`,
+      type: 'PLAYING'
+    },
+    {
+      name: `www.resolutebot.xyz | Shard: ${shardid}`,
+      type: 'PLAYING'
+    },
+  ]
+
+  function Presence() {
+    const base = status[Math.floor(Math.random() * status.length)]
+    client.user.setActivity(base)
+  }
+
+  Presence();
+  setInterval(() => Presence(), 5000)
 
   /*client.user.setActivity(`Online | Shard: ${shardid}`, {
     shardID: shardid
