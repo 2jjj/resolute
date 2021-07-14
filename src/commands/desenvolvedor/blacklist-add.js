42d24673cfe3b6087616aec41c10b2641e6fc8e5
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-14 14:14:48 -0300

    Update blacklist-add.js

--- src/commands/desenvolvedor/blacklist-add.js
+++ src/commands/desenvolvedor/blacklist-add.js
@@ -1,5 +1,7 @@
 const blacklist = require('../.././database/mongoDB/blacklist')
-const { Message } = require('discord.js')
+const {
+    Message
+} = require('discord.js')
 
 module.exports = {
     name: "blacklist",
@@ -7,23 +9,27 @@ module.exports = {
     category: "dev",
     description: "",
 
-    async run (client, message, args) {
+    async run(client, message, args) {
 
-        if(message.author.id !== '836345581424738354') return message.channel.send('não.')
+        if (message.author.id !== '836345581424738354') return message.channel.send('não.')
         const User = message.guild.members.cache.get(args[0])
-        if(!User) return message.channel.send('O ID do usuário está inválido.')
+        if (!User) return message.channel.send('O ID do usuário está inválido.')
 
-        blacklist.findOne({ id : User.user.id }, async(err, data) => {
-            if(err) throw err;
-            if(data) {
+        blacklist.findOne({
+            id: User.user.id
+        }, async (err, data) => {
+            if (err) throw err;
+            if (data) {
                 message.channel.send(`**${User.displayName}** já está na blacklist.`)
             } else {
-                data = new blacklist({ id : User.user.id })
+                data = new blacklist({
+                    id: User.user.id
+                })
                 data.save()
-                .catch(err => console.log(err))
-            message.channel.send(`${User.user.tag} foi adicionando na blacklist.`)
+                    .catch(err => console.log(err))
+                message.channel.send(`${User.user.tag} foi adicionando na blacklist.`)
             }
-           
+
         })
     }
 }
