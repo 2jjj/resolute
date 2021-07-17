5d1a2a3dd107cba1f55f8ca1dba4cef389e0ec2e
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-17 18:53:55 -0300

    Update botinfo.js

--- src/commands/information/botinfo.js
+++ src/commands/information/botinfo.js
@@ -19,21 +19,21 @@ module.exports = {
     .then(async results => { 	
         const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0); 	
         const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);   
-        //**//
+        
         let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
         let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
         let { version } = require("discord.js");
-        //**//
+
         let embed = new Discord.MessageEmbed()
         .setTitle("**Minhas informações!**")
         .setColor("RANDOM")
         .setThumbnail(avatar)
-        .setDescription(`Me chamo Resolute, fui criado no dia **11 de outubro de 2020** pelo **${config.spray}** na linguagem **Javascript** com o discord.js\n\n**Algumas informações:
-        <:early_developer_badge:854716150076538901> **|** Developer: ${config.spray}
+        .setDescription(`Informações:
+        <:early_developer_badge:854716150076538901> **|** Desenvolvedor: ${config.spray}
         <:pontin:852197383974551582> **|** Estou em ${totalGuilds} servidores.
-        <:pontin:852197383974551582> **|** ${totalMembers} de usuários.
+        <:pontin:852197383974551582> **|** ${totalMembers} usuários.
         <:cpu:854137097521987624> **|** Shard: ${message.guild.shard.id}/${config.shards}
-        <:cpu:854137097521987624> **|** Shards: ${config.shards}
+        <:cpu:854137097521987624> **|** Shards: ${config.shards
         <:memoryram:854135087037153280> **|** Memória ultilizada: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB
         <:ybs_bot:851955104345227294> **|** [Me adicione!](https://invite.resolutebot.xyz)
         **`)
