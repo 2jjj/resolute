4601555beccacd8abd532d7042dfed199d3cb959
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-16 22:30:47 -0300

    Eventos de adicionar em servidor.

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -2,28 +2,14 @@ const Discord = require("discord.js")
 const config = require("../../../config/config.json")
 
 module.exports = async (client, guild, message) => {
-    //https://discord.com/api/webhooks/841393653155102740/vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ
-    const webhook = new Discord.WebhookClient(`841393653155102740`, "vvccMKHnmDK-vNbGNQN98telbtrzCQyQU4fQaJnKG6OKocU5Ht_xfsWtL12LoEex8jQJ")
-
-    /*
-    const guilds = await client.shard.broadcastEval("this.guilds.cache.size"),
-    users = await client.shard.broadcastEval("this.users.cache.size");
-
-    const botGuilds = guilds.reduce((prev, val) => prev + val),
-    botUsers = users.reduce((prev, val) => prev + val);
-
-    //client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${botGuilds}`).catch((e) => console.log((e)))
-    //client.channels.cache.get("862771418484047892").setName(`📝│Users: ${botUsers}`).catch((e) => console.log((e)))
-    //client.channels.cache.get("855968171855839242").setName(`💻│Shards: ${config.shards}`).catch((e) => console.log((e)))
-
-    */
-
+    let canal = client.channels.cache.get("841393455694872597")
+    
     const dono = await client.guilds.fetch(guild.id).then(guilda => client.users.fetch(guilda.ownerID).then(o => `${o.tag} (${o.id})`))
     let embed = new Discord.MessageEmbed()
-        .setTitle(`Fui adicionado em um servidor!`)
+        .setTitle(`Novo servidor!`)
         .setDescription(`<:info:835206734225473546> | Servidor: ${guild.name}\n<:info:835206734225473546> | ID: ${guild.id}\n<:info:835206734225473546> | Membros: ${guild.memberCount} membros\n<:info:835206734225473546> | Dono: ${dono}`)
         .setThumbnail(client.user.displayAvatarURL())
         .setTimestamp()
         .setColor('#FFC4E7')
-    webhook.send(embed);
+    canal.send(embed);
 }
--- src/events/guild/guildDelete.js
+++ src/events/guild/guildDelete.js
@@ -1,54 +1,14 @@
 const Discord = require("discord.js")
-const guildSchema = require('../../database/mongoDB/guild.js');
+const config = require("../../../config/config.json")
 
-module.exports = async (client, guild) => {
+module.exports = async (client, guild, message) => {
+    let canal = client.channels.cache.get("841393455694872597")
 
-    //client.channels.cache.get("841353285381914624").setName(`📝│Servidores: ${botGuilds}`).catch((e) => console.log((e)))
-    //client.channels.cache.get("855968171855839242").setName(`💻│Shards: ${config.shards}`).catch((e) => console.log((e)))
-    //client.channels.cache.get("841733238165995570").setName(`📝│Users: ${client.users.cache.size}`).catch((e) => console.log((e)))
-
-    guildSchema.findOneAndDelete({
-        Guild: guild.id
-    }, async (err, data) => {
-        if (err) return console.log(err)
-    })
-
-    let icon = (!guild.iconURL() ? 'https://cdn.discordapp.com/avatars/764919941538775050/672afa4f2a4ac5fee624580229a2efbd.png?size=1024' : guild.iconURL())
-
-    client.shard.broadcastEval(`
-    (async () => {
-        let channel = this.channels.cache.get("841393455694872597")
-        const webhooks = await channel.fetchWebhooks(); 		
-        const webhook = webhooks.first();
-        let embed = {
-        color: "#5B00FF",
-        author: {
-         name: \`Sai de um servidor | (${guild.name}/${guild.id})\`,
-        icon_url: \`${icon}\`,
-        },
-        thumbnail: {
-        url: \`${icon}\`,
-        },
-        fields: [
-        {
-        name: \`Nome:\`,
-        value: \`(\\\`${guild.name}/${guild.id}\\\`)\`,
-        },
-        {
-        name: \`Dono:\`,
-        value: \`(\\\`${guild.owner.user.tag}/${guild.owner.id}\\\`)\`,
-        },
-        {
-        name: \`Total de membros\`,
-        value: \`${guild.memberCount}\`,
-        },
-        ],
-        timestamp: new Date(),
-        footer: {
-        text: \`${guild.id}\`, 
-        icon_url: \`${icon}\`, 	
-        },
-        }
-        webhook.send({embeds: [embed] })
-    })()`, 0)
-};
+    let embed = new Discord.MessageEmbed()
+        .setTitle(`Fui removido de um servidor!!`)
+        .setDescription(`<:info:835206734225473546> | Servidor: ${guild.name}\n<:info:835206734225473546> | ID: ${guild.id}\n<:info:835206734225473546> | Membros: ${guild.memberCount} membros\n<:info:835206734225473546> | Dono: ${guild.owner.user.tag}/${guild.owner.id}`)
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTimestamp()
+        .setColor('#FF0000')
+    canal.send(embed);
+}
