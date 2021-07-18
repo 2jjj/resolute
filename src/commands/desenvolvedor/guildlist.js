549454d7fe8d0e68d5febf4df95b8289f7db625d
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-17 21:49:48 -0300

    Update guildlist.js

--- src/commands/desenvolvedor/guildlist.js
+++ src/commands/desenvolvedor/guildlist.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const ownerid = "836345581424738354";
 
 module.exports = {
     name: "guildlist",
@@ -12,18 +13,24 @@ module.exports = {
     args: false,
 
     async run(client, message, args) {
-        let clientGuilds = message.client.guilds.cache;
-        let messageObj = Discord.Util.splitMessage(
-            clientGuilds.map(g => '\`' + g.id + `\` **|** \`` + g.name + `\` **|** \`` + g.members.cache.size + '\`') || 'None'
-        );
-        if (messageObj.length == 1) {
-            message.channel.send(messageObj[0]);
-        } else {
-            for (i = 0; messageObj.length < i; i++) {
-                message.channel.send(messageObj[i]);
-                console.log(messageObj[i]);
+
+        if (message.author.id == ownerid) {
+            
+            let clientGuilds = message.client.guilds.cache;
+            let messageObj = Discord.Util.splitMessage(
+                clientGuilds.map(g => '\`' + g.id + `\` **|** \`` + g.name + `\` **|** \`` + g.members.cache.size + '\`') || 'None'
+            );
+            if (messageObj.length == 1) {
+                message.channel.send(messageObj[0]);
+            } else {
+                for (i = 0; messageObj.length < i; i++) {
+                    message.channel.send(messageObj[i]);
+                    console.log(messageObj[i]);
+                }
             }
+            console.log(messageObj[i]);
+        } else {
+            return;
         }
-        console.log(messageObj[i]);
     }
 }
