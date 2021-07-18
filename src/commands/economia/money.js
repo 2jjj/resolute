541681cfebd1c8a7e6dde7a7c2c0f87e675d0483
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-18 15:27:00 -0300

    Update money.js

--- src/commands/economia/money.js
+++ src/commands/economia/money.js
@@ -22,15 +22,11 @@ module.exports = {
         let bank = db.fetch(`bank_${message.guild.id}_${user.id}`)
         if (bank === null) bank = 0;
 
-        let ienes = await db.fetch(`ienes_${message.guild.id}_${user.id}`)
-        if (ienes === null) ienes = 0;
-
         const embed = new Discord.MessageEmbed()
             .setColor("GREEN")
             .setTitle("Balanço Monetário")
             .setDescription(`**${user.username}**, veja as informações da sua carteira:` +
                 `\n\n<:ybs_dinheiro:856961057204600833> **| Coins:** **\`${money}\`**` +
-                `\n<:ybs_dinheiro:856961057204600833> **| ¥ Ienes:** **\`${ienes}\`**` +
                 `\n:bank: **| Banco:** **\`${bank}\`**`)
             .setFooter(`Requisitado por: ${message.author.username}`, message.author.displayAvatarURL({
                 dynamic: true
