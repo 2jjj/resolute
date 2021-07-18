b265e7c36a34187f9f6e2a728b3ccca1b528d8fd
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-18 20:15:04 -0300

    Update setlogs.js

--- src/commands/configuraveis/setlogs.js
+++ src/commands/configuraveis/setlogs.js
@@ -23,9 +23,10 @@ module.exports = {
         if (prefix === null) prefix = "s."
 
         if (args[0] === "edit") {
-            if (!message.member.hasPermission(module.exports.permissoes)) return;
-            if (!message.guild.me.hasPermission(module.exports.permissoes)) return;
+            if (!message.member.hasPermission(module.exports.permissoes[0])) return message.reply(`<:x_:856894534071746600> **|** Você não possui a permissão necessária para usar este comando, você precisa da permissão de \`${module.exports.permissoes[1]}\`!`)
+            if (!message.guild.me.hasPermission(module.exports.permissoes[0])) return message.reply(`<:x_:856894534071746600> **|** Eu não tenho a permissão necessária para executar este comando, eu preciso da permissão de \`${module.exports.permissoes[1]}\`!`)
             if (!canal) return message.channel.send(`<:x_:856894534071746600> | ${author} ${err}.`);
+            
             db.set(`msg_edit_${message.guild.id}`, canal.id);
             let confirm_pt1 = "O canal";
             let confirm_pt2 = "foi configurado com sucesso.";
@@ -33,10 +34,10 @@ module.exports = {
         }
 
         if (args[0] === "delete") {
-            if (!message.member.hasPermission(module.exports.permissoes)) return;
-            if (!message.guild.me.hasPermission(module.exports.permissoes)) return;
-
+            if (!message.member.hasPermission(module.exports.permissoes[0])) return message.reply(`<:x_:856894534071746600> **|** Você não possui a permissão necessária para usar este comando, você precisa da permissão de \`${module.exports.permissoes[1]}\`!`)
+            if (!message.guild.me.hasPermission(module.exports.permissoes[0])) return message.reply(`<:x_:856894534071746600> **|** Eu não tenho a permissão necessária para executar este comando, eu preciso da permissão de \`${module.exports.permissoes[1]}\`!`)
             if (!canal) return message.channel.send(`<:x_:856894534071746600> **|** ${author} ${err}.`);
+
             db.set(`msg_del_${message.guild.id}`, canal.id);
             let confirm_pt1 = "O canal";
             let confirm_pt2 = "foi configurado com sucesso.";
@@ -47,8 +48,8 @@ module.exports = {
             if (!message.member.hasPermission(module.exports.permissoes[0])) return message.reply(`<:x_:856894534071746600> **|** Você não possui a permissão necessária para usar este comando, você precisa da permissão de \`${module.exports.permissoes[1]}\`!`)
             if (!message.guild.me.hasPermission(module.exports.permissoes[0])) return message.reply(`<:x_:856894534071746600> **|** Eu não tenho a permissão necessária para executar este comando, eu preciso da permissão de \`${module.exports.permissoes[1]}\`!`)
             let canal = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
-
             if (!canal) return message.channel.send(`<:x_:856894534071746600> **|** ${author} ${msg_error_canal}`);
+            
             db.set(`mod_logs_${message.guild.id}`, canal.id);
             let mod_logs0 = db.get(`mod_logs_${message.guild.id}`, canal.id);
             message.channel.send(`<:v_:856894534184468480> **|** ${author} ${msg_confirmado} para <#${mod_logs0}> com sucesso.`)
