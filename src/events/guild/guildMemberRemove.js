54340426a06692e3b87adc3632618cf720fc3195
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-20 22:31:00 -0300

    Update guildMemberRemove.js

--- src/events/guild/guildMemberRemove.js
+++ src/events/guild/guildMemberRemove.js
@@ -2,10 +2,10 @@ const db = require("quick.db");
 //const crystol = require("crystolnetwork-log");
 
 module.exports = async (member) => {
-	db.delete(`money_${member.guild.id}_${member.id}`);
-	db.delete(`bank_${member.guild.id}_${member.id}`);
-	db.delete(`work_${member.guild.id}_${member.id}`);
-	db.delete(`daily_${member.guild.id}_${member.id}`);
-	db.delete(`rob_${member.guild.id}_${member.id}`);
+	//db.delete(`money_${member.guild.id}_${member.id}`);
+	//db.delete(`bank_${member.guild.id}_${member.id}`);
+	//db.delete(`work_${member.guild.id}_${member.id}`);
+	//db.delete(`daily_${member.guild.id}_${member.id}`);
+	//db.delete(`rob_${member.guild.id}_${member.id}`);
 	//crystol.log(`[LOGS] - [ECONOMIA] O ${member.tag} saiu do servidor ${member.guild.name}, e teve todos os status de economia removidos!`, "economia_member_remove.log", "America/Sao_Paulo");
 }
