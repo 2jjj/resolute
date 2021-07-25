ee2cb4c9393b5418359c08b9e3f81a4ea8a90d11
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-24 23:58:28 -0300

    agora sim

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const mongoose = require("mongoose");
 const ascii = require("ascii-table");
 let table = new ascii("MongoDB");
 table.setHeading("Mongo", "Load status");
-const client = new Client({
-  disableEveryone: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+  console.log(client.user.id)
 });
 
 require("./src/util/inlineReply")
--- src/commands/diversao/ship.js
+++ src/commands/diversao/ship.js
@@ -1,6 +1,6 @@
-const Discord = require("discord.js");
-const block = "⬛";
-const heart = ":red_square:";
+const Discord = require(`discord.js`);
+const Canvas = require('canvas');
+const canvas = Canvas.createCanvas(384, 128);
 
 module.exports = {
   name: "ship",
@@ -14,36 +14,77 @@ module.exports = {
   args: true,
 
   async run(client, message, args) {
-    const user = message.mentions.users.first();
-    if (!user) return;
-    if (user && user.id === message.author.id) {
-      return message.reply("Você não pode se shipar.")
-    }
-    if (message.mentions.users.size < 2) {
-      let loveEmbed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle('Shipando...')
-        .setDescription(`${message.author} e ${user}!`)
-        .setImage(`https://api.popcatdev.repl.co/ship?user1=${message.author.displayAvatarURL({ dynamic: false, format: "png" })}&user2=${user.displayAvatarURL({ dynamic: false, format: "png" })}`)
-        .addField(`**Porcentagem de amor**`, ship())
 
-      return message.channel.send(loveEmbed)
-    } else if (message.mentions.users.size > 1) {
-      let luv = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle('Shipando...')
-        .setDescription(`${message.mentions.users.first()} e ${message.mentions.users.last()}!`)
-        .setImage(`https://api.popcatdev.repl.co/ship?user1=${message.mentions.users.first().displayAvatarURL({ dynamic: false, format: "png" })}&user2=${message.mentions.users.last().displayAvatarURL({ dynamic: false, format: "png" })}`)
-        .addField(`**Porcentagem de amor**`, ship())
-      message.channel.send(luv)
+    if (!args[0]) return;
+
+    const membro1 = message.mentions.members.first(2)[0] || message.guild.members.cache.get(args[0]) || message.guild.members.cache.get(client.user.id);
+    const membro2 = message.mentions.members.first(2)[1] || message.guild.members.cache.get(args[1]) || message.guild.members.cache.get(message.author.id);
+
+    if (!membro1 || !membro2) return message.channel.send('Você precisa mencionar 2 pessoas!')
+    if (membro1 === membro2) return message.channel.send('Mencione duas pessoas diferentes!')
+
+    const amor = Math.floor(Math.random() * 101);
+    const loveIndex = Math.floor(amor / 10);
+    const loveLevel = "█".repeat(loveIndex) + ".".repeat(10 - loveIndex);
+
+    let nomeFim1 = membro1.user.username.length;
+    let nomeFim2 = membro2.user.username.length;
+
+    let calc1 = nomeFim1 - 3
+    let calc2 = nomeFim2 - 3
+
+    let nomeship;
+    if (amor > 60) {
+      nomeship = membro1.user.username.slice(0, 3) + membro2.user.username.slice(0, 3);
+    } else if (amor >= 40) {
+      nomeship = membro1.user.username.slice(0, calc1) + membro2.user.username.slice(0, calc2)
+    } else {
+      nomeship = membro1.user.username.slice(calc1, nomeFim1) + membro2.user.username.slice(calc2, nomeFim2)
     }
+
+    let emoticon;
+    if (amor > 60) {
+      emoticon = ('https://cdn.discordapp.com/attachments/727109164333138013/789198401903394826/heart.png')
+    } else if (amor >= 40) {
+      emoticon = ('https://cdn.discordapp.com/attachments/727109164333138013/789198402726527006/idontno.png')
+    } else {
+      emoticon = ('https://cdn.discordapp.com/attachments/727109164333138013/789198399508578374/sad.png')
+    }
+
+    let desc;
+    if (amor > 90) {
+      desc = (":sparkling_heart: HMMM, isso vai acontecer ou não? :sparkling_heart:\n``" + membro1.user.username + "``\n``" + membro2.user.username + "``\n:heart: ``" + nomeship + "`` Este é o casal perfeito! :heart:");
+    } else if (amor >= 70) {
+      desc = (":sparkling_heart: HMMM, isso vai acontecer ou não? :sparkling_heart:\n``" + membro1.user.username + "``\n``" + membro2.user.username + "``\n:neutral_face: ``" + nomeship + "`` Esses aqui já estão se pegando e não contaram pra ninguém! :neutral_face:");
+    } else if (amor >= 45) {
+      desc = (":sparkling_heart: HMMM, isso vai acontecer ou não? :sparkling_heart:\n``" + membro1.user.username + "``\n``" + membro2.user.username + "``\n:no_mouth: ``" + nomeship + "`` Talvez só precise que o " + membro2.user.username + " queira... :no_mouth:");
+    } else {
+      desc = (":sparkling_heart: HMMM, isso vai acontecer ou não? :sparkling_heart:\n``" + membro1.user.username + "``\n``" + membro2.user.username + "``\n:cry: ``" + nomeship + "`` Eu realmente queria dizer que é possível, mas ... :cry: ");
+    }
+
+    const canvas = Canvas.createCanvas(384, 128);
+    const ctx = canvas.getContext('2d');
+
+    const emote = await Canvas.loadImage(emoticon);
+    const foto1 = await Canvas.loadImage(membro1.user.displayAvatarURL({
+      format: "png"
+    }))
+    const foto2 = await Canvas.loadImage(membro2.user.displayAvatarURL({
+      format: "png"
+    }))
+
+    ctx.drawImage(emote, 125, 0, 128, 128)
+    ctx.drawImage(foto1, -10, 0, 128, 128)
+    ctx.drawImage(foto2, 260, 0, 128, 128)
+
+    const amorat = new Discord.MessageAttachment(canvas.toBuffer(), 'ship.png')
+
+    let amorEmbed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription("**" + amor + "%** [`" + loveLevel + "`]")
+      .setFooter("Resolute")
+      .attachFiles([amorat]).setImage('attachment://ship.png')
+
+    message.channel.send('<@' + message.author.id + '> \n' + desc, amorEmbed)
   }
-}
-
-function ship() {
-  const hearts = Math.floor(Math.random() * 110) + 0;
-  const hearte = (hearts / 10)
-
-  const str = `${heart.repeat(hearte)}${block.repeat(11 - hearte)} ${hearts}%`;
-  return str;
-}
+};
