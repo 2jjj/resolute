5c4bcf3e4b3760f47bab27b7d22b13f69c9670d9
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-25 12:15:30 -0300

    lindo

--- src/events/guild/messageDelete.js
+++ src/events/guild/messageDelete.js
@@ -3,40 +3,38 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = async (client, message) => {
 
-    let canal = db.get(`msg_del_${message.guild.id}`);
-    if(canal === null) return;
+  let canal = db.get(`msg_del_${message.guild.id}`);
+  if (canal === null) return;
 
-    if (!canal === null) return;
-  
-    if (message.author.bot) return;
-  
-    let author = message.author;
-    let canal_2 = message.channel;
-    let msg_del = message.content;
-  
-    let msg_embed = new MessageEmbed()
+  if (!canal === null) return;
+
+  if (message.author.bot) return;
+
+  let author = message.author;
+  let canal_2 = message.channel;
+  let msg_del = message.content;
+
+  let msg_embed = new MessageEmbed()
     .setTitle(`🗑 Mensagem excluída`)
     .setColor("RANDOM")
-    .addFields(
-      {
-        name: `Autor da mensagem`,
-        value: author,
-        inline: false
-      },
-      {
-        name: `Canal`,
-        value: canal_2,
-        inline: false
-      },
-      {
-        name: `Mensagem`,
-        value: `\`\`\`${msg_del}\`\`\``,
-        inline: false
-      }
-    )
-    .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+    .addFields({
+      name: `Autor da mensagem`,
+      value: author,
+      inline: false
+    }, {
+      name: `Canal`,
+      value: canal_2,
+      inline: false
+    }, {
+      name: `Mensagem`,
+      value: `\`\`\`${msg_del}\`\`\``,
+      inline: false
+    })
+    .setThumbnail(message.author.displayAvatarURL({
+      dynamic: true
+    }))
     .setTimestamp()
     .setFooter(message.guild.name, message.guild.iconURL());
-  
-    client.channels.cache.get(canal).send(msg_embed)
+
+  client.channels.cache.get(canal).send(msg_embed)
 }
