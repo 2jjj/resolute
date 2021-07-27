a8f1409dec65952cf9c0e67cb9f8fe09e9d8ccbf
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-27 00:18:22 -0300

    Update setprefix.js

--- src/commands/configuraveis/setprefix.js
+++ src/commands/configuraveis/setprefix.js
@@ -11,45 +11,18 @@ module.exports = {
     usage: "<prefixo>",
     example: "!",
     permissoes: ["MANAGE_GUILD", "Gerenciar Servidor"],
-    args: true,
+    args: false,
 
     async run(client, message, args, msg) {
 
-        if (!args[0]) return;
         if (!message.member.hasPermission(module.exports.permissoes[0])) return;
         //if (!message.guild.me.hasPermission(module.exports.permissoes[0])) return;
 
-        let prefix = db.get(`prefix_${message.guild.id}`)
-        if (prefix === null) prefix = "s."
-
-        if (args[0] === prefix) {
-            let atual = new Discord.MessageEmbed()
-                .setColor("RANDOM")
-                .setTitle('Este já é meu prefixo atual.')
-            return message.inlineReply(atual)
-        }
-
-        if (args[1]) {
-            let space = new Discord.MessageEmbed()
-                .setColor("RANDOM")
-                .setTitle('O prefixo não pode ter espaços.')
-            return message.inlineReply(space)
-        }
-
-        if (args[0].length > 3) {
-            let caracter = new Discord.MessageEmbed()
-                .setColor("RANDOM")
-                .setTitle('O prefixo não pode ter mais de 3 caracteres.')
-            return message.inlineReply(caracter)
-        }
-
-        db.set(`prefix_${message.guild.id}`, args[0])
         let alterado = new Discord.MessageEmbed()
             .setColor("RANDOM")
-            .setTitle("Prefixo alterado!")
+            .setTitle("Como posso alterar o prefixo?")
             .setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
-            .addField(`Autor:`, `ﾠ<:setaaa:860626769089265665> ${message.author} | \`${message.author.id}\``)
-            .addField(`Novo Prefixo:`, `ﾠ<:setaaa:860626769089265665> \`${args[0]}\``)
+            .addField(`A configuração do prefixo é realizado pela dashboard do Resolute:`, `https://painel.resolutebot.xyz`)
             .setFooter("Resolute", message.author.displayAvatarURL())
             .setTimestamp();
         return message.inlineReply(alterado)
