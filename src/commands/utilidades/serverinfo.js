61975f3d08e39cbd22147c51fa36308eb2713029
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-27 15:26:37 -0300

    Update serverinfo.js

--- src/commands/utilidades/serverinfo.js
+++ src/commands/utilidades/serverinfo.js
@@ -1,5 +1,6 @@
-const Discord = require("discord.js");
 const config = require("../../../config/config.json")
+const { Client, Message, MessageEmbed, Role } = require('discord.js');
+const moment = require('moment')
 
 module.exports = {
 	name: "serverinfo",
@@ -13,35 +14,38 @@ module.exports = {
     args: false,
 	
 	async run(client, message, args) {
+		const filterLevels = {
+            DISABLED: 'Off',
+            MEMBERS_WITHOUT_ROLES: 'No Role',
+            ALL_MEMBERS: 'Everyone'
+        };
 
-		let serverembed = new Discord.MessageEmbed()
-			.setColor("#RANDOM") //»
-			.setTitle(`<:5864blurplesearch:856520144817881118> ${message.guild.name}`)
-			.setDescription(`
-			
-			🧭 **| Nome:** \`${message.guild.name}\`
-
-			🧭 **| ID do servidor:** \`${message.guild.id}\`
-
-			📆 **| Servidor Criado em:** \`${message.guild.createdAt}\`
-
-			<:8263blurplemembers:856520144778952704> **| Membros** \`${message.guild.members.cache.filter(member => member.user.bot).size} Bots | ${(message.guild.memberCount) - (message.guild.members.cache.filter(member => member.user.bot).size)} Membros | ${message.guild.memberCount} Total de Membros | ${Math.round((message.guild.members.cache.filter(member => member.user.bot).size / message.guild.memberCount) * 100)}% Bots | ${Math.round((((message.guild.memberCount) - (message.guild.members.cache.filter(member => member.user.bot).size)) / message.guild.memberCount) * 100)}% Membros}\`
-
-			<:5702blurpletextchannel:856520145042931722> **| Canais:** \`${message.guild.channels.cache.filter(chan => chan.type === 'voice').size} Chats de Voz | ${message.guild.channels.cache.filter(chan => chan.type === 'text').size} Canais de Texto | ${message.guild.channels.cache.filter(chan => chan.type === 'category').size} Categorias | ${Math.round((message.guild.channels.cache.filter(chan => chan.type === 'voice').size / message.guild.channels.cache.size) * 100)}% Chats de Voz | ${Math.round((message.guild.channels.cache.filter(chan => chan.type === 'text').size / message.guild.channels.cache.size) * 100)}% Canais de Texto | ${Math.round((message.guild.channels.cache.filter(chan => chan.type === 'category').size / message.guild.channels.cache.size) * 100)}% Categorias\`
-
-			<:coroa:856527751816937482> **| Dono:** <@${message.guild.ownerID}>
-
-			<:coroa:856527751816937482>  **| ID do Dono:** \`${message.guild.ownerID}\`
-
-			📆 **| Você Entrou:** \`${message.member.joinedAt}\`
-
-			**-- Shard --**
-			<:cpu:854137097521987624> **Shard: ${message.guild.shard.id}/${config.shards}**
-    	`)
-			.setFooter(`Requisitado por: ${message.author.tag}`, message.author.displayAvatarURL({
-				dynamic: true
-			}))
-		message.channel.send(serverembed);
+        const vanityCode = message.guild.vanityURLCode;
+        let vanityInvite = `https://discord.gg/${'vanityCode'}`;
+        if (vanityCode === null) vanityInvite = 'No custom URL';
+        const members = message.guild.members.cache;
+        const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
+        const embed = new MessageEmbed()
+        .setTimestamp()
+        .setTitle(`<:5864blurplesearch:856520144817881118> ${message.guild.name}`)
+        .setColor('RANDOM')
+        .setThumbnail(message.guild.iconURL({ dynamic: true }))
+        .addField(`🎫 Nome:`, message.guild.name, true)
+        .addField(`🆔 ID do servidor:`, message.guild.id, true)
+        .addField(`👑 Dono do servidor:`, message.guild.owner, true)  
+        .addField(`🌎 Região:`, message.guild.region, true)
+        .addField(`👥 Membros:`, message.guild.members.cache.size, true)
+        .addField(`🤖 Bots`, message.guild.members.cache.filter(member => member.user.bot).size, true)
+        .addField(`😗 Emojis:`, message.guild.emojis.cache.size, true)
+        .addField(`👻 Emojis animados:`,message.guild.emojis.cache.filter(emoji => emoji.animated).size,true )
+        .addField(`💬 Canais de texto:`, message.guild.channels.cache.filter(channel => channel.type === 'text').size, true)
+        .addField(`🎤 Canais de voz:`, message.guild.channels.cache.filter(channel => channel.type === 'voice').size, true)
+        .addField(`👔 Total de cargos:`, message.guild.roles.cache.size, true)
+        .addField(`📅 Criado em`, `${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).format('LTS')} ${moment(message.guild.createdTimestamp).fromNow()},`, true)
+        .addField(`🚀 Nivel de boost:`, `${message.guild.premiemTier ? `Tier ${message.guild.premiemTier}` : 'None'}`, true)
+        .addField(`💨 Total de boosts:`, `${message.guild.premiemSubscriptionCount || '0'}`, true)
+        .addField(`Cargos [${roles.length}]`, roles.length < 15 ? roles.join(', ') : roles.length > 15 ? `${roles.slice(0, 15).join(', ')}\n+${roles.length-15} cargos...` : 'Nenhum cargo!')
+        .setFooter(`Requisitado por: ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+        message.inlineReply(embed);
 	}
 }
-//**| Proteção de verificação:** ${message.guild.verificationLevel}
