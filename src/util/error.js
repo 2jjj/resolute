ad493397c7dce357c286211bc207774af2e5b697
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-26 23:51:59 -0300

    autorole dash

--- src/util/error.js
+++ src/util/error.js
@@ -5,6 +5,7 @@ const { MessageEmbed } = require("discord.js")
  * @param {String} text 
  * @param {TextChannel} channel 
  */
+
 module.exports = async (text, channel) => {
     let embed = new MessageEmbed()
     .setColor("RED")
