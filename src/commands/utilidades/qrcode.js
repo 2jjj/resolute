e026671928d1082175903388b714899041ffda65
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-29 18:05:28 -0300

    Update qrcode.js

--- src/commands/utilidades/qrcode.js
+++ src/commands/utilidades/qrcode.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	name: "qrcode",
-	aliases: [],
-	cooldown: 1000 * 2,
-	description: "Consiga um qrcode para seu link.",
-	category: "util",
-	usage: "<link>",
-	example: "https://resolutebot.xyz",
+    name: "qrcode",
+    aliases: [],
+    cooldown: 1000 * 2,
+    description: "Consiga um qrcode para seu link.",
+    category: "util",
+    usage: "<link>",
+    example: "https://resolutebot.xyz",
     permissoes: [],
     args: true,
-	
-	async run(client, message, args) {
-    
+
+    async run(client, message, args) {
+
         let qrlink = `http://api.qrserver.com/v1/create-qr-code/?data=${args[0]}&size=200x200`
 
         if (!args[0]) return;
@@ -20,15 +20,17 @@ module.exports = {
         if (require('is-url')(args[0])) {
             const attachment = new Discord.MessageAttachment(qrlink, 'qrcode.png');
             const embed = new Discord.MessageEmbed()
-            .attachFiles(attachment)
-            .setImage('attachment://qrcode.png')
-            .setFooter(`Requisitado por ${message.author.username}`,  message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
+                .attachFiles(attachment)
+                .setImage('attachment://qrcode.png')
+                .setFooter(`Requisitado por ${message.author.username}`, message.author.displayAvatarURL({
+                    dynamic: true
+                }))
+                .setTimestamp()
 
             message.inlineReply(embed)
 
         } else {
             message.channel.send("URL inválida.")
-         }
+        }
     }
 }
