41c6e0fe826df903ca3a1d028e99881a28b61a65
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-29 00:56:56 -0300

    Update daily.js

--- src/commands/economia/daily.js
+++ src/commands/economia/daily.js
@@ -37,13 +37,17 @@ module.exports = {
         let timeout = 86400000;
         let daily = await db.fetch(`daily_${user.id}`);
         let amount = Math.floor(Math.random() * 10000) + 1000;
+        let agoratem = db.fetch(`money_${user.id}`)
+        if (agoratem === null) money = 0;
+
+        console.log(agoratem)
 
         if (daily !== null && timeout - (Date.now() - daily) > 0) {
             let time = ms(timeout - (Date.now() - daily));
 
             let timeEmbed = new Discord.MessageEmbed()
                 .setColor("RANDOM")
-                .setDescription(`<:ybs_dinheiro:856961057204600833> **|** Você já recebeu sua recompensa diária!\n<:interrogacao:856894534029541376> **|** Colete novamente daqui a **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
+                .setDescription(`<:ybs_dinheiro:856961057204600833> **|** Você já recebeu sua recompensa diária!\n<:interrogacao:856894534029541376> **|** Colete novamente daqui a **${time.hours} horas ${time.minutes} minutos e ${time.seconds} segundos!**`)
                 .setFooter(`Requisitado por: ${message.author.username}`, message.author.displayAvatarURL({
                     dynamic: true
                 }))
@@ -51,9 +55,11 @@ module.exports = {
                 .setTimestamp();
             message.channel.send(`${user}`, timeEmbed);
         } else {
+            let time = ms(timeout - (Date.now() - daily));
+
             let moneyEmbed = new Discord.MessageEmbed()
                 .setColor("RANDOM")
-                .setDescription(`<:ybs_dinheiro:856961057204600833> **|** Você recebeu **\`${amount}\`** Coins!\n<:pontin:852197383974551582> **|** Compre itens com ${storedSettings.prefix}shop!`)
+                .addField(`<:ybs_dinheiro:856961057204600833> **|** Você recebeu **\`${amount}\`** RCoins!`, `<:interrogacao:856894534029541376> **| Veja suas estatisticas usando** \`${storedSettings.prefix}atm\` **!**`)
                 .setFooter(`Requisitado por ${message.author.username}`, message.author.displayAvatarURL({
                     dynamic: true
                 }))
@@ -61,7 +67,7 @@ module.exports = {
                 .setTimestamp();
             message.channel.send(`${user}`, moneyEmbed);
 
-            db.add(`money_${user.id}`, amount);
+            db.add(`money_${message.author.id}_${message.author.id}`, amount);
             db.set(`daily_${user.id}`, Date.now());
         }
     }
