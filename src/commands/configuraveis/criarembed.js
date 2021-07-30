261f9ba1a6f5298f72a02cc16584dd99e8f050d7
Pedro <71204668+sprayx@users.noreply.github.com>
2021-07-29 22:15:37 -0300

    Update criarembed.js

--- src/commands/configuraveis/criarembed.js
+++ src/commands/configuraveis/criarembed.js
@@ -18,42 +18,42 @@ module.exports = {
 	if (!message.guild.me.hasPermission(module.exports.permissoes[0])) return;
 
     let canalsetado = db.get(`setlogadm_${message.guild.id}`);
-    message.channel.send(`[Texto] -> Qual o título que deseja colocar na embed?`).then(m2 => {
+    message.channel.send(`[1/6] **|** Qual o título que deseja colocar na embed?`).then(m2 => {
         let cp = message.channel.createMessageCollector(x => x.author.id === message.author.id, {
                 max: 1
             })
             .on('collect', c => {
                 titulo = c.content
 
-                message.channel.send(`[Texto] -> Qual a descrição que deseja colocar na embed?`).then(m3 => {
+                message.channel.send(`[2/6] **|** Qual a descrição que deseja colocar na embed?`).then(m3 => {
                     let cd = message.channel.createMessageCollector(x => x.author.id === message.author.id, {
                             max: 1
                         })
                         .on('collect', c => {
                             descrição = c.content
 
-                            message.channel.send(`[Imagem] -> Qual a thumbnail que deseja colocar na embed? \n `).then(m3 => {
+                            message.channel.send(`[3/6] **|** Qual a thumbnail que deseja colocar na embed? \n `).then(m3 => {
                                 let vr = message.channel.createMessageCollector(x => x.author.id === message.author.id, {
                                         max: 1
                                     })
                                     .on('collect', c => {
                                         thumb = c.content
 
-                                        message.channel.send(`[Imagem] -> Qual a imagem que deseja colocar na embed?`).then(m3 => {
+                                        message.channel.send(`[4/6] **|** Qual a imagem que deseja colocar na embed?`).then(m3 => {
                                             let vr = message.channel.createMessageCollector(x => x.author.id === message.author.id, {
                                                     max: 1
                                                 })
                                                 .on('collect', c => {
                                                     imagem = c.content
 
-                                                    message.channel.send(`[Texto] -> Qual o footer que deseja colocar na embed?`).then(m3 => {
+                                                    message.channel.send(`[5/6] **|** Qual o footer que deseja colocar na embed?`).then(m3 => {
                                                         let dc = message.channel.createMessageCollector(x => x.author.id === message.author.id, {
                                                                 max: 1
                                                             })
                                                             .on('collect', c => {
                                                                 footer = c.content
 
-                                                                message.channel.send(`Qual a cor que deseja colocar na embed?\n 1- Vermelho \n 2- Verde \n 3- Preto \n 4- Aleatório`).then(m3 => {
+                                                                message.channel.send(`[6/6] **|** Qual a cor que deseja colocar na embed?\n 1- Vermelho \n 2- Verde \n 3- Preto \n 4- Aleatório`).then(m3 => {
                                                                     let kv = message.channel.createMessageCollector(x => x.author.id === message.author.id, {
                                                                             max: 1
                                                                         })
