78db4acc0856f4dd53202bf29aafe8e1f2838f6d
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-01 16:43:08 -0300

    🔮Events

---
+++ events/guild/messageDelete.js
@@ -0,0 +1,40 @@
+const db = require("quick.db")
+const { MessageEmbed } = require("discord.js")
+
+module.exports = async (client, message) => {
+
+  let canal = db.get(`msg_del_${message.guild.id}`);
+  if (canal === null) return;
+
+  if (!canal === null) return;
+
+  if (message.author.bot) return;
+
+  let author = message.author;
+  let canal_2 = message.channel;
+  let msg_del = message.content;
+
+  let msg_embed = new MessageEmbed()
+    .setTitle(`🗑 Mensagem excluída`)
+    .setColor("RANDOM")
+    .addFields({
+      name: `Autor da mensagem`,
+      value: author,
+      inline: false
+    }, {
+      name: `Canal`,
+      value: canal_2,
+      inline: false
+    }, {
+      name: `Mensagem`,
+      value: `\`\`\`${msg_del}\`\`\``,
+      inline: false
+    })
+    .setThumbnail(message.author.displayAvatarURL({
+      dynamic: true
+    }))
+    .setTimestamp()
+    .setFooter(message.guild.name, message.guild.iconURL());
+
+  client.channels.cache.get(canal).send(msg_embed)
+}
