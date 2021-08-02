a093870b22e0fced20c6dd74eb6196a12badb0db
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-02 00:33:10 -0300

    Update loop.js

--- commands/Music/loop.js
+++ commands/Music/loop.js
@@ -1,9 +1,8 @@
-const {
-  MessageEmbed
-} = require(`discord.js`);
+const { MessageEmbed } = require(`discord.js`);
 const config = require(`../../config/config.json`);
 const ee = require(`../../config/embed.json`);
 const emoji = require(`../../config/emojis.json`);
+
 module.exports = {
   name: `loop`,
   category: `🎶 Music`,
@@ -15,9 +14,10 @@ module.exports = {
   cooldown: 8,
   args: false,
   parameters: { "type": "music", "activeplayer": true, "previoussong": false },
-  run: async (client, message, args, cmduser, text, prefix, player) => {
-    try {
 
+  run: async (client, message, args, cmduser, text, prefix, player) => {
+     
+    try {
       if (!args[0])
         return message.channel.send(new MessageEmbed()
           .setColor(ee.wrongcolor)
