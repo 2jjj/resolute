802c458cca1e7db04373a7b191a944e65b8740bb
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-02 00:13:25 -0300

    Update addsimilar.js

--- commands/Music/addsimilar.js
+++ commands/Music/addsimilar.js
@@ -1,11 +1,9 @@
-const Discord = require(`discord.js`);
-const {
-  MessageEmbed
-} = require(`discord.js`);
+const { MessageEmbed } = require(`discord.js`);
 const config = require(`../../config/config.json`);
 const ee = require(`../../config/embed.json`);
 const emoji = require(`../../config/emojis.json`);
 const playermanager = require(`../../handlers/playermanager`);
+
 module.exports = {
   name: `addsimilar`,
   category: `🎶 Music`,
@@ -17,6 +15,7 @@ module.exports = {
   cooldown: 8,
   args: false,
   parameters: {"type":"music", "activeplayer": true, "previoussong": false},
+  
   run: async (client, message, args, cmduser, text, prefix, player) => {
     try {
       playermanager(client, message, Array(`https://www.youtube.com/watch?v=${player.queue.current.identifier}&list=RD${player.queue.current.identifier}`), `similar:add`);
