88607cf6a8447f338daf226740c45ac9a9e372d2
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-02 20:00:11 -0300

    Update requestreacts.js

--- handlers/requestreacts.js
+++ handlers/requestreacts.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js")
-const {
-  MessageEmbed
-} = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 const config = require("../config/config.json")
 const emoji = require("../config/emojis.json");
 const ee = require("../config/embed.json")
