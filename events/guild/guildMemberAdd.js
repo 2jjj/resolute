0bbfebf687498167d92a65b93bbc1231b16dca43
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-05 12:01:18 -0300

    fds

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,7 +1,11 @@
 const db = require("quick.db");
 
 module.exports = async (member) => {
-    var autorole_resolute = db.get(`autorole_${member.guild.id}`);
-    console.log(autorole_resolute)
-    member.roles.add(autorole_resolute)
+    try {
+        var autorole_resolute = db.get(`autorole_${member.guild.id}`);
+        console.log(autorole_resolute)
+        member.roles.add(autorole_resolute)
+    } catch (err) {
+        /* */
+    }
 }
