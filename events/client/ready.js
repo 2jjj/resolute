52f3855eb10c0c4d6be1e50aaa5379c4adf1260c
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-06 19:56:02 -0300

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,11 +1,12 @@
-const Dashboard = require("../../dashboard/dashboard");
+//const Dashboard = require("../../dashboard/dashboard");
 
 module.exports = async(client) => {
   
+  /*
   for (const [id, guild] of client.guilds.cache) {
     await guild.members.fetch();
   }
-  Dashboard(client);
+  Dashboard(client);*/
 
   const stringlength = 69;
   console.log("\n")
