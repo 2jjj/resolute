d9cd355ce8d4a234880b3c653a25f6386db97a9e
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-08 20:05:52 -0300

    Update reactions.js

--- commands/Info/reactions.js
+++ commands/Info/reactions.js
@@ -17,10 +17,10 @@ module.exports = {
         .setColor(ee.color)
         .setTitle("🩸 My Reactions when playing a Track does those Things")
         .setFooter(ee.footertext, ee.footericon)
-        .addField(`\u200b`, `${emoji.msg.rewind} Rewind 20 seconds\n${emoji.msg.forward} Forward 20 seconds\n${emoji.msg.pause_resume} Pause/Resume\n${emoji.msg.stop} Stop Track\n${emoji.msg.previous_track} Play previous\n`, true)
-        .addField(`\u200b`, `${emoji.msg.skip_track} Skip / Next\n${emoji.msg.replay_track} Replay Track\n${emoji.msg.reduce_volume} Volume -10 %\n${emoji.msg.raise_volume} Volume +10 %\n${emoji.msg.toggle_mute} Toggle Volume Mute`, true)
-        .addField(`\u200b`, `${emoji.msg.repeat_mode} Change repeat mode\n${emoji.msg.autoplay_mode} Toggle Autoplay\n${emoji.msg.shuffle} Shuffle the queue\n${emoji.msg.show_queue} Show the Queue\n${emoji.msg.show_current_track} Shows Current Track`, true)
-      );
+        .addField(`Comandos`, musiccmds.join(", "))
+        .addField(`Reações`, `${emoji.msg.rewind} Voltar 20 segundos\n${emoji.msg.forward} Avançar 20 segundos\n${emoji.msg.pause_resume} Pausar/Despausar\n${emoji.msg.stop} Parar a música\n${emoji.msg.previous_track} Tocar a música anterior.\n`, true)
+        .addField(`\u200b`, `${emoji.msg.skip_track} Pular / Próxima\n${emoji.msg.replay_track} Replay música.\n${emoji.msg.reduce_volume} Volume -10 %\n${emoji.msg.raise_volume} Volume +10 %\n${emoji.msg.toggle_mute} Alternar volume para mudo.`, true)
+        .addField(`\u200b`, `${emoji.msg.repeat_mode} Alterar o modo de repetição\n${emoji.msg.autoplay_mode} Ativar o autoplay.\n${emoji.msg.shuffle} Embaralhe a fila.\n${emoji.msg.show_queue} Mostrar a fila.\n${emoji.msg.show_current_track} Mostrar a música atual.`, true)      );
     } catch (e) {
       console.log(String(e.stack).bgRed)
       return message.channel.send(new MessageEmbed()
