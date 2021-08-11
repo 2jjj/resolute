a38402ac5d5d10354ff9b6ccdddf6c2ff082a15c
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-11 20:44:42 -0300

    botinfo top

--- commands/Info/botinfo.js
+++ commands/Info/botinfo.js
@@ -15,16 +15,18 @@ module.exports = {
 
   async run(client, message, args, cmduser, text, prefix, player) {
 
-    const promises = [ client.shard.fetchClientValues('guilds.cache.size'), 
-    client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)')];
-    Promise.all(promises) 	
-    .then(async results => { 	
-        const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0); 	
-        const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);   
+    client.shard.broadcastEval(client => client.guilds.cache.size)
+		  .then(async results => {
+        const totalGuilds = results.reduce((prev, val) => prev + val, 0)
+        //const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);   
         
         let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
         let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
         let { version } = require("discord.js");
+        let secs = Math.floor(client.uptime % 60);
+        let days = Math.floor((client.uptime % 31536000) / 86400);
+        let hours = Math.floor((client.uptime / 3600) % 24);
+        let mins = Math.floor((client.uptime / 60) % 60);
 
         let embed = new Discord.MessageEmbed()
         .setTitle("**Prazer, meu nome é Resolute!**")
@@ -32,22 +34,21 @@ module.exports = {
         .setThumbnail(avatar)
         .setDescription(`
         **Informações**
-        > 👑 **|** Criador: ${config.spray}
         > <:early_developer_badge:854716150076538901> **|** Desenvolvedor: ${config.spray}
-        > <:config:856894534197313536> **|** Versão: 2.5.0
+        > <:1520blurplesettings:856520144851435540> **|** Versão: 2.5.0
+        > <:1598blurplesupport:856520144599777291> **|** Prefixo: ${prefix}
         > <:djs:868314375751102484> **|** Versão Discord.js: ${version}
-        
+
         **Estatísticas**
+        > 🤔 **|** Uptime ${hours}h ${mins}m
         > <:pontin:852197383974551582> **|** Estou em ${totalGuilds} servidores.
-        > <:pontin:852197383974551582> **|** ${totalMembers} usuários.
+        > <:pontin:852197383974551582> **|** a usuários.
         > <:cpu:854137097521987624> **|** Shard: ${message.guild.shard.id}/${config.shards}
-        > 🤔 Prefixo: ${prefix}
 
         **Links úteis :**
-        > 🤖 **|** [Me adicione!](https://www.resolutebot.xyz/add)
-        > :small_blue_diamond: **|** [Website](https://www.resolutebot.xyz)
-        > :small_blue_diamond: **|** [Dashboard](https://painel.resolutebot.xyz)
+        > <:6055blurpleinvite:856520144884727818> **|** [Me adicione!](https://www.resolutebot.xyz/add)
+        > <:8512blurplelink:856520144843046922> **|** [Website](https://www.resolutebot.xyz)
         `)
-        await message.channel.send(embed);
+        await message.channel.send({ embeds: [embed] });
     })
 }}
--- commands/Info/status.js
+++ commands/Info/status.js
@@ -26,19 +26,20 @@ module.exports = {
       let days = Math.floor((client.uptime % 31536000) / 86400);
       let hours = Math.floor((client.uptime / 3600) % 24);
       let mins = Math.floor((client.uptime / 60) % 60);
+      
       //let duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+
       let embedStats = new Discord.MessageEmbed()
       .setTitle("** Status **")
       .setColor("#RANDOM")
       .addField("<:memoryram:854135087037153280> Memória ultilizada", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
       .addField("<a:SETA:852194614927818812> Uptime ", `${hours}h ${mins}m`, true) //`${duration}`, true)
-      .addField("<:3434_Discord_js_logo:845779460899733553> Discord.js", `v${version}`, true)
+      .addField("<:djs:868314375751102484> Discord.js", `v${version}`, true)
       .addField("<:node:845780252940959744> Versão do Node", `${process.version}`, true)
       .addField("<:cpu:854137097521987624> CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
       .addField("<:cpuv:854137395254657026> CPU Usada", `\`${percent.toFixed(2)}%\``,true)
       .addField("<:linux:854135555557425163> Arquitetura", `\`${os.arch()}\``,true)
       .addField("<:linux:854135555557425163> OS", `\`\`${os.platform()}\`\``,true)
-      .setFooter("Status")
       message.channel.send(embedStats)
    })
   }
