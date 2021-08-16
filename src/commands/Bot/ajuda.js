ec42b2645740816ad3eea4e825366781db061ce9
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-16 10:36:40 -0300

    Custom queues

--- src/commands/Bot/ajuda.js
+++ src/commands/Bot/ajuda.js
@@ -54,9 +54,9 @@ module.exports = {
       let fun = client.commands.filter((cmd) => cmd.category === 'fun');
       let economia = client.commands.filter((cmd) => cmd.category === 'economia');
       let filter = client.commands.filter((cmd) => cmd.category === '👀 Filter');
-      let util = client.commands.filter((cmd) => cmd.category === 'util');
+      let util = client.commands.filter((cmd) => cmd.category === 'bot');
       let manipulacao = client.commands.filter((cmd) => cmd.category === 'manipulacao');
-      let info = client.commands.filter((cmd) => cmd.category === 'info');
+      let bot = client.commands.filter((cmd) => cmd.category === 'bot');
       let minecraft = client.commands.filter((cmd) => cmd.category === 'minecraft');
       let mod = client.commands.filter((cmd) => cmd.category === 'mod');
       let music = client.commands.filter((cmd) => cmd.category === '🎶 Music');
@@ -74,7 +74,7 @@ module.exports = {
         .addField(`⚜️ ** | Custom Queue(s)** [${queue.size}]:`, `\`${queue.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`👀 ** | Filter** [${filter.size}]:`, `\`${filter.map(cmd => cmd.name).join(' | ')}\``)
         .addField(`<:mine_foguete:852197847754604565> ** | Minecraft** [${minecraft.size}]:`, `\`${minecraft.map(cmd => cmd.name).join(' | ')}\``)
-        .addField(`<:interrogacao:856894534029541376> ** | Bot** [${info.size}]:`, `\`${info.map(cmd => cmd.name).join(' | ')}\``)
+        .addField(`<:interrogacao:856894534029541376> ** | Bot** [${bot.size}]:`, `\`${bot.map(cmd => cmd.name).join(' | ')}\``)
         //.addField(`<:early_developer_badge:854716150076538901> ** | Desenvolvedor** [${dev.size}]:`, `\`${dev.map(cmd => cmd.name).join(' | ')}\``)
         //.addFields(categories)
         .setDescription(`Use \`${prefix}help\` seguido por um nome de comando para obter mais informações adicionais sobre um comando.\nPor exemplo: \`${prefix}help ban\`.\n**Prefixo atual: ${prefix}**\n**Meus Comandos[${client.commands.size}]:**`)
