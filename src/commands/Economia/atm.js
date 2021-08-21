86a0fe722a907d9bbbb15007e244e6d9c9605e20
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-20 21:17:36 -0300

    a

--- src/commands/Economia/atm.js
+++ src/commands/Economia/atm.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
+const { flags } = require("../../util/Emojis")
 
 module.exports = {
     name: "atm",
@@ -25,22 +26,6 @@ module.exports = {
         let badges = db.fetch(`badges_${user.id}`);
         if (badges == null) badges = "Nada";
         
-        const flags = {
-            DISCORD_STAFF: '<:BadgeDiscordStaff:869961802409209877>',
-            DISCORD_EMPLOYEE: 'Discord Employee',
-            DISCORD_PARTNER: '<:5270blurplepartner:856520144628744213>',
-            BUGHUNTER_LEVEL_1: '<:BadgeDiscordBugHunter:853786346498031627>',
-            BUGHUNTER_LEVEL_2: '<:BadgeDiscordBugHunterGold:853786346871193600>',
-            HYPESQUAD_EVENTS: '<:HypeSquadEvents:869962392078020698>',
-            HOUSE_BRAVERY: '<:BadgeHypeSquadBravery:869962263870705674>',
-            HOUSE_BRILLIANCE: '<:1350discordbrillance:856520144762175498>',
-            HOUSE_BALANCE: '<:BadgeHypeSquadBalance:869962147805921361>',
-            EARLY_SUPPORTER: '<:early_developer_badge:854716150076538901>',
-            TEAM_USER: 'Team User',
-            SYSTEM: 'Sistema',
-            VERIFIED_DEVELOPER: '<:early_developer_badge:854716150076538901>'
-        };
-
         const userFlags = user.flags.toArray();
 
         db.push(`badges_${user.id}`, `${userFlags.length ? userFlags.map(flag => flags[flag]) : ''}`)
