cc61b55398bfce0b304d91f96ee7a81d7b19bb45
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-22 00:23:33 -0300

    Voltar pro normal msm >3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-﻿const Discord = require('discord.js')
-require('colors')
-const Enmap = require('enmap')
-const fs = require('fs')
-const DBL = require('dblapi.js')
-const { token, topgg_token } = require('./src/config/config.json')
-const mongoose = require('mongoose')
-const dbs = require('discord-buttons')
+﻿const Discord = require("discord.js");
+const colors = require("colors");
+const Enmap = require("enmap");
+const fs = require("fs");
+const DBL = require("dblapi.js");
+const { token, topgg_token } = require("./src/config/config.json")
+const mongoose = require("mongoose");
+const dbs = require("discord-buttons");
 
 const client = new Discord.Client({
   messageCacheLifetime: 604800,
@@ -16,37 +16,38 @@ const client = new Discord.Client({
   restWsBridgetimeout: 100,
   disableEveryone: true,
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
-})
+});
 
 dbs(client);
 
-['clientvariables', 'command', 'events', 'erelahandler', 'requestreacts'].forEach(handler => {
-  require(`./src/handlers/${handler}`)(client)
-})
+["clientvariables", "command", "events", "erelahandler", "requestreacts"].forEach(handler => {
+  require(`./src/handlers/${handler}`)(client);
+});
 
-require('./src/util/inlineReply')
+require("./src/util/inlineReply")
 
 mongoose.connect('mongodb+srv://spray:spray@cluster0.u1wmc.mongodb.net/db', {
   useUnifiedTopology: true,
   useNewUrlParser: true
-}).then(console.log('MongoDB conectado com sucesso!'))
+}).then(console.log("MongoDB conectado com sucesso!"));
 
-client.premium = new Enmap({ name: 'premium', dataDir: './src/databases/premium' })
-client.stats = new Enmap({ name: 'stats', dataDir: './src/databases/stats' })
-client.settings = new Enmap({ name: 'setups', dataDir: './src/databases/settings' })
-client.setups = new Enmap({ name: 'setups', dataDir: './src/databases/setups' })
-client.queuesaves = new Enmap({ name: 'queuesaves', dataDir: './src/databases/queuesaves', ensureProps: false })
-client.modActions = new Enmap({ name: 'actions', dataDir: './src/databases/warns' })
-client.userProfiles = new Enmap({ name: 'userProfiles', dataDir: './src/databases/warns' })
 
-const dbl = new DBL(topgg_token, client)
+client.premium = new Enmap({ name: "premium", dataDir: "./src/databases/premium" });
+client.stats = new Enmap({ name: "stats", dataDir: "./src/databases/stats" });
+client.settings = new Enmap({ name: "setups", dataDir: "./src/databases/settings" });
+client.setups = new Enmap({ name: "setups", dataDir: "./src/databases/setups" });
+client.queuesaves = new Enmap({ name: "queuesaves", dataDir: "./src/databases/queuesaves", ensureProps: false });
+client.modActions = new Enmap({ name: 'actions', dataDir: "./src/databases/warns" });
+client.userProfiles = new Enmap({ name: 'userProfiles', dataDir: "./src/databases/warns" });
+
+const dbl = new DBL(topgg_token, client);
 
 dbl.on('posted', () => {
-  console.log('Top.gg servers updated!')
+  console.log('Top.gg servers updated!');
 })
 
 dbl.on('error', e => {
-  console.log(`Error to update top.gg servers!\n ${e}`)
+  console.log(`Error to update top.gg servers!\n ${e}`);
 })
 
-client.login(token)
+client.login(token);
--- src/commands/Musica/addprevious.js
+++ src/commands/Musica/addprevious.js
@@ -1,20 +1,22 @@
-const playermanager = require('../../handlers/playermanager')
+const playermanager = require(`../../handlers/playermanager`);
 
 module.exports = {
-  name: 'addprevious',
-  category: '🎶 Music',
-  aliases: ['addp', 'addpre', 'addprevius', 'addprevios'],
-  description: 'Adiciona a música anterior à fila novamente!',
-  usage: 'addprevious',
-  example: '',
+  name: `addprevious`,
+  category: `🎶 Music`,
+  aliases: [`addp`, `addpre`, `addprevius`, `addprevios`],
+  description: `Adiciona a música anterior à fila novamente!`,
+  usage: `addprevious`,
+  example: "",
   permissoes: [],
   cooldown: 8,
   args: false,
-  parameters: { type: 'music', activeplayer: true, previoussong: true },
-
+  parameters: {"type":"music", "activeplayer": true, "previoussong": true},
+  
   run: async (client, message, args, cmduser, text, prefix, player) => {
-    let type = 'song:youtube'
-    if (player.queue.previous.uri.includes('soundcloud')) type = 'song:soundcloud'
-    playermanager(client, message, Array(player.queue.previous.uri), type)
+
+    let type = `song:youtube`;
+    if (player.queue.previous.uri.includes(`soundcloud`)) type = `song:soundcloud`
+    playermanager(client, message, Array(player.queue.previous.uri), type);
+  
   }
 }
