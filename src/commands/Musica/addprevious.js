7ce05caff8f7c2b6b825347669352d7ae8fc659b
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-22 00:13:13 -0300

    eslint

--- src/commands/Musica/addprevious.js
+++ src/commands/Musica/addprevious.js
@@ -1,22 +1,20 @@
-const playermanager = require(`../../handlers/playermanager`);
+const playermanager = require('../../handlers/playermanager')
 
 module.exports = {
-  name: `addprevious`,
-  category: `🎶 Music`,
-  aliases: [`addp`, `addpre`, `addprevius`, `addprevios`],
-  description: `Adiciona a música anterior à fila novamente!`,
-  usage: `addprevious`,
-  example: "",
+  name: 'addprevious',
+  category: '🎶 Music',
+  aliases: ['addp', 'addpre', 'addprevius', 'addprevios'],
+  description: 'Adiciona a música anterior à fila novamente!',
+  usage: 'addprevious',
+  example: '',
   permissoes: [],
   cooldown: 8,
   args: false,
-  parameters: {"type":"music", "activeplayer": true, "previoussong": true},
-  
-  run: async (client, message, args, cmduser, text, prefix, player) => {
+  parameters: { type: 'music', activeplayer: true, previoussong: true },
 
-    let type = `song:youtube`;
-    if (player.queue.previous.uri.includes(`soundcloud`)) type = `song:soundcloud`
-    playermanager(client, message, Array(player.queue.previous.uri), type);
-  
+  run: async (client, message, args, cmduser, text, prefix, player) => {
+    let type = 'song:youtube'
+    if (player.queue.previous.uri.includes('soundcloud')) type = 'song:soundcloud'
+    playermanager(client, message, Array(player.queue.previous.uri), type)
   }
 }
