77faecaac5f04533c7cd4e51a4e78df1767e6bfb
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-27 22:59:57 -0300

    funcionando

--- src/commands/SlashCommands/info/report.js
+++ src/commands/SlashCommands/info/report.js
@@ -20,22 +20,26 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
 
-        let canal = client.channels.cache.get("842984263392428032")
+        try {
+            var canal = client.channels.cache.get("842984263392428032")
 
-        let embed = new MessageEmbed()
-            .setTitle("Novo Bug/Report")
-            .setThumbnail(client.user.displayAvatarURL())
-            .addField("<a:SETA:852194614927818812> Servidor que reportou", `${interaction.guild.name}`)
-            .addField("<a:SETA:852194614927818812> Reportado por", `${interaction.user.tag}`)
-            .addField("<a:SETA:852194614927818812> Menção", `${interaction.user}`)
-            .addField("<a:SETA:852194614927818812> ID de quem reportou", `${interaction.user.id}`)
-            .addField("<a:SETA:852194614927818812> Bug", `\`${args[0]}\``)
-            .setColor("RANDOM")
-        canal.send(embed)
-
-        let z = new MessageEmbed()
-            .setDescription("<:setaaa:860626769089265665> Seu bug/report foi enviado para minha equipe, muito obrigado por me ajudar a melhorar!\n Contato com o desenvolvedor: Spray#7725")
-            .setColor('RANDOM')
-        interaction.followUp({ embeds:  [z] });
+            let embed = new MessageEmbed()
+                .setTitle("<:BadgeDiscordBugHunter:869963243056164927> Novo Bug/Report")
+                .setThumbnail(client.user.displayAvatarURL())
+                .addField("<:3199blurplejoin:856520144829808650> Servidor que reportou", `${interaction.guild.name}`)
+                .addField("<:3199blurplejoin:856520144829808650> Reportado por", `${interaction.user.tag}`)
+                .addField("<:3199blurplejoin:856520144829808650> Menção", `${interaction.user}`)
+                .addField("<:3199blurplejoin:856520144829808650> ID de quem reportou", `${interaction.user.id}`)
+                .addField("<:3199blurplejoin:856520144829808650> Bug", `\`${args[0]}\``)
+                .setColor("RANDOM")
+            canal.send({ embeds: [embed] })
+    
+            let z = new MessageEmbed()
+                .setDescription("<:setaaa:860626769089265665> Seu bug/report foi enviado para minha equipe, muito obrigado por me ajudar a melhorar!\n Contato com o desenvolvedor: Spray#7725")
+                .setColor('RANDOM')
+            interaction.followUp({ embeds:  [z] });
+        } catch(e) {
+            console.log(e)
+        }
     },
 };
