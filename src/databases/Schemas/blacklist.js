41bc12069da83bda0f9589100a5d6fb38139a632
Pedro <71204668+sprayx@users.noreply.github.com>
2021-09-20 11:42:41 -0300

    '

--- src/databases/Schemas/blacklist.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const Schema = new mongoose.Schema({
-  id: String
-})
-
-module.exports = mongoose.model('blacklist', Schema)
--- src/databases/Schemas/settings.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose')
-
-const guildSettingSchema = new Schema({
-  gid: { type: String },
-  prefix: { type: String, default: 's.' }
-})
-
-module.exports = model('guild_settings', guildSettingSchema)
