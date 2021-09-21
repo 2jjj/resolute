5c96baceadd1469567313953ec3433d27cbda8d0
Pedro <71204668+sprayx@users.noreply.github.com>
2021-09-20 22:53:30 -0300

    KK att play

--- src/events/lavalink/trackStart.js
+++ src/events/lavalink/trackStart.js
@@ -4,9 +4,9 @@ const { convertTime } = require('../../util/convert')
 module.exports = async (client, player, track, payload) => {
   const channel = client.channels.cache.get(player.textChannel)
   const emojiplay = client.emoji.play
-
+  console.log(track)
   const thing = new MessageEmbed()
-    .setDescription(`${emojiplay} **Começando a tocar:**\n [${track.title}](${track.uri}) - \`[${convertTime(track.duration)}]\` [<@${track.requester.id}>]`)
+    .setDescription(`${emojiplay} **Começando a tocar:**\n [${track.title}](${track.uri})\n\`Autor: ${track.author}\`\n\`Duração: ${convertTime(track.duration)}\`\n\`Requisitado por: ${track.requester.tag}\``)
     .setThumbnail(track.displayThumbnail('3'))
     .setColor(client.embedColor)
     .setTimestamp()
