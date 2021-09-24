038a2765ef074d425b9e69cbf7e6c02a52e6a99d
Pedro <71204668+sprayx@users.noreply.github.com>
2021-09-23 21:26:00 -0300

    d

--- src/commands/Message/Pescaria/pescar.js
+++ src/commands/Message/Pescaria/pescar.js
@@ -20,12 +20,9 @@ module.exports = {
 
     let varinhas = db.fetch(`varinhas_${user.id}`)
     if (varinhas === null) varinhas = 0
-    
-    const iscas = db.fetch(`iscas_${user.id}`)
-    if (iscas === null) iscas = 0
 
     if (varinhas < 1) {
-      const embed = new Discord.MessageEmbed()
+      let embed = new Discord.MessageEmbed()
         .addField('Você não possui varinhas!', `Para comprar uma varinha digite \`${prefix}buy varinha\` e compre uma varinha.`)
         .setFooter(ee.footertext, ee.footericon)
         .setColor('#1E90FF')
@@ -34,7 +31,7 @@ module.exports = {
 
     if (varinhas >= 1) {
       if (iscas < 1) {
-        const embed_isca = new Discord.MessageEmbed()
+        let embed_isca = new Discord.MessageEmbed()
           .addField('Você não possui iscas!', `Para comprar uma isca digite \`${prefix}buy iscas <quantidade>\` e compre uma isca.`)
           .setFooter(ee.footertext, ee.footericon)
           .setColor('#1E90FF')
@@ -50,7 +47,7 @@ module.exports = {
       const pirararas = Math.floor(Math.random() * 4 + 1)
       console.log(baiacus)
       console.log(arr_final)
-      const embed = new Discord.MessageEmbed()
+      let embed_pescar = new Discord.MessageEmbed()
         .setTitle('<:pescaria:879504651739861064> Pescaria')
         .setDescription(`Você pescou e conseguiu:
                 ${baiacus} Baiacus
@@ -59,7 +56,7 @@ module.exports = {
                 `)
         .setFooter(ee.footertext, ee.footericon)
         .setColor('#1E90FF')
-      return message.reply({ embeds: [embed] })
+      return message.reply({ embeds: [embed_pescar] })
     }
   }
 }
