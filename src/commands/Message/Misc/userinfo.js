b49add651cdfe7edfa8c03627ac6265ec892be62
Pedro <71204668+sprayx@users.noreply.github.com>
2021-09-27 22:14:23 -0300

    Update userinfo.js

--- src/commands/Message/Misc/userinfo.js
+++ src/commands/Message/Misc/userinfo.js
@@ -1,3 +1,4 @@
+const { flags } =require("../../../util/flags") 
 const Discord = require('discord.js')
 const moment = require('moment')
 
@@ -18,21 +19,6 @@ module.exports = {
 
     const user = message.mentions.users.first() || message.author
 
-    const flags = {
-      DISCORD_STAFF: '<:BadgeDiscordStaff:869961802409209877>',
-      DISCORD_EMPLOYEE: 'Discord Employee',
-      DISCORD_PARTNER: '<:5270blurplepartner:856520144628744213>',
-      BUGHUNTER_LEVEL_1: '<:BadgeDiscordBugHunter:853786346498031627>',
-      BUGHUNTER_LEVEL_2: '<:BadgeDiscordBugHunterGold:853786346871193600>',
-      HYPESQUAD_EVENTS: '<:HypeSquadEvents:869962392078020698>',
-      HOUSE_BRAVERY: '<:BadgeHypeSquadBravery:869962263870705674>',
-      HOUSE_BRILLIANCE: '<:1350discordbrillance:856520144762175498>',
-      HOUSE_BALANCE: '<:BadgeHypeSquadBalance:869962147805921361>',
-      EARLY_SUPPORTER: '<:early_developer_badge:854716150076538901>',
-      TEAM_USER: 'Team User',
-      SYSTEM: 'System',
-      VERIFIED_DEVELOPER: '<:early_developer_badge:854716150076538901>'
-    }
 
     const userFlags = user.flags.toArray()
 
