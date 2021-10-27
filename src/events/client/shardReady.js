fe34915ef6ca45fd424b395eb17964ac172791fb
Pedro <71204668+sprayx@users.noreply.github.com>
2021-10-27 18:50:07 -0300

    Update shardReady.js

--- src/events/client/shardReady.js
+++ src/events/client/shardReady.js
@@ -4,7 +4,7 @@ module.exports = async (client, shardid) => {
   const botUsers = usersEval.reduce((prev, val) => prev + val, 0)
   const botGuilds = guildsEval.reduce((prev, val) => prev + val, 0)
 
-  client.user.setActivity(`${botGuilds} guilds | Shard: ${shardid}`, {
+  client.user.setActivity(`Procurar música do youtube está desativado. Use o SoundCloud! | Shard: ${shardid}`, {
     shardID: shardid
   })
 }
